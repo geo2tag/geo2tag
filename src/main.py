29d46ca05cb1f0b23fff7a796bc3094f3d0533d6
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-04-29 21:45:16 +0300

    added status

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, current_app
 from flask.ext.restful import Resource, Api
 from service_resource import ServiceResource
+from status_resource import StatusResource
 from config_reader import getInstancePrefix
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(ServiceResource, '/'+getInstancePrefix()+'/service/<string:serviceName>')
+api.add_resource(StatusResource, '/'+getInstancePrefix()+'/status')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
---
+++ src/status_resource.py
@@ -0,0 +1,6 @@
+from flask import request
+from flask.ext.restful import Resource
+
+class StatusResource(Resource):
+    def get(self):
+        return 'OK'
