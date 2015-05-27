37e72fc47837b40673f9d7de93e958b7ee0f2a74
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-05-27 17:33:35 +0300

    GT-1196: file returned to stable state

--- src/tst/test_fisrt.py
+++ src/tst/test_fisrt.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
-from not_existing_module import not_existing_class
 class TestFirst(TestCase):
     def testFirst(self):
         print 'First test'
