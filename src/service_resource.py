023938322ed074a3aa9b85ecbe7100b0fcda230f
ilyaolchikov <ilyushaol26@gmail.com>
2015-05-30 12:36:43 +0300

    GT-1204. Done

--- src/service_resource.py
+++ src/service_resource.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask.ext.restful import Resource
 from flask_restful import reqparse
+from db_model import addService
 
 class ServiceResource(Resource):
     def get(self, serviceName):
@@ -14,6 +15,14 @@ class ServiceResource(Resource):
     def delete(self, serviceName):
         parserList = parse()
         return {serviceName: 'Service removed'} 
+
+class ServiceListResource(Resource):
+    def post(self):
+        listAgrs = parse()
+        result = addService(listAgrs.get('name', 0), listAgrs.get('logSize', 0), listAgrs.get('ownerId', 0))
+        if result is None:
+            return  "Service already exists", 400
+        return result
 
 def parse():
     parser = reqparse.RequestParser()
