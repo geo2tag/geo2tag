76ebdf779d72b8a2397ed7b5cb3d27c0351b9a27
Tatyana Berlenko <tank.berlin@gmail.com>
2015-05-30 14:16:23 +0200

    GT-1179

--- src/service_resource.py
+++ src/service_resource.py
@@ -1,6 +1,9 @@
 from flask import request
 from flask.ext.restful import Resource
 from flask_restful import reqparse
+from pymongo import MongoClient
+from config_reader import getHost, getPort, getDbName
+
 
 class ServiceResource(Resource):
     def get(self, serviceName):
@@ -22,3 +25,12 @@ def parse():
     parser.add_argument('ownerId', type=str, default='STUB')
     args = parser.parse_args()
     return args
+
+def getServiceList(number, offset):
+    db = MongoClient(getHost(), getPort())[getDbName()]
+    if number is None:
+        number = db['services'].count()
+    if offset is None:
+        offset = 0
+    result = list(db['services'].find().sort('name', 1).skip(offset).limit(number))
+    return result
