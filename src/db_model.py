2eab63376cd76b43fe19a056b8aefb3a55def0b9
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-01 19:11:46 +0300

    GT-1206

--- src/db_model.py
+++ src/db_model.py
@@ -1,29 +1,12 @@
 from pymongo import MongoClient
 from config_reader import getHost, getPort, getDbName
-from  service_not_found_exception import ServiceNotFoundException
 
 # Collections
 TAGS = 'tags'
 db = MongoClient(getHost(), getPort())[getDbName()]
-COLLECTION = 'services'
 
 def addTag(tag):
     db[TAGS].insert(tag)
 
-def addService(name, logSize, ownerld):
-    obj = db[COLLECTION].find_one({'name' : name})
-    if obj != None:
-        return False
-    db[COLLECTION].save({'name' : name, 'config' : {'log_size' : logSize}, 'owner_id' : ownerld})
-    obj = db[COLLECTION].find_one({'name' : name})
-    if obj == None:
-    	return None
-    else:
-    	return obj['_id']
-
 #    def getNearTags(self, latitude, longitude):
-def  getServiceIdByName(name):
-    obj = db[COLLECTION].find_one({'name' : name})
-    if obj != None:
-        return obj
-    raise ServiceNotFoundException()
+
