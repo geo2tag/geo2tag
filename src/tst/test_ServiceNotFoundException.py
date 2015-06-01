e33992269a4378df2a751d863b462ac4647ae63e
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-01 19:08:50 +0300

    GT-1206. Edit test

--- src/tst/test_ServiceNotFoundException.py
+++ src/tst/test_ServiceNotFoundException.py
@@ -9,9 +9,7 @@ from  service_not_found_exception import ServiceNotFoundException
 
 class test_ServiceNotFoundException(unittest.TestCase):
     def test_ServiceNotFoundException_func(self):
-        mas = []
         try:
-            mas[1] = 1
-        except Exception, e:
-            ServiceException = ServiceNotFoundException(e)
-            self.assertTrue(('Service not found', 400) == ServiceException.getReturnObject())
+            raise ServiceNotFoundException()
+        except ServiceNotFoundException as e:
+            self.assertEquals(('Service not found', 400), e.getReturnObject())
