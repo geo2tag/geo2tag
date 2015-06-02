6152d4a97a7960a5d3bff22f4b80861519797cf5
galiaf95 <galiaf1995@gmail.com>
2015-06-03 01:28:03 +0300

    Added unit-test for GT-1183, and modified getLog function

--- src/db_model.py
+++ src/db_model.py
@@ -3,7 +3,9 @@ from config_reader import getHost, getPort, getDbName
 import pymongo
 from datetime import datetime
 
-COLL_LOG = "log"
+# getLog constants
+COLLECTION_LOG_NAME = "log"
+FIND_AND_SORT_KEY = "date"
 
 # Collections
 TAGS = 'tags'
@@ -15,17 +17,20 @@ def addTag(tag):
 #    def getNearTags(self, latitude, longitude):
 
 def getLog(dbName, number, offset, dateFrom, dateTo) :
-	collection = db[COLL_LOG]
-	if number == None :
-		number = 0
-	if offset == None :
-		offset = 0
-	if dateFrom == None && dateTo == None :
+	collection = db[COLLECTION_LOG_NAME]
+	#if collection.count() == 0
+	#	collection.drop()
+	#	return None
+	number = 0 if (number == None or number < 0) else number
+	offset = 0 if (offset == None or offset < 0) else offset
+	if (dateFrom == None and dateTo == None) :
 		return None
 	elif dateFrom == None :
-		return collection.find({"date" : {"$lte" : dateTo}}, None, offset, number).sort("date", pymongo.ASCENDING)	
+		return collection.find({FIND_AND_SORT_KEY : {"$lte" : dateTo}}, None, offset, number).sort(FIND_AND_SORT_KEY, pymongo.ASCENDING)	
 	elif dateTo == None :
-		return collection.find({"date" : {"$gte" : dateFrom}}, None, offset, number).sort("date", pymongo.ASCENDING)
-	else 
-		return collection.find({"date" : {"$gte" : dateFrom , "$lte" : dateTo}}, None, offset, number).sort("date", pymongo.ASCENDING)
+		return collection.find({FIND_AND_SORT_KEY : {"$gte" : dateFrom}}, None, offset, number).sort(FIND_AND_SORT_KEY, pymongo.ASCENDING)
+	else :
+		if dateFrom > dateTo :
+			return None
+		return collection.find({FIND_AND_SORT_KEY : {"$gte" : dateFrom , "$lte" : dateTo}}, None, offset, number).sort(FIND_AND_SORT_KEY, pymongo.ASCENDING)
 
