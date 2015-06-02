8b9a074fad2613d1e860c626614d1a9097352b7d
galiaf95 <galiaf1995@gmail.com>
2015-06-02 20:46:04 +0300

    getLog function moved to db_model.py and fixed

--- src/db_model.py
+++ src/db_model.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 from config_reader import getHost, getPort, getDbName
+import pymongo
+from datetime import datetime
+
+COLL_LOG = "log"
 
 # Collections
 TAGS = 'tags'
@@ -9,4 +13,19 @@ def addTag(tag):
     db[TAGS].insert(tag)
 
 #    def getNearTags(self, latitude, longitude):
+
+def getLog(dbName, number, offset, dateFrom, dateTo) :
+	collection = db[COLL_LOG]
+	if number == None :
+		number = 0
+	if offset == None :
+		offset = 0
+	if dateFrom == None && dateTo == None :
+		return None
+	elif dateFrom == None :
+		return collection.find({"date" : {"$lte" : dateTo}}, None, offset, number).sort("date", pymongo.ASCENDING)	
+	elif dateTo == None :
+		return collection.find({"date" : {"$gte" : dateFrom}}, None, offset, number).sort("date", pymongo.ASCENDING)
+	else 
+		return collection.find({"date" : {"$gte" : dateFrom , "$lte" : dateTo}}, None, offset, number).sort("date", pymongo.ASCENDING)
 
