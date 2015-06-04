ce61eb72323e66af46b5f7dcc2d58218038b67a1
4ukuta <nikita@nikita-Lenovo-Z580>
2015-06-04 23:30:06 +0300

    GT-1223

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,11 +1,12 @@
 from configparser import ConfigParser
-
-CONFIG_PATH = '../config/config.ini'
+from configparser import  SafeConfigParser
+import os
+CONFIG_PATH = os.path.dirname(os.path.realpath(__file__))+'/config.ini'
 SECTION = 'main'
 OPTION_HOST = 'host'
 OPTION_PORT = 'port'
 OPTION_DBNAME = 'db_name'
-OPTION_INSTANCEPREFIX = 'instanceprefix'
+OPTION_INSTANCEPREFIX = 'instance_prefix'
 
 HOST = 'localhost'
 PORT = 27017
@@ -13,20 +14,20 @@ DBNAME = 'geomongo'
 INSTANCEPREFIX = 'instance'
 
 def getConfigParser():
-	config = ConfigParser()
+	config = SafeConfigParser({OPTION_HOST:HOST,OPTION_PORT:str(PORT),OPTION_DBNAME:DBNAME,OPTION_INSTANCEPREFIX:INSTANCEPREFIX})
 	config.read(CONFIG_PATH)
 	return config
 	
 def getHost():
-	return getConfigParser().get(SECTION,OPTION_HOST) if getConfigParser().has_option(SECTION,OPTION_HOST) else HOST
+	return getConfigParser().get(SECTION,OPTION_HOST) 
 
 def getPort():
-    return getConfigParser().get(SECTION,OPTION_PORT) if getConfigParser().has_option(SECTION,OPTION_PORT) else PORT
+    return getConfigParser().get(SECTION,OPTION_PORT)
 
 def getDbName():
-    return getConfigParser().get(SECTION,OPTION_DBNAME) if getConfigParser().has_option(SECTION,OPTION_DBNAME) else DBNAME
+    return getConfigParser().get(SECTION,OPTION_DBNAME) 
 
 def getInstancePrefix():
-	return getConfigParser().get(SECTION,OPTION_INSTANCEPREFIX) if getConfigParser().has_option(SECTION,OPTION_INSTANCEPREFIX) else INSTANCEPREFIX
+	return getConfigParser().get(SECTION,OPTION_INSTANCEPREFIX)
    	
  
--- src/tst/test_GT1223ConfigParser.py
+++ src/tst/test_GT1223ConfigParser.py
@@ -1,19 +1,51 @@
 from unittest import TestCase
-
+from configparser import ConfigParser
+from configparser import  SafeConfigParser
+import os
 import sys
+
 sys.path.append('../')
-import config_reader
+CONFIG_PATH = os.path.dirname(os.path.realpath(__file__))+'/DEBUG'
+SECTION = 'main'
+OPTION_HOST = 'host'
+OPTION_PORT = 'port'
+OPTION_DBNAME = 'db_name'
+OPTION_INSTANCEPREFIX = 'instance_prefix'
+
 HOST = 'localhost'
 PORT = 27017
 DBNAME = 'geomongo'
 INSTANCEPREFIX = 'instance'
 
+def getConfigParser():
+	config = SafeConfigParser({OPTION_HOST:HOST,OPTION_PORT:str(PORT),OPTION_DBNAME:DBNAME,OPTION_INSTANCEPREFIX:INSTANCEPREFIX})
+	config.read(CONFIG_PATH)
+	return config
+	
+def getHost():
+	return getConfigParser().get(SECTION,OPTION_HOST) 
+
+def getPort():
+    return getConfigParser().get(SECTION,OPTION_PORT)
+
+def getDbName():
+    return getConfigParser().get(SECTION,OPTION_DBNAME) 
+
+def getInstancePrefix():
+	return getConfigParser().get(SECTION,OPTION_INSTANCEPREFIX)
+
+HOST = 'localhost'
+PORT = 27017
+DBNAME = 'geomongo'
+INSTANCEPREFIX = 'instance'
+
+
 class TestConfingParser(TestCase):
     def testHost(self):
-    	self.assertEqual(HOST, config_reader.getHost())
+    	self.assertEqual(HOST, getHost())
     def testPort(self):
-    	self.assertEqual(PORT, config_reader.getPort())
+    	self.assertEqual(str(PORT), getPort())
     def testDbName(self):
-    	self.assertEqual(DBNAME, config_reader.getDbName())
+    	self.assertEqual(DBNAME, getDbName())
     def testInstancePrefix(self):
-    	self.assertEqual(INSTANCEPREFIX, config_reader.getInstancePrefix())
+    	self.assertEqual(INSTANCEPREFIX, getInstancePrefix())
