728fbc61636a47e3e1334cbabddf84eac2b8882a
galiaf95 <galiaf1995@gmail.com>
2015-06-05 01:05:03 +0300

    GT-1205 : String constants in parsers defined as variables

--- src/service_list_resource.py
+++ src/service_list_resource.py
@@ -3,16 +3,25 @@ from flask.ext.restful import Resource
 from db_model import addService, getServiceList
 from service_resource import parse
 
+GET_ARGS_NUMBER = "number"
+GET_ARGS_OFFSET = "offset"
+POST_ARGS_NAME = "name"
+POST_ARGS_LOG_SIZE = "logSize"
+POST_ARGS_OWNER_ID = "ownerId"
+
+SERVICE_ALREADY_EXIST_MSG = "Service already exists"
+
+
 class ServiceListResource(Resource):
 
     def get(self):
         args = parser()
-        if 'number' in args:
-            number = args['number']
+        if GET_ARGS_NUMBER in args:
+            number = args[GET_ARGS_NUMBER]
         else:
             number = None
-        if 'offset' in args:
-            offset = args['offset']
+        if GET_ARGS_OFFSET in args:
+            offset = args[GET_ARGS_OFFSET]
         else:
             offset = None
         serviceList = getServiceList(number, offset)
@@ -20,14 +29,14 @@ class ServiceListResource(Resource):
 
     def post(self):
         listAgrs = parse()
-        result = addService(listAgrs.get('name'), listAgrs.get('logSize'), listAgrs.get('ownerId'))
+        result = addService(listAgrs.get(POST_ARGS_NAME), listAgrs.get(POST_ARGS_LOG_SIZE), listAgrs.get(POST_ARGS_OWNER_ID))
         if result is None:
-            return  "Service already exists", 400
+            return  SERVICE_ALREADY_EXIST_MSG, 400
         return result
         
 def parser():
     parser = reqparse.RequestParser()
-    parser.add_argument('number', type=int, default=None)
-    parser.add_argument('offset', type=int, default=None)
+    parser.add_argument(GET_ARGS_NUMBER, type=int, default=None)
+    parser.add_argument(GET_ARGS_OFFSET, type=int, default=None)
     args = parser.parse_args()
     return args
