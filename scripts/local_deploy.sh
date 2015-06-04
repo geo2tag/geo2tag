b7f16ebbcf45d97c55d2ac29875def8f73f23351
Rostislav Ivanov <kitemeall@gmail.com>
2015-06-04 14:02:02 +0300

    GT-1224

--- scripts/local_deploy.sh
+++ scripts/local_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HOSTS_STRING="127.0.0.1 geomongo"
+DEBUG_FILE="/var/www/geomongo/DEBUG"
 if ! grep -Fxq "$HOSTS_STRING" /etc/hosts
 then
 	echo "$HOSTS_STRING" >> /etc/hosts
@@ -15,8 +16,23 @@ cp config/geomongo.conf /etc/apache2/sites-available/
 
 ./scripts/setup_pip_dependencies.sh
 
+COMMIT=$(git rev-parse HEAD)
+BRANCH=$(git status |head -1| cut -d' ' -f 3)
+DATE=$(date -R)
+VERSION=$(git describe)
+[ ${#VERSION} == 0 ] && VERSION="no version" 
+echo "{ 
+'commit' : $COMMIT 
+'date' : $DATE
+'branch' : $BRANCH
+'version' : $VERSION
+}" > $DEBUG_FILE
+
 chown -R www-data:www-data /var/www/geomongo
 
 a2ensite geomongo.conf
 
 service apache2 restart
+
+
+
