bff28c0d5c351dc16e3e6f178049fdd69eb8f66b
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-04 17:39:32 +0300

    GT-1228

--- src/service_list_resource.py
+++ src/service_list_resource.py
@@ -1,7 +1,6 @@
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from db_model import addService, getServiceList
-from service_resource import parse
 from bson.json_util import dumps
 from service_parsers import ServiceListParser
 
@@ -26,10 +25,3 @@ class ServiceListResource(Resource):
         if result is None:
             return  "Service already exists", 400
         return dumps(result, ensure_ascii=False).encode('utf8')
-        
-def parser():
-    parser = reqparse.RequestParser()
-    parser.add_argument('number', type=int, default=None)
-    parser.add_argument('offset', type=int, default=None)
-    args = parser.parse_args()
-    return args
