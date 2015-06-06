d5a7b136e980134b95e62dd2df35443bb9143b06
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-06 13:50:16 +0300

    GT-1174. Test

--- src/main.py
+++ src/main.py
@@ -9,19 +9,15 @@ from debug_info_resource import DebugInfoResource
 app = Flask(__name__)
 api = Api(app)
 
-<<<<<<< HEAD
 def getPathWithPrefix(str):
     path = '/'+getInstancePrefix()+str
     return path
 
 api.add_resource(ServiceResource, getPathWithPrefix('/service/<string:serviceName>'))
-api.add_resource(StatusResource, getPathWithPrefix('/status'))
-=======
 api.add_resource(ServiceResource, '/'+getInstancePrefix()+'/service/<string:serviceName>')
 api.add_resource(StatusResource, '/'+getInstancePrefix()+'/status')
 api.add_resource(ServiceListResource, '/'+getInstancePrefix()+'/service/')
 api.add_resource(DebugInfoResource, '/'+getInstancePrefix()+'/debug_info/')
->>>>>>> master
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
