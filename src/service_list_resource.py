75babe59a6d1bb81059f46a2e3d244aadfcccb94
Max_Borisov <MaxBorisov33@mail.ru>
2015-06-08 23:19:26 +0300

    GT-1212

--- src/service_list_resource.py
+++ src/service_list_resource.py
@@ -3,6 +3,7 @@ from flask.ext.restful import Resource
 from db_model import addService, getServiceList
 from bson.json_util import dumps
 from service_list_parsers import ServiceListParser
+from service_already_exists_exception import ServiceAlreadyExistsException
 
 GET_ARGS_NUMBER = "number"
 GET_ARGS_OFFSET = "offset"
@@ -29,9 +30,9 @@ class ServiceListResource(Resource):
         return serviceList
 
     def post(self):
-        listAgrs = parse()
+        listAgrs = ServiceListParser.parsePostParameters()
         try:
-            result = addService(listAgrs.get('name'), listAgrs.get('logSize'), listAgrs.get('ownerId'))
+            result = addService(listAgrs.get(POST_ARGS_NAME), listAgrs.get(POST_ARGS_LOG_SIZE), listAgrs.get(POST_ARGS_OWNER_ID))
         except ServiceAlreadyExistsException as e:
             return e.getReturnObject()
         return dumps(result, ensure_ascii=False).encode('utf8')
