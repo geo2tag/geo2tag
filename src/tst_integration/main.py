8e70f747ca13d7473b6f96de87c7fe68f49414ae
Tatyana Berlenko <tank.berlin@gmail.com>
2015-06-10 15:23:48 +0200

    GT-1240

--- src/tst_integration/main.py
+++ src/tst_integration/main.py
@@ -2,11 +2,13 @@ import unittest
 import sys
 from basic_integration_test import BasicIntegrationTest 
 from test_status_request import TestStatusRequest
+from test_request_delete import TestStatusRequestDelete
 
 def main(host):
     suite = unittest.TestSuite()
     suite.addTest(BasicIntegrationTest.parametrize(TestStatusRequest, param=host))
-    
+    suite.addTest(BasicIntegrationTest.parametrize(TestStatusRequestDelete, param=host))
+
     returnCode = not unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
     sys.exit(returnCode)
 
---
+++ src/tst_integration/test_request_delete.py
@@ -0,0 +1,20 @@
+import unittest
+import requests
+
+from basic_integration_test import BasicIntegrationTest
+
+TEST_URL = '/instance/service/testservice'
+TEST_NOT_VALID_URL = '/instance/service/nameservicenotvalid'
+
+VALID_RESPONSE_CODE = 200
+VALID_RESPONSE_CODE_NOT_VALID_URL = 400
+
+class TestStatusRequestDelete(BasicIntegrationTest):
+    def testStatusRequest(self):
+        response = requests.delete(self.getUrl(TEST_NOT_VALID_URL))
+        responseCode = response.status_code
+        self.assertEquals(responseCode, VALID_RESPONSE_CODE_NOT_VALID_URL)
+        response = requests.delete(self.getUrl(TEST_URL))
+        responseCode = response.status_code        
+        self.assertEquals(responseCode, VALID_RESPONSE_CODE)
+        
