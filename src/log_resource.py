8178c1906349a4385c066f07ae243350213aa1ab
Rostislav Ivanov <kitemeall@gmail.com>
2015-06-11 21:51:51 +0300

    GT-1184

--- src/log_resource.py
+++ src/log_resource.py
@@ -1,1 +1,15 @@
 from db_model import getLog
+from flask import request
+from flask.ext.restful import Resource
+from flask_restful import reqparse
+
+
+class LogResource(Resource):
+    def get(self, serviceName = None):
+    	parser_dict = parse()
+    	if serviceName == None:
+    		serviceName = 'masterDbName'
+
+    	return getLog(serviceName, parser_dict['number'], parser_dict['offset'], 
+    		parser_dict['dateFrom'], parser_dict['dateTo'])
+    	
