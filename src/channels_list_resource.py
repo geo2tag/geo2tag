edb60e99e7947b2b8c3c7d28b4e5eeeee2a3f093
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-17 20:28:42 +0300

    GT-1264. First test

--- src/channels_list_resource.py
+++ src/channels_list_resource.py
@@ -1,8 +1,8 @@
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from channels_list_parsers import ChannelsListResourceParser
-
+from db_model import getChannelsList
 class ChannelsListResource(Resource):
     def get(self, serviceName):
-        return ChannelsListResourceParser.parseGetParameters()
-
+        s = ChannelsListResourceParser.parseGetParameters()
+        return getChannelsList(serviceName, '_', 2)
