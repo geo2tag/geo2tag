b3bc1cf4fbb4b14b20ded0e19060691d890ce253
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-18 21:02:24 +0300

    GT-1281. Parser done

--- src/channels_list_parsers.py
+++ src/channels_list_parsers.py
@@ -3,6 +3,8 @@ from flask_restful import reqparse
 ARGS_SUBSTRING = 'substring'
 ARGS_NUMBER = 'number'
 ARGS_OFFSET = 'offset'
+ARGS_NAME = 'name'
+ARGS_JSON = 'json'
 
 class ChannelsListResourceParser():
     @staticmethod
@@ -11,5 +13,13 @@ class ChannelsListResourceParser():
         parser.add_argument(ARGS_SUBSTRING, type=str)
         parser.add_argument(ARGS_NUMBER, type=int)
         parser.add_argument(ARGS_OFFSET, type=int)
+        args = parser.parse_args()
+        return args
+
+    @staticmethod
+    def parseGetParameters():
+        parser = reqparse.RequestParser()
+        parser.add_argument(ARGS_NAME, type=str, required=True)
+        parser.add_argument(ARGS_JSON, type=dict, required=True)
         args = parser.parse_args()
         return args
