35c3cdd510d1905a9a9b3a201b2da7759a0a9499
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-18 23:08:41 +0300

    GT-1283. Done

--- src/channels_list_resource.py
+++ src/channels_list_resource.py
@@ -1,8 +1,12 @@
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from channels_list_parsers import ChannelsListResourceParser
+from db_model import addChannel
 
 class ChannelsListResource(Resource):
     def get(self, serviceName):
         return ChannelsListResourceParser.parseGetParameters()
 
+    def post(self, serviceName):
+        listArgs = ChannelsListResourceParser.parsePostParameters()
+        return addChannel(listArgs.get('name', None), listArgs.get('json', None), 'STUB', serviceName)
