e4d0dca4a32c383e6afbaf23e3af7331db345d72
Rostislav Ivanov <kitemeall@gmail.com>
2015-06-23 01:06:42 +0300

    GT-1238

--- src/tst_integration/main.py
+++ src/tst_integration/main.py
@@ -6,6 +6,7 @@ from test_get_service_name import TestServiceGetRequest
 from test_get_request import TestServiceListGetRequest
 from test_put_service_name import TestServicePutRequest
 from test_post_request import TestServiceListPostRequest
+from test_service_name_get import TestServiceNameGetRequest
 
 def main(host):
     suite = unittest.TestSuite()
@@ -14,6 +15,9 @@ def main(host):
     suite.addTest(BasicIntegrationTest.parametrize(TestServiceListGetRequest, param=host))
     suite.addTest(BasicIntegrationTest.parametrize(TestServiceListPostRequest, param=host))
     suite.addTest(BasicIntegrationTest.parametrize(TestServiceGetRequest, param=host))
+    suite.addTest(BasicIntegrationTest.parametrize(TestServiceNameGetRequest, param=host))
+
+    
     returnCode = not unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
     sys.exit(returnCode)
 
