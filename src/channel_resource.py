19ddb46650b15cbf75952526905586347f0ed416
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-22 18:33:56 +0300

    GT-1293. Test

---
+++ src/channel_resource.py
@@ -0,0 +1,16 @@
+from flask_restful import reqparse
+from flask.ext.restful import Resource
+
+from channel_does_not_exist import ChannelDoesNotExist
+from channel_parsers import ChannelResourceParser
+from db_model import updateChannel
+
+class ChannelResource(Resource):
+    
+    def put(self, serviceName, channelId):
+    	listArgs = ChannelResourceParser.parsePutParameters()
+    	try:
+    		updateChannel(serviceName, channelId, listArgs.get('name'), listArgs.get('json'), listArgs.get('acl'))
+    	except ChannelDoesNotExist as e:
+    		return e.getReturnObject()
+    	return {}, 200
