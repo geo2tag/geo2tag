49e76a2d53b091b2db9caf74e57a4a42267d89f4
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-22 12:30:19 +0300

    GT-1290. Add delete method

---
+++ src/channel_resource.py
@@ -0,0 +1,12 @@
+from flask_restful import reqparse
+from flask.ext.restful import Resource
+from db_model import deleteChannelById
+
+
+class ChannelResource(Resource):
+    def delete(self, serviceName, channelId):
+    	try:
+    		deleteChannelById(serviceName, channelId) 
+    	except ChannelDoesNotExist as e:
+    		return e.getReturnObject()
+        return {}, 200
--- src/channels_list_resource.py
+++ src/channels_list_resource.py
@@ -1,17 +1,7 @@
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from channels_list_parsers import ChannelsListResourceParser
-from channel_does_not_exist import ChannelDoesNotExist
-from db_model import deleteChannelById
 
 class ChannelsListResource(Resource):
     def get(self, serviceName):
         return ChannelsListResourceParser.parseGetParameters()
-
-    def delete(self, serviceName):
-    	try:
-    		deleteChannelById(serviceName, 'channelId') 
-    	except ChannelDoesNotExist as e:
-    		return e.getReturnObject()
-        return {}, 200
-
