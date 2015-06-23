c07ace299e305eb4c0b9d11f974c9107b1893240
Max_Borisov <MaxBorisov33@mail.ru>
2015-06-24 01:49:25 +0300

    GT-1288

--- src/channels_list_resource.py
+++ src/channels_list_resource.py
@@ -2,7 +2,9 @@ from flask_restful import reqparse
 from flask.ext.restful import Resource
 from channels_list_parsers import ChannelsListResourceParser
 from db_model import addChannel
-from db_model import getChannelsList
+from db_model import getChannelsList, getChannelByName
+from channel_does_not_exist import ChannelDoesNotExist
+from channel_already_exists import ChannelAlreadyExists
 
 class ChannelsListResource(Resource):
     def get(self, serviceName):
@@ -11,4 +13,9 @@ class ChannelsListResource(Resource):
 
     def post(self, serviceName):
         listArgs = ChannelsListResourceParser.parsePostParameters()
-        return addChannel(listArgs.get('name', None), listArgs.get('json', None), 'STUB', serviceName)
+        try:
+        	obj = getChannelByName(serviceName, listArgs.get('name', None))
+        	e = ChannelAlreadyExists()
+        	return e.getReturnObject()
+        except ChannelDoesNotExist as e:
+        	return addChannel(listArgs.get('name', None), listArgs.get('json', None), 'STUB', serviceName)
