d522d91559b451f5d9477aad0161cfacbc26874b
Tatyana Berlenko <tank.berlin@gmail.com>
2015-06-23 14:16:50 +0200

    GT-1240

--- src/tst_integration/test_delete_service_name.py
+++ src/tst_integration/test_delete_service_name.py
@@ -1,20 +1,24 @@
 import unittest
 import requests
-
+import sys
+sys.path.append('../')
+from db_model import addService
 from basic_integration_test import BasicIntegrationTest
 
-TEST_URL = '/instance/service/testservice'
+TEST_URL = '/instance/service/testservice_1'
 TEST_NOT_VALID_URL = '/instance/service/nameservicenotvalid'
 
 VALID_RESPONSE_CODE = 200
 VALID_RESPONSE_CODE_NOT_VALID_URL = 400
+
 
 class TestServiceDeleteRequest(BasicIntegrationTest):
     def testServiceDeleteRequest(self):
         response = requests.delete(self.getUrl(TEST_NOT_VALID_URL))
         responseCode = response.status_code
         self.assertEquals(responseCode, VALID_RESPONSE_CODE_NOT_VALID_URL)
+        addService('testservice_1', 1, 1)
         response = requests.delete(self.getUrl(TEST_URL))
         responseCode = response.status_code        
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
-        
+        response = requests.get(self.getUrl(TEST_URL))
