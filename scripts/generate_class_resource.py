dbfa22ff9af30cb8714c59397a1ce125cdde5d4c
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-25 13:44:29 +0300

    GT-1270. Done

--- scripts/generate_class_resource.py
+++ scripts/generate_class_resource.py
@@ -1,4 +1,5 @@
 import os
+import sys
 METHODS = ['post', 'get', 'put', 'delete']
 TYPES = ['bool', 'int', 'float', 'tuple', 'list', 'str', 'dict', 'set']
 INCLUDE_MODULE = 'from flask_restful import reqparse\n\
@@ -12,6 +13,7 @@ DEF = 'def '
 MAIN_STR = "if __name__ == '__main__':\n"
 addResource = 'api.add_resource('
 MAIN_FILE = 'main.py'
+PARSER_ARGS = {}
 def make_generator(args):
     if args.m is None or args.name is None:
         parser.print_help()
@@ -20,14 +22,6 @@ def make_generator(args):
         if method.lower() not in METHODS:
             print 'method ' + method + 'not in ' + str(METHODS)
             return
-
-    for i in range(len(args.p)):
-        if i%2 == 0:
-            print args.p[i]
-            if args.p[i].lower() not in TYPES:
-                print 'Type ' + args.p[i] + ' is not base type '
-                return
-
 
     fileName = args.name + '.py'
     os.chdir('../src')
@@ -38,6 +32,7 @@ def make_generator(args):
     for methods in args.m:
         newResource.write(TAB + DEF + methods.lower() + '():\n')
         newResource.write(TAB + TAB + '\n')
+        PARSER_ARGS[methods] = {}
 
     main = open(MAIN_FILE, 'r')
     mainStrings = main.readlines()
@@ -54,6 +49,25 @@ def make_generator(args):
             mainWrite.write(MAIN_STR)
     print 'Resourse added successfully'
 
+    flag = True
+    i = 0
+    while flag:
+        print('Enter arguments of parser:')
+        while i < len(args.m):
+            countArgs = int(input('Enter count of arguments by method ' + args.m[i] + ' '))
+            j = 0
+            while j < countArgs:
+                print 'Method ' + str(args.m[i]) + ' argument ' + str(j + 1)
+                argument = sys.stdin.readline()
+                argumentDict = argument.split(' ')
+                if argumentDict[1][0:-1] in TYPES:
+                    PARSER_ARGS[args.m[i]][argumentDict[0].lower()] = argumentDict[1][0:-1]
+                    j+=1
+            i+=1
+        flag = False
+
+    print PARSER_ARGS
+
     fileNameParser = args.name + '_parser.py'
     newParser = open(fileNameParser, 'w')
     newParser.write(INCLUDE_MODULE_PARSER)
@@ -63,7 +77,8 @@ def make_generator(args):
         newParser.write(TAB + STATIC)
         newParser.write(TAB + DEF + 'parse' + methods[0].upper() + methods[1::].lower() + 'Parameters' + '():\n')
         newParser.write(TAB + TAB + PARSER_TEMPLETE + '\n')
-        newParser.write(TAB + TAB + ADD_ARGUMENT + args.p[i] + ', type = ' + args.p[i+1] + ')')
+        for arg in PARSER_ARGS[methods]:
+            newParser.write(TAB + TAB + ADD_ARGUMENT + arg + ', type = ' + PARSER_ARGS[methods][arg] + ')\n')
         newParser.write(TAB + TAB + '\n')
         i+=1
 
@@ -72,6 +87,5 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate class resourse')
 parser.add_argument('--name', help='enter name of resourse class', required=True)
 parser.add_argument('--m', type = str, help='enter methods of resourse class', required=True, nargs = '+')
-parser.add_argument('--p', type = str, help='enter parameters of parser', required=True, nargs = '+')
 args = parser.parse_args()
 make_generator(args)
