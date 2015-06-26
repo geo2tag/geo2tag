ba626d813637f8e78311f21be26b9f5cf1bd6947
Max_Borisov <MaxBorisov33@mail.ru>
2015-06-26 12:35:37 +0300

    GT-1272

--- scripts/generate_unittest.py
+++ scripts/generate_unittest.py
@@ -9,19 +9,19 @@ INCLUDE_MODULE = 'import unittest\nimport sys\nfrom pymongo import MongoClient\n
 TAB = '    '
 
 def generateUnittest(args):
-	os.chdir('../src/tst')
-	unittestFile = open(FILE_NAME + args.name + '.py', 'w')
-	unittestFile.write(INCLUDE_MODULE)
-	unittestFile.write('class Test' + args.name + '(unittest.TestCase):\n')
-	unittestFile.write(TAB + 'def test' + args.name + '(self):\n\n')
-	unittestFile.write(TAB + '@classmethod\n' + TAB + 'def setUpClass(cls):\n\n')
-	unittestFile.write(TAB + '@classmethod\n' + TAB + 'def tearDownClass(cls):\n\n')
+    os.chdir('../src/tst')
+    unittestFile = open(FILE_NAME + args.name + '.py', 'w')
+    unittestFile.write(INCLUDE_MODULE)
+    unittestFile.write('class Test' + args.name + '(unittest.TestCase):\n')
+    unittestFile.write(TAB + 'def test' + args.name + '(self):\n\n')
+    unittestFile.write(TAB + '@classmethod\n' + TAB + 'def setUpClass(cls):\n\n')
+    unittestFile.write(TAB + '@classmethod\n' + TAB + 'def tearDownClass(cls):\n\n')
 
 def run():
-	parser = argparse.ArgumentParser(description='Generate unittest')
-	parser.add_argument('--name', help='enter unittest name', required=True)
-	args = parser.parse_args()
-	generateUnittest(args)
+    parser = argparse.ArgumentParser(description='Generate unittest')
+    parser.add_argument('--name', help='enter unittest name', required=True)
+    args = parser.parse_args()
+    generateUnittest(args)
 
 if __name__ == '__main__':
     run()
