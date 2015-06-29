ec1f37b855dab3eeac6305b6c14852ebf9895c6e
4ukuta <nikita@nikita-Lenovo-Z580>
2015-06-29 20:43:32 +0300

    GT-1235

--- src/tst_integration/test_instance_log.py
+++ src/tst_integration/test_instance_log.py
@@ -4,15 +4,24 @@ from basic_integration_test import BasicIntegrationTest
 from db_model import addService, removeService
 import json
 
-TEST_URL = "/instance/service/testservice/log?date_from='1983-01-22T08:00:00'"
+TEST_URL1 = "/instance/service/testservice/log?date_from='1983-01-22T08:00:00'"
 TEST_URL2 = "/instance/log?date_from='1983-01-22T08:00:00'"
 VALID_RESPONSE_CODE = 200
 INVALID_RESPONSE_CODE = 400
 
 class TestInstanceLogRequest(BasicIntegrationTest):
-    def testInstanceLogRequest(self):
-        response = requests.get(self.getUrl(TEST_URL))
+    def testInstanceLogRequest_URL1_VALID(self):
+        response = requests.get(self.getUrl(TEST_URL1))
         responseText = response.text
         responseJson = json.loads(responseText)
         responseCode = response.status_code
+        print responseCode
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
+    def testInstanceLogRequest_URL2_VALID(self):
+        response = requests.get(self.getUrl(TEST_URL2))
+        responseText = response.text
+        responseJson = json.loads(responseText)
+        responseCode = response.status_code
+        print responseCode
+        self.assertEquals(responseCode, VALID_RESPONSE_CODE)
+
