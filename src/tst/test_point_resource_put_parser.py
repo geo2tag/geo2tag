568cf5013798e18f72d997e1a899242dce09fca2
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-29 19:41:38 +0300

    GT-1318

--- src/tst/test_point_resource_put_parser.py
+++ src/tst/test_point_resource_put_parser.py
@@ -3,28 +3,28 @@
 
 from unittest import TestCase
 from flask import Flask, request
-from werkzeug.exceptions import BadRequest, NotFound
+from werkzeug.exceptions import BadRequest
 import urllib2
 import sys
 sys.path.append('../')
 import service_resource
 from json import dumps
 from point_resource_parsers import PointResourceParsers
+from point_does_not_exist import PointDoesNotExist
 
 URL = '/testservice/point/552833515c0dd1178d37f7bb/'
-BAD_URL = '/testservice/point/111133515c0dd1178d37f711/'
+BAD_URL = '/testservice/point/point_id/'
 LAT = 55.24
-LAT_STR = '55.334'
 JSON = "{'1': 'a', '2': 'b'}"
-JSON_INT = 13
-_ALT_LIST = [1,2,3,4,5,5]
+ALT = 1.34
+LON = 3.54
 _LAT = 'lat'
 _LON = 'lon'
 _ALT = 'alt'
 _JSON = 'json'
 _CHANNEL_ID = 'channel_id'
-CORRECT_ARGS = {_LAT: LAT, _JSON: JSON}
-INCORRECT_ARGS = {_LAT: LAT_STR, _JSON: JSON_INT, '_ALT': _ALT_LIST}
+CORRECT_ARGS = {_LAT: LAT, _JSON: JSON, _LON: LON, _ALT: ALT}
+INCORRECT_ARGS = {_LAT: True, _JSON: JSON, _ALT: [1,2,3]}
 
 app = Flask(__name__)
 
@@ -32,10 +32,12 @@ class test_GT_1318_Point_Parser(TestCase):
     def test_GT_1318_Point_Parser(self):
 
         with app.test_request_context(BAD_URL, data=INCORRECT_ARGS, method='PUT'):
-            with self.assertRaises(NotFound):
+            with self.assertRaises(BadRequest):
                 args = PointResourceParsers.parsePutParameters()
 
         with app.test_request_context(URL, data=CORRECT_ARGS, method='PUT'):
             args = PointResourceParsers.parsePutParameters()
             self.assertEqual(args[_LAT], LAT)
             self.assertEqual(args[_JSON], JSON)
+            self.assertEqual(args[_LON], LON)
+            self.assertEqual(args[_ALT], ALT)
