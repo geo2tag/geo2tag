2ba1d6182da4cdcf985a6065adca7c17d9794a9d
ilyaolchikov <ilyaolchikov@mail.ru>
2015-06-30 23:43:50 +0300

    GT-1331

--- src/point_list_resource_parser.py
+++ src/point_list_resource_parser.py
@@ -1,6 +1,7 @@
 from flask_restful import reqparse
+import log_resource
 
-CHANNELS_IDS = 'channel_ids'
+CHANNEL_IDS = 'channel_ids'
 NUMBER = 'number'
 GEOMETRY = 'geometry'
 ALTITUDE_FROM = 'altitude_from'
@@ -15,14 +16,14 @@ class PointListResourceParser():
     @staticmethod
     def parseGetParameters():
         parser = reqparse.RequestParser()
-        parser.add_argument(CHANNELS_IDS, type=list, required=True)
+        parser.add_argument(CHANNEL_IDS, type=str, required=True)
         parser.add_argument(NUMBER, type=int, required=True)
         parser.add_argument(GEOMETRY, type=str)
         parser.add_argument(ALTITUDE_FROM, type=float)
         parser.add_argument(ALTITUDE_TO, type=float)
         parser.add_argument(SUBSTRING, type=str)
-        parser.add_argument(DATE_FROM, type=int)
-        parser.add_argument(DATE_TO, type=int)
+        parser.add_argument(DATE_FROM, type=log_resource.datetime_from_iso8601)
+        parser.add_argument(DATE_TO, type=log_resource.datetime_from_iso8601)
         parser.add_argument(OFFSET, type=int)
         parser.add_argument(RADIUS, type=float, default=1000)
         args = parser.parse_args()
---
+++ src/tst/test_point_list_resource_parser_get.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+from datetime import datetime, date, time
+from flask import Flask, request
+from calendar import timegm
+import json
+import aniso8601
+import pytz
+from werkzeug.exceptions import BadRequest
+
+import sys
+sys.path.append('../')
+import log_resource
+from point_list_resource_parser import PointListResourceParser
+from geojson import MultiPoint
+
+NUMBER = 'number'
+NUMBER_VALUE = 0
+OFFSET = 'offset'
+OFFSET_VALUE = 0
+DATE_FROM = 'date_from'
+DATE_FROM_VALUE = '1970-06-15T18:00:00'
+DATE_TO = 'date_to'
+DATE_TO_VALUE = '2015-06-15T17:00:00'
+CHANNEL_IDS = 'channel_ids'
+CHANNEL_IDS_VALUE = 'channel_ids_value'
+GEOMETRY = 'geometry'
+GEOMETRY_VALUE_LIST = [(-155.52, 19.61), (-156.22, 20.74), (-157.97, 21.46)]
+GEOMETRY_VALUE_STR = '[(-155.52, 19.61), (-156.22, 20.74), (-157.97, 21.46)]'
+
+CORRECT_ARGS = NUMBER+'='+str(NUMBER_VALUE)+'&'+OFFSET+'='+str(OFFSET_VALUE)+'&'+DATE_FROM+'='+str(DATE_FROM_VALUE)+'&'+DATE_TO+'='+str(DATE_TO_VALUE)+'&'+CHANNEL_IDS+'='+CHANNEL_IDS_VALUE+'&'+GEOMETRY+'='+GEOMETRY_VALUE_STR
+INCORRECT_ARGS='incorect='
+app = Flask(__name__)
+class TestParserPointListGetResource(TestCase):
+    def testParserPointListGetResource(self):
+        with app.test_request_context('/instance/service/testservice/point/?' + CORRECT_ARGS):
+            args = PointListResourceParser.parseGetParameters()
+            print args
+            print CORRECT_ARGS
+            self.assertEquals(args[OFFSET], OFFSET_VALUE)
+            self.assertEquals(args[NUMBER], NUMBER_VALUE)
+            loadedDatetime_from = json.loads(args[DATE_FROM], object_hook = log_resource.datetimeDeserialiser(args))
+            self.assertEquals(loadedDatetime_from, DATE_FROM_VALUE)
+            loadedDatetime_to = json.loads(args[DATE_TO], object_hook = log_resource.datetimeDeserialiser(args))
+            self.assertEquals(loadedDatetime_to, DATE_TO_VALUE)
+
+
+        with app.test_request_context('/instance/service/testservice/point/?'+INCORRECT_ARGS):
+            with self.assertRaises(BadRequest):
+                args = PointListResourceParser.parseGetParameters()
