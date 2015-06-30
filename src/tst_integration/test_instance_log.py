7fc9c46e9b2435f0dd29dd75cdcb9c39072147c3
4ukuta <nikita@nikita-Lenovo-Z580>
2015-06-30 12:11:59 +0300

    GT-1235

--- src/tst_integration/test_instance_log.py
+++ src/tst_integration/test_instance_log.py
@@ -1,11 +1,12 @@
 import unittest
 import requests
 from basic_integration_test import BasicIntegrationTest
-from db_model import addService, removeService
 import json
 
 TEST_URL1 = "/instance/service/testservice/log?date_from='1983-01-22T08:00:00'"
 TEST_URL2 = "/instance/log?date_from='1983-01-22T08:00:00'"
+TEST_URL1_IV = "/instance/service/testservice/log?date_from='1111'"
+TEST_URL2_IV = "/instance/log?date_from='1111'"
 VALID_RESPONSE_CODE = 200
 INVALID_RESPONSE_CODE = 400
 
@@ -15,13 +16,23 @@ class TestInstanceLogRequest(BasicIntegrationTest):
         responseText = response.text
         responseJson = json.loads(responseText)
         responseCode = response.status_code
-        print responseCode
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
     def testInstanceLogRequest_URL2_VALID(self):
         response = requests.get(self.getUrl(TEST_URL2))
         responseText = response.text
         responseJson = json.loads(responseText)
         responseCode = response.status_code
-        print responseCode
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
+    def testInstanceLogRequest_URL1_INVALID(self):
+        response = requests.get(self.getUrl(TEST_URL1_IV))
+        responseText = response.text
+        responseJson = json.loads(responseText)
+        responseCode = response.status_code
+        self.assertEquals(responseCode, INVALID_RESPONSE_CODE)
+    def testInstanceLogRequest_URL2_INVALID(self):
+        response = requests.get(self.getUrl(TEST_URL2_IV))
+        responseText = response.text
+        responseJson = json.loads(responseText)
+        responseCode = response.status_code
+        self.assertEquals(responseCode, INVALID_RESPONSE_CODE)
 
