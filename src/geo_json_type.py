d97916b2dc849458450f44782719d8608fd2cf21
ilyaolchikov <ilyaolchikov@mail.ru>
2015-07-01 20:37:02 +0300

    GT-1333

--- src/geo_json_type.py
+++ src/geo_json_type.py
@@ -1,9 +1,10 @@
 import geojson
 
 def GeoJsonType(obj):
+    TYPES = ['Point', 'Polygon', 'MultiPolygon']
     jsonObj = geojson.loads(obj)
     if len(jsonObj.keys()) == 2 and 'coordinates' in jsonObj.keys() and isinstance(jsonObj.get('coordinates'), list):
-        if jsonObj.get('type') == 'Point' or jsonObj.get('type') == 'Polygon' or jsonObj.get('type') == 'MultiPolygon':
+        if jsonObj.get('type') in TYPES:
             return jsonObj
         else:
             raise ValueError
