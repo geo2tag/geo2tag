15fe46f64140a266c34e7ea1c8c3562d475f7cc1
Tatyana Berlenko <tank.berlin@gmail.com>
2015-07-02 15:30:03 +0200

    GT-1323

--- src/db_model.py
+++ src/db_model.py
@@ -45,7 +45,7 @@ def possibleException(func):
     def funcPossibleException(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except Exception as e:
+        except BaseException as e:
             if hasattr(e, 'getReturnObject'):
                 return e.getReturnObject()
             else:
