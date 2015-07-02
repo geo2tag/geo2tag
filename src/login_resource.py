dc1720e4e9190afd043787ddba15aec44585d5ed
ilyaolchikov <ilyaolchikov@mail.ru>
2015-07-02 15:54:34 +0300

    GT-1340

--- src/login_resource.py
+++ src/login_resource.py
@@ -1,7 +1,7 @@
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from flask import render_template
-
+from flask import make_response
 class LoginResource(Resource):
     def get(self):
-        return render_template('login.html')
+        return make_response(render_template('login.html'))
