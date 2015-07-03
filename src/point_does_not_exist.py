f608cdc66b86c3784444a2c299cff599248bd77d
Tatyana Berlenko <tank.berlin@gmail.com>
2015-07-03 16:54:09 +0200

    GT-1323

--- src/point_does_not_exist.py
+++ src/point_does_not_exist.py
@@ -1,4 +1,6 @@
-class PointDoesNotExist(Exception):
+from base_exception import BaseException
+
+class PointDoesNotExist(BaseException):
     def getReturnObject(self):
         ERROR = "Point does not exist"
         return ERROR, 404
