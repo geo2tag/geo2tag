30e0d7d827d47ec9db9eab3b75001d2b004f0f31
ilyaolchikov <ilyaolchikov@mail.ru>
2015-07-06 22:18:05 +0300

    GT-1340

--- src/login_resource.py
+++ src/login_resource.py
@@ -4,4 +4,4 @@ from flask import render_template
 from flask import make_response
 class LoginResource(Resource):
     def get(self):
-        return make_response(render_template('login.html'))
+        return make_response(render_template('login.html', instance_prefix='instance'))
