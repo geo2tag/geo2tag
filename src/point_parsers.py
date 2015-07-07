137859ba4f02676276f173cb1863efe990d53465
Max_Borisov <MaxBorisov33@mail.ru>
2015-07-08 02:12:34 +0300

    GT-1301

--- src/point_parsers.py
+++ src/point_parsers.py
@@ -9,17 +9,20 @@ class PointParser():
         return args
 
 def validatePointsList(json):
-	if type(json) != type([]):
-		raise ValueError
-	for obj in json:
-		print obj.keys()
-		if not ('lon' in obj.keys() and 'lat' in obj.keys() and 'alt' in obj.keys() and 'json' in obj.keys() and 'channel_id' in obj.keys()):
-			raise ValueError
-		else: 
-			if not ((type(obj['lat']) == type(1) or type(obj['lat']) == type(1.1)) and (type(obj['lon']) == type(1) or type(obj['lon']) == type(1.1))):
-				raise ValueError
-			if not (type(obj['alt']) == type(1) or type(obj['alt']) == type(1.1)):
-				raise ValueError
-			if not (type(obj['json']) == type({}) and type(obj['channel_id']) == type('')):
-				raise ValueError
-	return json
+    if type(json) != type([]):
+        raise ValueError
+    for obj in json:
+        if not ('lon' in obj.keys() and 'lat' in obj.keys() and 'alt' in obj.keys() and 'json' in obj.keys() and 'channel_id' in obj.keys()):
+            raise ValueError
+        else: 
+            if not (type(obj['lat']) == int or type(obj['lat']) == float):
+            	raise ValueError
+            if not (type(obj['lon']) == int or type(obj['lon']) == float):
+                raise ValueError
+            if not (type(obj['alt']) == int or type(obj['alt']) == float):
+                raise ValueError
+            if not type(obj['json']) == dict:
+            	raise ValueError
+            if not type(obj['channel_id']) == str:
+                raise ValueError
+    return json
