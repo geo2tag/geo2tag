538c2b878be552f2897f8e7d8c8ddbf224141296
4ukuta <nikita@nikita-Lenovo-Z580>
2015-07-08 23:09:39 +0300

    GT-1335

--- scripts/db/setupMasterDbTemplate.py
+++ scripts/db/setupMasterDbTemplate.py
@@ -1,21 +1,19 @@
 import os
+import sys
 from configparser import ConfigParser
 
+sys.path.append("src/")
+import config_reader
 
-
-CONFIG_PATH =  'config/config.ini'
-SECTION = 'main'
-OPTION_DBNAME = 'db_name'
 TEMPLATE_PATH = 'master_db_template/'
+db_name = None
+
+
 def import_db(db_name, template_path):
     str_forsh = 'scripts/db/mongo.sh -l -H localhost:27017 ' + db_name
     os.system(str_forsh)
-def getConfigParser():
-    config = ConfigParser()
-    config.read(CONFIG_PATH)
-    return config
 def getDbName():
-    return getConfigParser().get(SECTION,OPTION_DBNAME)
+    return config_reader.getDbName()
 def run():
     db_name = getDbName();
     import_db(db_name,TEMPLATE_PATH)
