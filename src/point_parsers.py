c192c745dedc1894d0f2478c56086d61f596ce4c
Max_Borisov <MaxBorisov33@mail.ru>
2015-07-08 18:53:09 +0300

    GT-1301

--- src/point_parsers.py
+++ src/point_parsers.py
@@ -9,20 +9,20 @@ class PointParser():
         return args
 
 def validatePointsList(json):
-    if type(json) != type([]):
-        raise ValueError
+    if type(json) != list:
+        raise ValueError('Value is not a list')
     for obj in json:
         if not ('lon' in obj.keys() and 'lat' in obj.keys() and 'alt' in obj.keys() and 'json' in obj.keys() and 'channel_id' in obj.keys()):
-            raise ValueError
+            raise ValueError('Incorrect keys')
         else: 
             if not (type(obj['lat']) == int or type(obj['lat']) == float):
-            	raise ValueError
+            	raise ValueError("'lat' - Incorrect type")
             if not (type(obj['lon']) == int or type(obj['lon']) == float):
-                raise ValueError
+                raise ValueError("'lon' - Incorrect type")
             if not (type(obj['alt']) == int or type(obj['alt']) == float):
-                raise ValueError
+                raise ValueError("'alt' - Incorrect type")
             if not type(obj['json']) == dict:
-            	raise ValueError
+            	raise ValueError("'json' - Incorrect type")
             if not type(obj['channel_id']) == str:
-                raise ValueError
+                raise ValueError("'channel_id' - Incorrect type")
     return json
