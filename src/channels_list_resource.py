0dec4f820065ed188bc9141d9da89fbbf5d5e0ed
ilyaolchikov <ilyaolchikov@mail.ru>
2015-07-08 17:33:01 +0300

    GT-1359

--- src/channels_list_resource.py
+++ src/channels_list_resource.py
@@ -1,3 +1,4 @@
+from possible_exception import possibleException
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from channels_list_parsers import ChannelsListResourceParser
@@ -14,16 +15,15 @@ STUB = 'STUB'
 NAME = 'name'
 
 class ChannelsListResource(Resource):
+    @possibleException
     def get(self, serviceName):
         parserResult = ChannelsListResourceParser.parseGetParameters()
         return getChannelsList(serviceName, parserResult.get(SUBSTRING, None), parserResult.get(NUMBER, None), parserResult.get(OFFSET, None))
 
+    @possibleException
     def post(self, serviceName):
         listArgs = ChannelsListResourceParser.parsePostParameters()
         try:
             obj = getChannelByName(serviceName, listArgs.get(NAME, None))
         except ChannelDoesNotExist as e:
             return addChannel(listArgs.get(NAME, None), listArgs.get(JSON, None), STUB, serviceName)
-        else:
-            e = ChannelAlreadyExists()
-            return e.getReturnObject()
--- src/point_list_resource.py
+++ src/point_list_resource.py
@@ -1,9 +1,8 @@
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from point_list_resource_parser import PointListResourceParser
+
 class PointListResource(Resource):
     def get(self, serviceName):
         newPoint = PointListResourceParser.parseGetParameters()
         return newPoint
-    def post(self):
-        pass
