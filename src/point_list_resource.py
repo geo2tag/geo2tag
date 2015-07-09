212399d7d9c2aa952be5b39bcf013ba5d7f4978d
ilyaolchikov <ilyaolchikov@mail.ru>
2015-07-09 22:39:43 +0300

    GT-1359

--- src/point_list_resource.py
+++ src/point_list_resource.py
@@ -1,8 +1,10 @@
+from possible_exception import possibleException
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from point_list_resource_parser import PointListResourceParser
 
 class PointListResource(Resource):
+    @possibleException
     def get(self, serviceName):
         newPoint = PointListResourceParser.parseGetParameters()
         return newPoint
