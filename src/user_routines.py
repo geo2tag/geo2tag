d99a1e723f82bb643ba46e13ae62ad24da82e120
galiaf95 <galiaf1995@gmail.com>
2015-07-11 01:24:27 +0300

    GT-1325 : Fixed unit test and fixed user_routines script

--- src/user_routines.py
+++ src/user_routines.py
@@ -12,8 +12,8 @@ USER_ID = 'user_id'
 def logUserIn(_id):
     session[USER_ID] = _id    
     writeInstanceLog(session[USER_ID], 'login')
+
 def logUserOut():
-    SESSION_VALUE = "Wasn't logged in"
     if USER_ID in session:
         SESSION_VALUE = session.pop(USER_ID)
-    writeInstanceLog(SESSION_VALUE, 'logout')
+        writeInstanceLog(SESSION_VALUE, 'logout')
