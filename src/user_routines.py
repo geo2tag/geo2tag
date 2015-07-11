ef2f684e578a3049eb370ece1f1070602e0945fd
galiaf95 <galiaf1995@gmail.com>
2015-07-11 17:17:43 +0300

    GT-1362 : Done

--- src/user_routines.py
+++ src/user_routines.py
@@ -1,13 +1,12 @@
-from pymongo import MongoClient
-from log import writeInstanceLog
-from config_reader import getDbName
 from flask import session
 
-#DB initialisation
-client = MongoClient()
-dbName = getDbName()
+from config_reader import getDbName
+from db_model import getDbObject
+from log import writeInstanceLog
+from user_does_not_exist import UserDoesNotExist
 
 USER_ID = 'user_id'
+COLLECTION_NAME_USERS = "users"
 
 def logUserIn(_id):
     session[USER_ID] = _id
@@ -17,3 +16,10 @@ def logUserOut(_id):
     if USER_ID in session:
         session.pop(USER_ID)
     writeInstanceLog(_id, 'logout')
+
+def findUserById(_id) :
+    collectionUsers = getDbObject(getDbName())[COLLECTION_NAME_USERS]
+    userById = collectionUsers.find_one({USER_ID : _id})
+    if userById != None :
+        return userById
+    raise UserDoesNotExist
