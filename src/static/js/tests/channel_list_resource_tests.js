53359d131e8a8b3c2fdf1d563342dfbfd4e488a9
4ukuta <nikita@nikita-Lenovo-Z580>
2015-07-17 18:22:02 +0300

    GT-1388

--- src/static/js/tests/channel_list_resource_tests.js
+++ src/static/js/tests/channel_list_resource_tests.js
@@ -1,19 +1,39 @@
 var channel_list_resource_tests = {
     'GET':{
+        data:{'number':2,'offset':1},
+        url : '/instance/service/testservice/channel'
+    },
+    'POST':{
+        data:{'name': 'tes t_name', 'json': "{'1': 2, '2': '4'}"},
         url : '/instance/service/testservice/channel'
     }
 };
-QUnit.test( channel_list_resource_tests.GET.url, function( assert ) {
+QUnit.test( 'GET ' + channel_list_resource_tests.GET.url + JSON.stringify(channel_list_resource_tests.GET.data), function( assert ) {
     var done = assert.async(); 
     var getCallbackFail = function() {
-        assert.ok(false, 'get failed' );
+        assert.ok(false, 'GET failed' );
         done();
     };
     var getCallbackSuccess = function() {
-        assert.ok(true, 'get success' );
+        assert.ok(true, 'GET success' );
         done();
     };
     $.get(channel_list_resource_tests.GET.url, channel_list_resource_tests.GET.data )
         .fail(getCallbackFail).done(getCallbackSuccess);
  
 });
+QUnit.test( 'POST ' + channel_list_resource_tests.POST.url + JSON.stringify(channel_list_resource_tests.POST.data), function( assert ) {
+    var done = assert.async(); 
+    var getCallbackFail = function() {
+        assert.ok(false, 'POST failed' );
+        done();
+    };
+    var getCallbackSuccess = function() {
+        assert.ok(true, 'POST success' );
+        done();
+    };
+    $.post(channel_list_resource_tests.POST.url, channel_list_resource_tests.POST.data )
+        .fail(getCallbackFail).done(getCallbackSuccess);
+ 
+});
+
