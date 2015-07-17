f4398016c67c3fb263b4b8bbe33b5f876ceb0508
ilyaolchikov <ilyaolchikov@mail.ru>
2015-07-17 13:33:42 +0300

    GT-1389. Edit tests

--- src/static/js/tests/channel_resource_tests.js
+++ src/static/js/tests/channel_resource_tests.js
@@ -3,55 +3,23 @@ var test_data_debug = {
         url : '/instance/service/testservice/channel/558807a47ec8ff5da755ee49'
     },
     'PUT':{
-        data: {'name': 'test_channel_GT-1290'},
+        data: {'name': 'test_channel_GT-1389_2'},
         url : '/instance/service/testservice/channel/558807a47ec8ff5da755ee49'
     },
     'DELETE':{
         url : '/instance/service/testservice/channel/558807a47ec8ff5da755ee49'
     }
 };
-$.put = function(url, data, callback, type){
- 
-  if ( $.isFunction(data) ){
-    type = type || callback,
-    callback = data,
-    data = {}
-  }
- 
-  return $.ajax({
-    url: url,
-    type: 'PUT',
-    success: callback,
-    data: data,
-    contentType: type
-  });
-}
-$.delete = function(url, data, callback, type){
- 
-  if ( $.isFunction(data) ){
-    type = type || callback,
-        callback = data,
-        data = {}
-  }
- 
-  return $.ajax({
-    url: url,
-    type: 'DELETE',
-    success: callback,
-    data: data,
-    contentType: type
-  });
-}
 
 QUnit.test( test_data_debug.PUT.url + JSON.stringify(test_data_debug.PUT.data),
     function( assert ) { 
-    var done = assert.async(); // Код необходим для того, чтобы делать в проверки в коллбэке
+    var done = assert.async();
     var putCallbackFail = function() {
-        assert.ok(false, 'put failed' );
+        assert.ok(false, 'ChannelResource put failed' );
         done();
     }; 
     var putCallbackSuccess = function() {
-        assert.ok(true, 'put success' );
+        assert.ok(true, 'ChannelResource put success' );
         done();
     };
     $.put(test_data_debug.PUT.url, test_data_debug.PUT.data )
@@ -59,31 +27,29 @@ QUnit.test( test_data_debug.PUT.url + JSON.stringify(test_data_debug.PUT.data),
     });
 
 QUnit.test( test_data_debug.GET.url, function( assert ) {
-    var done = assert.async(); // Код необходим для того, чтобы делать в проверки в коллбэке
+    var done = assert.async();
     var getCallbackFail = function() {
-        assert.ok(false, 'get failed' );
+        assert.ok(false, 'ChannelResource get failed' );
         done();
     };
     var getCallbackSuccess = function() {
-        assert.ok(true, 'get success' );
+        assert.ok(true, 'ChannelResource get success' );
         done();
     };
     $.get(test_data_debug.GET.url, test_data_debug.GET.data )
         .fail(getCallbackFail).done(getCallbackSuccess);
  
 });
-  
-// Запрос, требующий предварительной подготовки
+
 QUnit.test( test_data_debug.DELETE.url, function( assert ) {
     var done = assert.async();
     
     var deleteCallbackFail = function() {
-        // Подготовка данных не удалась - валим assert
-        assert.ok(false, 'delete failed' );
+        assert.ok(false, 'ChannelResource delete failed' );
         done();
     };
     var deleteCallbackSuccess = function() {
-        assert.ok(true, 'delete success' );
+        assert.ok(true, 'ChannelResource delete success' );
         done();
     };
     $.delete(test_data_debug.DELETE.url)
