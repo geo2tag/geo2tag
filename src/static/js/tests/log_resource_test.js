dd047f14dc7b436639dcf63bff670ef84f3a2f57
Tatyana Berlenko <tank.berlin@gmail.com>
2015-07-21 10:58:17 +0200

    GT-1397

--- src/static/js/tests/log_resource_test.js
+++ src/static/js/tests/log_resource_test.js
@@ -1,18 +1,18 @@
 var log_resource_test_data = { 'GET':{
         url : '/instance/log'
-    } ,
+    } 
 };
   
 var log_resource_serviceName_test_data = { 'GET':{
         url : '/instance/service/test_service_name/log'
-    } ,
+    } 
 };
 
 var data = {
-            'number' : 1,
-            'offset' : 1,
-            'date_from' : '1983-01-22T08:00:00',
-            'date_to' : '2014-01-22T08:00:00'
+    'number' : 1,
+    'offset' : 1,
+    'date_from' : '1983-01-22T08:00:00',
+    'date_to' : '2014-01-22T08:00:00'
 }
 
 QUnit.test('GET ' + log_resource_serviceName_test_data.GET.url + JSON.stringify(data), function( assert ) {
