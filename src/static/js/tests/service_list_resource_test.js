394d671f27d13ca62df98eabaa1f62fcbc8a857a
galiaf95 <galiaf1995@gmail.com>
2015-07-21 15:45:13 +0300

    Fixed test for instance/service/testservice (service resource) and added test for instance/service (serviceList resource)

---
+++ src/static/js/tests/service_list_resource_test.js
@@ -0,0 +1,54 @@
+var test_data_service_list_resource = {
+    'GET' : {
+        url : '/instance/service',
+        data : {
+            'number' : 0,
+            'offset' : 0
+        }
+    },
+    'POST' : {
+        url : '/instance/service',
+        data : {
+            'name' : 'serviceListResourceTestName',
+            'ownerId' : 'serviceListResourceTestOwnerId',
+            'logSize' : 10
+        }
+    }
+};
+
+
+QUnit.test( 'GET' + test_data_service_list_resource.GET.url + JSON.stringify(test_data_service_list_resource.GET.data), function( assert ) {
+    var done = assert.async();
+
+    var callbackFail = function() {
+        assert.ok(false, 'get failed' );
+        done();
+    };
+
+    var callbackOk = function() {
+        assert.ok(true, 'went ok' );
+        done();
+    };
+
+    $.get(test_data_service_list_resource.GET.url, test_data_service_list_resource.GET.data)
+        .fail( callbackFail )
+        .done( callbackOk )
+});
+
+QUnit.test( 'POST' + test_data_service_list_resource.POST.url + JSON.stringify(test_data_service_list_resource.POST.data), function( assert ) {
+    var done = assert.async();
+
+    var callbackFail = function() {
+        assert.ok(false, 'get failed' );
+        done();
+    };
+
+    var callbackOk = function() {
+        assert.ok(true, 'went ok' );
+        done();
+    };
+
+    $.post(test_data_service_list_resource.POST.url, test_data_service_list_resource.POST.data)
+        .fail( callbackFail )
+        .done( callbackOk )
+});
