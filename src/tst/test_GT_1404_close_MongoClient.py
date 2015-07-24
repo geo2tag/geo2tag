f69f64d7253e5f0986f4c34b9223917ad2f2e0c5
Rostislav Ivanov <kitemeall@gmail.com>
2015-07-24 20:48:21 +0300

    GT-1404

--- src/tst/test_GT_1404_close_MongoClient.py
+++ src/tst/test_GT_1404_close_MongoClient.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
-from db_model import closeConnection, MONGO_CLIENT, getClientObject
-
+from db_model import closeConnection, getClientObject
+getClientObject()
+from db_model import MONGO_CLIENT
 class test_GT_1404Close_MongoClient(TestCase):
-    def test_Close_MongoClient(self):
-        getClientObject()
+    def test_Close_MongoClient(self):        
         self.assertEquals(MONGO_CLIENT.is_primary, True)
         closeConnection() 
         self.assertEquals(MONGO_CLIENT.is_primary, False) 
