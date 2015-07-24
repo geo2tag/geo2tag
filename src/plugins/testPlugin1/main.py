20f2b17214236a77a052226b9ba314d65ce31045
ilyaolchikov <ilyaolchikov@mail.ru>
2015-07-24 20:24:17 +0300

    GT-1416. Done

--- src/plugins/testPlugin1/main.py
+++ src/plugins/testPlugin1/main.py
@@ -4,7 +4,6 @@ from url_utils import getPathWithPrefix
 from testResource_GT_1416 import Resource_GT_1416
 
 def getPluginResources():
-    print '1111111111'
     return [{'/service/GT_1416': Resource_GT_1416}]
 
 def getPluginInfo():
