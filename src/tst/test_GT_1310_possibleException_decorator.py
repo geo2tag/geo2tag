bfeb12a29c10dd0cc03753cfa4a4e7cdedd7990c
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-07-26 18:22:50 +0300

    GT-1294

--- src/tst/test_GT_1310_possibleException_decorator.py
+++ src/tst/test_GT_1310_possibleException_decorator.py
@@ -6,7 +6,7 @@ sys.path.append('../')
 
 TEST_VALID_EXCEPTION = ServiceNotFoundException
 TEST_NOT_VALID_EXCEPTION = ArithmeticError
-TEST_VALID_TUPLE = ('Service not found', 400)
+TEST_VALID_TUPLE = ('Service not found', 404)
 TEST_NOT_VALID_EXCEPTION_STRING = 'This is not valid exception'
 
 @possibleException
@@ -17,5 +17,5 @@ class TestPossibleExceptionDecorator(TestCase):
     def testPossibleExceptionDecorator(self):
         self.assertEqual(funcForTesting(TEST_VALID_EXCEPTION), TEST_VALID_TUPLE)
         with self.assertRaises(TEST_NOT_VALID_EXCEPTION) as context:
-            funcForTesting(TEST_NOT_VALID_EXCEPTION)
+        funcForTesting(TEST_NOT_VALID_EXCEPTION)
         self.assertTrue(TEST_NOT_VALID_EXCEPTION_STRING in context.exception)        
