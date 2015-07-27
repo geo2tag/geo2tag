0efce816045e45844b4dfb471e3c749baf42b5a2
Tatyana Berlenko <tank.berlin@gmail.com>
2015-07-27 14:04:12 +0200

    GT-1418

--- src/test_resource_1.py
+++
@@ -1,6 +0,0 @@
-from flask import request
-from flask.ext.restful import Resource
-
-class TestResource1(Resource):
-    def get(self):
-        return 'test_resource_1'
--- src/test_resource_2.py
+++
@@ -1,6 +0,0 @@
-from flask import request
-from flask.ext.restful import Resource
-
-class TestResource2(Resource):
-    def get(self):
-        return 'test_resource_2'
