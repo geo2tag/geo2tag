91dc53d881e583287f92cc062b80c7e2e0999901
Tatyana Berlenko <tank.berlin@gmail.com>
2015-07-27 15:33:10 +0200

    GT-1419

--- src/tst/test_url_routines.py
+++ src/tst/test_url_routines.py
@@ -8,7 +8,7 @@ TEST_PLUGIN = 'test_plugin'
 VALID_RESULT = '/instance/plugins/test_plugin/test_url'
 
 class TestUrlRoutines(unittest.TestCase):
-    def testUrlRoutines(self):
+    def testGetPluginUrl(self):
         RESULT = getPluginUrl(TEST_URL, TEST_PLUGIN)
         self.assertTrue(VALID_RESULT, RESULT)
 
