3887e8c63712b3ae72c25c1fcaa7051eeb2838f2
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-07-28 13:10:09 +0300

    plugin path corrected

--- src/url_routines.py
+++ src/url_routines.py
@@ -1,3 +1,3 @@
 from config_reader import getInstancePrefix
 def getPluginUrl(url, pluginName):
-    return '/' + getInstancePrefix() + '/plugins/' + pluginName + '/' + url
+    return '/' + getInstancePrefix() + '/plugin/' + pluginName + '/' + url
