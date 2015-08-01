fdb7acd3ea1e7530b07d94ba3c2302f6c265d801
ilyaolchikov <ilyaolchikov@mail.ru>
2015-08-01 14:49:52 +0300

    GT-1442

--- src/manage_plugins.py
+++ src/manage_plugins.py
@@ -1,6 +1,7 @@
 from flask_restful import reqparse
 from flask.ext.restful import Resource
 from possible_exception import possibleException
+from manage_plugins_parser import ManagePluginsParser
 
 class ManagePluginsResource(Resource):
     @possibleException
