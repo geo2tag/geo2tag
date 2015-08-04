2e8be87d591a50ab252be55a8bce21755d930d08
Max_Borisov <MaxBorisov33@mail.ru>
2015-08-04 15:25:49 +0300

    GT-1467

--- src/static/js/geo2tag.js
+++
@@ -1,9 +0,0 @@
-function Geo2TagRequests(server, instance) {
-	this.server = server;
-	this.instance = instance;
-
-	this.getChannels = function(serviceName, callback, substring, number, offset) {
-		$.get(getUrlWithPrefix('/service/') + serviceName + '/channel', {'substring': substring, 'number': number, 'offset': offset})
-        .done(callback);
-	};
-};
