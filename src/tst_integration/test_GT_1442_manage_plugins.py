c82a604d0dacc215de2d5b0aa079af0e4bf4a867
Tatyana Berlenko <tank.berlin@gmail.com>
2015-08-04 11:53:00 +0200

    GT-1442

--- src/tst_integration/test_GT_1442_manage_plugins.py
+++ src/tst_integration/test_GT_1442_manage_plugins.py
@@ -3,11 +3,12 @@ import unittest
 import requests
 from basic_integration_test import BasicIntegrationTest
 
-TEST_URL = '/instance/manage_plugins?test_plugin=True&testPlugin1=False'
-TEST_URL2 = '/instance/manage_plugins?test_plugin=True&testPlugin=False'
+TEST_URL = '/instance/manage_plugins?test_plugin=False&testPlugin1=False'
+TEST_URL2 = '/instance/manage_plugins?test_plugin_not_valid_name=True'
 VALID_TEXT = 'null'
 VALID_RESPONSE_CODE = 200
-NOT_VALID_RESPONSE_CODE = 400
+NOT_VALID_RESPONSE_CODE = 500
+
 class Test_GT_1442_managePlugins(BasicIntegrationTest):
     def test_GT_1442_managePlugins(self):
         response = requests.get(self.getUrl(TEST_URL))
@@ -16,7 +17,5 @@ class Test_GT_1442_managePlugins(BasicIntegrationTest):
         self.assertEquals(VALID_TEXT, responseText)
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
         response = requests.get(self.getUrl(TEST_URL2))
-        responseText = response.text
         responseCode = response.status_code
-        self.assertEquals(VALID_TEXT, responseText)
         self.assertEquals(responseCode, NOT_VALID_RESPONSE_CODE)
