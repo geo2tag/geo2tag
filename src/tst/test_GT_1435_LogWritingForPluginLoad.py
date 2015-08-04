66a740c9e6400a5a53cc68876b8a123e1362e836
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-08-04 15:05:56 +0300

    GT-1435: print removed from src/tst/test_GT_1435_LogWritingForPluginLoad.py

--- src/tst/test_GT_1435_LogWritingForPluginLoad.py
+++ src/tst/test_GT_1435_LogWritingForPluginLoad.py
@@ -37,13 +37,11 @@ class TestLogWritingForPluginLoad(TestCase):
     def testLogWritingForPluginLoadDone(self):
         enablePlugin(api, PLUGIN_DONE_PLUGIN)
         last_log_document = db[COLLECTION_LOG].find().sort(ID, pymongo.DESCENDING).limit(1)
-        print last_log_document[0]
         self.assertNotEqual(last_log_document[0][FIELD_MESSAGE].find(MESSAGE_LOAD_DONE), -1)
         self.assertEqual(last_log_document[0][FIELD_USERID], LOG_USERID)
     def testLogWritingForPluginLoadFail(self):
         enablePlugin(api, PLUGIN_FAIL_PLUGIN)
         last_log_document = db[COLLECTION_LOG].find().sort(ID, pymongo.DESCENDING).limit(1)
-        print last_log_document[0]
         self.assertNotEqual(last_log_document[0][FIELD_MESSAGE].find(MESSAGE_LOAD_FAIL), -1)
         self.assertEqual(last_log_document[0][FIELD_USERID], LOG_USERID)
 
