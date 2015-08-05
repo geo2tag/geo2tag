6c75a15edeeda0f0873a315c64293e9d76805aab
Tatyana Berlenko <tank.berlin@gmail.com>
2015-08-05 17:55:21 +0200

    fix bug datetime_from_iso8601 import

--- src/point_list_resource_parser.py
+++ src/point_list_resource_parser.py
@@ -2,6 +2,7 @@ from flask_restful import reqparse
 import geo_json_type
 from flask import request
 from date_utils import dateDeserialiser
+from date_utils import datetime_from_iso8601
 
 CHANNEL_IDS = 'channel_ids'
 NUMBER = 'number'
