7aa190ca38227a8fa322e8bda8ff67faceba0b90
Max_Borisov <MaxBorisov33@mail.ru>
2015-08-05 13:34:48 +0300

    GT-1471

--- src/static/js/geo2tag.js
+++ src/static/js/geo2tag.js
@@ -2,16 +2,16 @@ function Geo2TagRequests(server, instance) {
 	this.server = server;
 	this.instance = instance;
 
-	this.getChannels = function(serviceName, callback, substring, number, offset) {
+	this.getChannels = function(serviceName, callbacksuccess, callbackfail, substring, number, offset) {
 		$.get(getUrlWithPrefix('/service/') + serviceName + '/channel', {'substring': substring, 'number': number, 'offset': offset})
-        .done(callback(data));
+			.fail(callbackfail).done(function(){ callbacksuccess(data) });
 	};
 
-	this.getPoints = function(serviceName, callback, channel_ids, number, geometry, altitude_from, 
+	this.getPoints = function(serviceName, callbacksuccess, callbackfail, channel_ids, number, geometry, altitude_from, 
 		                      altitude_to, substring, date_from, date_to, offset, radius) {
 		$.get(getUrlWithPrefix('/service/') + serviceName + '/point', {'channel_ids': channel_ids, 'number': number, 
 			                  'geometry': geometry, 'altitude_from': altitude_from, 'altitude_to': altitude_to, 'substring': substring,
 			                  'date_from': date_from, 'date_to': date_to, 'offset': offset, 'radius': radius})
-        .done(callback(data));
+        	.fail(callbackfail).done(function(){ callbacksuccess(data) });
 	};
 };
--- src/static/js/tests/geo2tag_requests_tests.js
+++ src/static/js/tests/geo2tag_requests_tests.js
@@ -1,10 +1,31 @@
 QUnit.test( 'Get channels test',
     function( assert ) {
     	var done = assert.async();
-    	var testCallback = function (date) {
+    	var callbackSuccess = function (data) {
     		assert.ok(true);
+    		done();
+    	};
+    	var callbackFail = function () {
+    		assert.ok(false);
+    		done();
     	};
 
     	var test = new Geo2TagRequests('test', 'test');
-    	test.getChannels('testservice', testCallback);
+    	test.getChannels('testservice', callbackSuccess, callbackFail);
+    }); 
+
+QUnit.test( 'Get points test',
+    function( assert ) {
+    	var done = assert.async();
+    	var callbackSuccess = function (data) {
+    		assert.ok(true);
+    		done();
+    	};
+    	var callbackFail = function () {
+    		assert.ok(false);
+    		done();
+    	};
+
+    	var test = new Geo2TagRequests('test', 'test');
+    	test.getPoints('testservice', callbackSuccess, callbackFail, '556721a52a2e7febd2744201', 5);
     }); 
