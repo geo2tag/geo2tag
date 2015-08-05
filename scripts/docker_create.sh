a76c937ed45ac753d8cae000c91fab6267cc9ae8
Vladislav <farmatholin@gmail.com>
2015-08-05 15:57:32 +0300

    GT-1427

--- scripts/docker_create.sh
+++ scripts/docker_create.sh
@@ -6,7 +6,7 @@ reset=$(tput sgr0)
 toend=$(tput hpa $(tput cols))$(tput cub 6)
 
 if [[ $# -eq 4 && "$1" == "-p" && "$3" == "-n" ]]; then
- res = $(docker run -d -p $2:80 -v `pwd`/src:/app/src -v `pwd`/scripts:/app/scripts -v `pwd`/config:/app/config -v `pwd`/master_db_template:/app/master_db_template -v `pwd`/testdump:/app/testdump --name $4 osll/geomongo 2>&1)
+ res=$(docker run -d -p $2:80 -v `pwd`/src:/app/src -v `pwd`/scripts:/app/scripts -v `pwd`/config:/app/config -v `pwd`/master_db_template:/app/master_db_template -v `pwd`/testdump:/app/testdump --name $4 osll/geomongo 2>&1)
 
   if [[ $res =~ "Error" || $res =~ "Invalid" ]]; then
 	echo -n "${res}"
