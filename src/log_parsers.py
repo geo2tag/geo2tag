d9109c65461ca130e543533f346235208a97b574
Tatyana Berlenko <tank.berlin@gmail.com>
2015-08-06 16:02:15 +0200

    GT-1445

--- src/log_parsers.py
+++ src/log_parsers.py
@@ -1,7 +1,8 @@
 from flask_restful import reqparse
 from datetime import datetime, date, time
 from calendar import timegm
-import date_utils, pymongo
+import date_utils,
+import pymongo
 import json
 import aniso8601
 import pytz
