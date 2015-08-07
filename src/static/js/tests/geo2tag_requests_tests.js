7b01de7e20e50821a341532d58e982cb5da3d703
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-08-07 19:54:08 +0300

    GT-1471

--- src/static/js/tests/geo2tag_requests_tests.js
+++ src/static/js/tests/geo2tag_requests_tests.js
@@ -1,16 +1,15 @@
 QUnit.test( 'Get channels test',
     function( assert ) {
-    	var done = assert.async();
+        var done = assert.async();
     	var callbackSuccess = function (data) {
-    		assert.ok(true);
-    		done();
+    	    assert.ok(true);
+    	    done();
     	};
     	var callbackFail = function () {
-    		assert.ok(false);
-    		done();
+    	    assert.ok(false);
+    	    done();
     	};
-
-    	var test = new Geo2TagRequests('test', 'test');
+        var test = new Geo2TagRequests('test', 'test');
     	test.getChannels('testservice', callbackSuccess, callbackFail);
     }); 
 
@@ -18,12 +17,12 @@ QUnit.test( 'Get points test',
     function( assert ) {
     	var done = assert.async();
     	var callbackSuccess = function (data) {
-    		assert.ok(true);
-    		done();
+            assert.ok(true);
+    	    done();
     	};
     	var callbackFail = function () {
-    		assert.ok(false);
-    		done();
+    	    assert.ok(false);
+    	    done();
     	};
     	var test = new Geo2TagRequests('test', 'test');
         var channel_ids_list = ["556721a52a2e7febd2744201","556721a52a2e7febd2744202"];
