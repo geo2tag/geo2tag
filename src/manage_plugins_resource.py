78c0ffaedda3f1c85f9bc1fc9af2ce1798fa2c58
ilyaolchikov <ilyaolchikov@mail.ru>
2015-08-08 16:30:45 +0300

    GT-1489. Standart pep8

--- src/manage_plugins_resource.py
+++ src/manage_plugins_resource.py
@@ -6,12 +6,15 @@ from db_model import setPluginState
 from plugin_routines import isPluginEnabled, enablePlugin
 from plugin_does_not_exist_exception import PluginDoesNotExistException
 import os
+
+
 class ManagePluginsResource(Resource):
+
     @possibleException
     def get(self):
         from main import app, getApi
-        pluginsDict = dict((key, request.args.get(key)) 
-                            for key in request.args.keys())
+        pluginsDict = dict((key, request.args.get(key))
+                           for key in request.args.keys())
         print pluginsDict
         for plugin in pluginsDict:
             setPluginState(plugin, pluginsDict[plugin])
