a9b8278ee0dc7b6f537fed00ed1bee20b3660183
Vladislav <farmatholin@gmail.com>
2015-08-08 19:05:44 +0300

    GT-1428

--- scripts/pdocker.py
+++ scripts/pdocker.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 import argparse
 from pymongo import MongoClient
-import pymongo
-from pymongo import Connection
+from subprocess import Popen, PIPE
 
 HOST = 'localhost'
 PORT = 27017
 DBNAME = 'docker-client'
 COLLECTION_NAME = "ports"
 db = MongoClient(HOST, PORT)[DBNAME]
+# keys
+CONTAINER_NAME = "name"
+CONTAINER_PORT = "port"
 
 
 def usage():
@@ -16,10 +18,20 @@ def usage():
 
 
 def start_container(name, port):
-    return 1
+    child = Popen(["scripts/docker_create.sh", '-p', str(port), '-n', name], stdout=PIPE, stderr=PIPE)
+    output = child.stdout.read()
+    err = child.stderr.read()
+    streamdata = child.communicate()[0]
+    rc = child.returncode
+    print output, err
+    print rc
 
 
 def stop_container(name):
+    return 1
+
+
+def run_tests(name):
     return 1
 
 
@@ -33,10 +45,19 @@ def main():
         usage()
     else:
         collection = db[COLLECTION_NAME]
-        ports_range = args.ports.split('-')
-        for i in range(int(ports_range[0]), int(ports_range[1]) + 1):
-            collection.save(dict(name=args.name, port=i))
-            break
+        container = collection.find_one({CONTAINER_NAME: args.name})
+        if container is None:
+            ports_range = args.ports.split('-')
+            for i in range(int(ports_range[0]), int(ports_range[1]) + 1):
+                container_on_port = collection.find_one({CONTAINER_PORT: i})
+                if container_on_port is None:
+                    collection.save({CONTAINER_NAME: args.name, CONTAINER_PORT: i})
+                    start_container(args.name, i)
+                break
+        else:
+            stop_container(args.name)
+            start_container(args.name, container[CONTAINER_PORT])
+    return 0
 
 
 if __name__ == "__main__":
