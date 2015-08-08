092a323899b823e8e37bfdb8174c98d93b5f4d05
ilyaolchikov <ilyaolchikov@mail.ru>
2015-08-08 16:24:28 +0300

    GT-1489. Done

--- src/manage_plugins_resource.py
+++ src/manage_plugins_resource.py
@@ -5,16 +5,20 @@ from flask import request
 from db_model import setPluginState
 from plugin_routines import isPluginEnabled, enablePlugin
 from plugin_does_not_exist_exception import PluginDoesNotExistException
+import os
 class ManagePluginsResource(Resource):
     @possibleException
     def get(self):
         from main import app, getApi
         pluginsDict = dict((key, request.args.get(key)) 
                             for key in request.args.keys())
+        print pluginsDict
         for plugin in pluginsDict:
             setPluginState(plugin, pluginsDict[plugin])
             if pluginsDict[plugin].lower() == u'true' and isPluginEnabled(
                     plugin, app) == False:
-                enablePlugin(getApi(), plugin)
-            else:
-                raise PluginDoesNotExistException(plugin)
+                root, dirs, files = os.walk('plugins').next()
+                if plugin not in dirs:
+                    raise PluginDoesNotExistException(plugin)
+                else:
+                    enablePlugin(getApi(), plugin)
