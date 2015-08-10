ade6bffc4d7214256979393557305a9c27da5cf5
Max_Borisov <MaxBorisov33@mail.ru>
2015-08-10 14:25:29 +0300

    GT-1498

--- src/db_model.py
+++ src/db_model.py
@@ -73,19 +73,6 @@ def addLogEntry(dbName, userId, message, service='instance'):
     else:
         collection.save({USER_ID: userId, DATE: currentDate, MESSAGE: message})
 
-
-def possibleException(func):
-    def funcPossibleException(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except BaseException as e:
-            if hasattr(e, 'getReturnObject'):
-                return e.getReturnObject()
-            else:
-                raise
-    return funcPossibleException
-
-
 def addTag(tag):
     getDbObject()[TAGS].insert(tag)
 
