dda4049e31e26e36d2205d05d97804ffe93eb01a
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-08-11 10:15:16 +0300

    GT-1504: removed old and strange code

--- src/plugins/ok_import/job_resource.py
+++ src/plugins/ok_import/job_resource.py
@@ -1,11 +1,10 @@
 from possible_exception import possibleException
 from flask_restful import reqparse
 from flask.ext.restful import Resource
-from job_manager_class import JobManager
+from job_manager import JobManager
 from ok_import_resource_parser import OKImportParser
-from thread_job_class import ThreadJob
-def f(): 
-    pass
+from thread_job import ThreadJob
+
 class JobResource(Resource):
 
     @possibleException
@@ -15,5 +14,10 @@ class JobResource(Resource):
     @possibleException
     def post(self, serviceName):
         job = OKImportParser.parsePostParameters()
-        thread = ThreadJob(f, job.get('channelName'), job.get('openDataUrl'), job.get('showObjectUrl'), job.get('showImageUrl'), serviceName)
+        thread = ThreadJob(f, job.get('channelName'), \
+                           job.get('openDataUrl'), \
+                           job.get('showObjectUrl'), \
+                           job.get('showImageUrl'), \
+                           serviceName)
+
         return JobManager.startJob(thread)
