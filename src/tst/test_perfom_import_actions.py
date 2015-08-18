f1f2c3453e61106553b3addcaf0189807b6b8676
Tatyana Berlenko <tank.berlin@gmail.com>
2015-08-18 12:57:27 +0200

    GT-1508

--- src/tst/test_perfom_import_actions.py
+++ src/tst/test_perfom_import_actions.py
@@ -7,13 +7,13 @@ from db_model import  getDbObject
 class testOdLoaderClass():
     def __init__(self, testOpenDataUrl):
         self.openDataUrl = testOpenDataUrl
-    def load():
+    def load(self):
         return "OK"
 
 class testOdParserClass():
     def __init__(self, testOpenData):
         self.openData = testOpenData
-    def parse():
+    def parse(self):
         return "OK"
 
 class testOdToPointTranslatorClass():
@@ -25,37 +25,28 @@ class testOdToPointTranslatorClass():
         self.version = testVersion
         self.openDataUrl = testOpenDataUrl
         self.channelId = testChannelId
-    def getPoint():
+    def getPoint(self):
         return "OK"
 
 class odToPointsLoaderClass():
     def __init__(self, testServiceName, testPoints):
         self.serviceName = testServiceName
         self.points = testPoints
-    def loadPoints():
+    def loadPoints(self):
         return "OK"
 
 
 def callBack(self):
     self.done = True
 
-DB = "testservice"
-COLLECTION = "channels"
-ID = "_id"
-NAME = 'name'
 TEST_SERVICE_NAME = 'testservice'
-TEST_CHANNEL_NAME = 'test_GT_1508'
+TEST_CHANNEL_NAME = u'test_channel_1'
 TEST_OPEN_DATA_URL = 'test_open_data_url'
 TEST_SHOW_OBJECT_URL = 'test_show_object_url'
 TEST_SHOW_IMAGE_URL = 'test_show_image_url'
-TEST_SERVICE_NAME = 'test_service_name'
 
 class TestPerfomImportActions(unittest.TestCase):
     def testPerfomImportActions(self):
-        db = getDbObject(DB)
-        obj_id = db[COLLECTION].save({NAME: TEST_SERVICE_NAME})
-        print "----------------"
-        print obj_id
         performImportActions(self, callBack, testOdLoaderClass, testOdParserClass, testOdToPointTranslatorClass, \
             odToPointsLoaderClass, TEST_CHANNEL_NAME, TEST_OPEN_DATA_URL, \
             TEST_SHOW_OBJECT_URL, TEST_SHOW_IMAGE_URL, TEST_SERVICE_NAME)
