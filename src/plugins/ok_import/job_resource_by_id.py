563bc3e188c74c38262dcd00a92305b9d6d00412
ilyaolchikov <ilyaolchikov@mail.ru>
2015-08-18 12:35:38 +0300

    GT-1511. Fixed

--- src/plugins/ok_import/job_resource_by_id.py
+++ src/plugins/ok_import/job_resource_by_id.py
@@ -8,9 +8,9 @@ from thread_job import ThreadJob
 class JobResourceById(Resource):
 
     @possibleException
-    def get(self, serviceName, job_id):
+    def get(self, serviceName, jobId):
         return JobManager.getJob(job_id)
 
     @possibleException
-    def delete(self, serviceName, job_id):
+    def delete(self, serviceName, jobId):
         return JobManager.stopJob(job_id)
