5e20188b6619b84a138da3b623f5f7b17c0a806d
Tatyana Berlenko <tank.berlin@gmail.com>
2015-08-20 15:22:20 +0200

    GT-1560

---
+++ src/plugins/ok_import/job_resource_by_id.py
@@ -0,0 +1,16 @@
+from possible_exception import possibleException
+from flask_restful import reqparse
+from flask.ext.restful import Resource
+from job_manager import JobManager
+from ok_import_resource_parser import OKImportParser
+from thread_job import ThreadJob
+
+class JobResourceById(Resource):
+
+    @possibleException
+    def get(self, serviceName, jobId):
+        return JobManager.getJob(jobId)
+
+    @possibleException
+    def delete(self, serviceName, jobId):
+        return JobManager.stopJob(jobId)
