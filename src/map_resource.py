c8f6f3f55c0c3b939c9ecdc7ea7d380ad6e89afb
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-08-20 19:18:37 +0300

    GT-1556

--- src/map_resource.py
+++ src/map_resource.py
@@ -3,9 +3,17 @@ from flask.ext.restful import Resource
 from flask_restful import reqparse
 from flask import render_template
 from flask import make_response
+from base_exception import BaseException
+from point_list_resource_parser import PointListResourceParser
 
 
 class MapResource(Resource):
 
     def get(self, serviceName=None):
-        return make_response(render_template('map.html'))
+        try:
+            args = PointListResourceParser.parseGetParameters()
+            args['serviceName'] = serviceName
+            getparam = args
+            return make_response(render_template('map.html', params=getparam))
+        except Exception as e:
+            return make_response(render_template('map.html'))
