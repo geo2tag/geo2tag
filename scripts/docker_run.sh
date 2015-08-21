f723d26744e00a85658a1ca5494d359aef861bbc
Vladislav <farmatholin@gmail.com>
2015-08-22 00:14:06 +0300

    GT-1535

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+#start mongo
+exec mongod --smallfiles --noprealloc &
 #deploy app
 ./scripts/local_deploy.sh
-#start services
-exec mongod --smallfiles --noprealloc &
+#start service
 cp config/supervisord.conf /etc/supervisord.conf
 supervisord -n
