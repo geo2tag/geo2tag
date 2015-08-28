f56baf2f85d06762ee55b9994b12b7b55aabbb13
ilyaolchikov <ilyaolchikov@mail.ru>
2015-08-28 20:59:22 +0300

    GT-1577. Done

--- src/tst/test_jobs_creator.py
+++ src/tst/test_jobs_creator.py
@@ -3,12 +3,13 @@ import sys
 import requests
 sys.path.append('../performance/od_performance/')
 from jobs_creator import createImportJob
-TEST_URL = 'http://geomongo/instance/plugin/ok_import/service/testservice/job'
-TEST_DATA = '{"channelName":"test_GT_1286","openDataUrl":"http://mobile.openkarelia.org//get_nearest_objects?latitude=61.787458487564&longitude=34.362810647964", "showObjectUrl":"", "showImageUrl":""}'
+TEST_URL = 'http://httpbin.org/status/200'
+TEST_DATA = '{}'
 
 
 class TestJobsCreator(unittest.TestCase):
 
     def testJobsCreator(self):
+    	
         self.assertEquals(
             createImportJob(TEST_URL, TEST_DATA).status_code, 200)
