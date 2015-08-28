20941d76894e10b406caec80e8d552dd860b6812
ilyaolchikov <ilyaolchikov@mail.ru>
2015-08-28 21:04:46 +0300

    GT-1576. Convert some code to standart pep8

--- src/performance/od_performance/jobs_parser.py
+++ src/performance/od_performance/jobs_parser.py
@@ -8,14 +8,18 @@ VALUE = 'value'
 TIME = 'time'
 JOBS_LIST = {'average': {}, 'min': {}, 'max': {}}
 
+
 def timeConvert(data):
-    return string.zfill(str(data/3600000000), 1) + ':' + string.zfill(str(data/60000000), 2) + ':' + string.zfill(str(data/1000000),2) + '.' + str(data)[:6]
+    return string.zfill(str(data / 3600000000), 1) + ':' + string.zfill(str(data / 60000000), 2) + ':' + string.zfill(str(data / 1000000), 2) + '.' + str(data)[:6]
+
 
 def getImportJobsText(viewJobsLink):
     return requests.get(viewJobsLink).text
 
+
 def parseJobs(jobsText):
     return json.loads(jobsText)
+
 
 def areAllJobsDone(jobsList):
     for job in jobsList:
@@ -23,12 +27,14 @@ def areAllJobsDone(jobsList):
             return False
     return True
 
+
 def createJobStatistic(jobsList):
     minValue = maxMalue = averageValue = summ = 0
     for i in range(len(jobsList)):
         jobTime = jobsList[i].get(TIME)
         timeObj = datetime.strptime(jobTime, "%H:%M:%S.%f")
-        summ += int(timeObj.strftime("%f")) + (int(timeObj.strftime("%S")) + int(timeObj.strftime("%M")) * 60 + int(timeObj.strftime("%H")) * 3600) * 1000000
+        summ += int(timeObj.strftime("%f")) + (int(timeObj.strftime("%S")) + int(
+            timeObj.strftime("%M")) * 60 + int(timeObj.strftime("%H")) * 3600) * 1000000
         if i == 0:
             minValue = timeObj
             maxValue = timeObj
@@ -36,7 +42,7 @@ def createJobStatistic(jobsList):
             maxValue = timeObj
         if minValue > timeObj:
             minValue = timeObj
-    JOBS_LIST['average'] = timeConvert(summ/len(jobsList))
+    JOBS_LIST['average'] = timeConvert(summ / len(jobsList))
     JOBS_LIST['min'] = minValue.strftime("%H:%M:%S.%f")
     JOBS_LIST['max'] = maxValue.strftime("%H:%M:%S.%f")
     return JOBS_LIST
