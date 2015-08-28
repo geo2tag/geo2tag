e07f114527975159d81789fcd11dbf766c6040a5
ilyaolchikov <ilyaolchikov@mail.ru>
2015-08-28 15:36:59 +0300

    GT-1576. Test

--- src/performance/od_performance/jobs_parser.py
+++ src/performance/od_performance/jobs_parser.py
@@ -1,10 +1,16 @@
 import requests
 import json
-
+from datetime import datetime
+import time
+import string
 DONE = 'done'
 VALUE = 'value'
 TIME = 'time'
 JOBS_LIST = {'average': {}, 'min': {}, 'max': {}}
+
+def timeConvert(data):
+    return string.zfill(str(data/3600000000), 1) + ':' + string.zfill(str(data/60000000), 2) + ':' + string.zfill(str(data/1000000),2) + '.' + str(data)[:6]
+
 def getImportJobsText(viewJobsLink):
     return requests.get(viewJobsLink).text
 
@@ -12,6 +18,7 @@ def parseJobs(jobsText):
     return json.loads(jobsText)
 
 def areAllJobsDone(jobsList):
+    print JOBS_LIST1, '***********'
     for job in jobsList:
         if job.get(DONE) == False:
             return False
@@ -20,12 +27,17 @@ def areAllJobsDone(jobsList):
 def createJobStatistic(jobsList):
     minValue = maxMalue = averageValue = summ = 0
     for i in range(len(jobsList)):
+        jobTime = jobsList[i].get(TIME)
+        timeObj = datetime.strptime(jobTime, "%H:%M:%S.%f")
+        summ += int(timeObj.strftime("%f")) + (int(timeObj.strftime("%S")) + int(timeObj.strftime("%M")) * 60 + int(timeObj.strftime("%H")) * 3600) * 1000000
         if i == 0:
-            minValue = jobsList[i].get(TIME)
-            maxValue = jobsList[i].get(TIME)
-        if jobsList[i] > maxValue:
-            maxValue = jobsList[i].get(TIME)
-        if jobsList[i] < minValue:
-            minValue = jobsList[i].get(TIME)
-        summ += jobsList[i]
-    
+            minValue = timeObj
+            maxValue = timeObj
+        if timeObj > maxValue:
+            maxValue = timeObj
+        if minValue > timeObj:
+            minValue = timeObj
+    JOBS_LIST['average'] = timeConvert(summ/len(jobsList))
+    JOBS_LIST['min'] = minValue.strftime("%H:%M:%S.%f")
+    JOBS_LIST['max'] = maxValue.strftime("%H:%M:%S.%f")
+    return JOBS_LIST
