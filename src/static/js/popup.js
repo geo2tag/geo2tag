0988f4d786e5f423b0f5d9cbf8bffefbda2782f2
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-08-30 13:00:09 +0300

    GT-1567

--- src/static/js/popup.js
+++ src/static/js/popup.js
@@ -1,18 +1,22 @@
 function getPointPopupHtml(point){
+	var text;
 	var JSON = 'json';
 	var IMAGE_URL = 'image_url'
 	var NAME = 'name';
 	var DESCRIPTION = 'description';
 	var SOURCE_URL = 'source_url';
-	var text;
-	var image_url = point[JSON][IMAGE_URL];
-	var name = point[JSON][NAME];
-	var description = point[JSON][DESCRIPTION];
-	var source_url = point[JSON][SOURCE_URL];
-	var text_img = '<img src="' + image_url + '" width="50" height="50">';
-	var text_name = '<b>  Name:</b>' + name  + '<br>';
-	var text_description = '<b>  Description:</b>' + description + '<br>';
-	var text_source = '<b>  Source_url:</b><a href="' + source_url + '">' + source_url + '</a><br>';
-	text = '<table><tr><td>' + text_img + '</td>' + '<td>' + text_name + text_description + text_source + '</td></tr></table>';
+	if(point[JSON] != null){
+		var image_url = point[JSON][IMAGE_URL] != null ? '<img src="' + point[JSON][IMAGE_URL] + '" width="50" height="50">': 'No image';
+		var name = point[JSON][NAME] != null ? point[JSON][NAME] : ' - ';
+		var description = point[JSON][DESCRIPTION] != null ? point[JSON][DESCRIPTION]: ' - ';
+		var source_url = point[JSON][SOURCE_URL] != null ? '<a href="' + point[JSON][SOURCE_URL] + '">' + point[JSON][SOURCE_URL] + '</a>' : ' - ';
+		var text_img = image_url;
+		var text_name = '<b>  Name:</b>' + name  + '<br>';
+		var text_description = '<b>  Description:</b>' + description + '<br>';
+		var text_source = '<b>  Source_url:</b>' + source_url + '<br>';
+		text = '<table><tr><td>' + text_img + '</td>' + '<td>' + text_name + text_description + text_source + '</td></tr></table>';
+	}else{
+		text = "<b>" + point['_id']['$oid'] + "</b>";
+	}
 	return text;
 }
