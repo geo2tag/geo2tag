f0837f1a7a8d13df5136606f81345f337bef4466
ilyaolchikov <ilyaolchikov@mail.ru>
2015-09-03 23:24:02 +0300

    GT-1583

--- src/tst/test_applyDateCriterion.py
+++ src/tst/test_applyDateCriterion.py
@@ -7,27 +7,18 @@ from bson.objectid import ObjectId
 
 TEST_SERVICE = 'testservice'
 
-
+#(112, 9, 18, 22, 13, 20) -58610224000000
+#(1063, 5, 19, 3, 33, 20) -28610224000000
 TEST_CHANNELS = [
-    ObjectId("556721a52a2e7febd2744201")]
+    ObjectId("556721a52a2e7febd2744203")]
 TEST_NUMBER = 100
 
 
-class TestFindpoints(unittest.TestCase):
+class TestApplydateCriterion(unittest.TestCase):
 
-    def testChannelSearch(self):
-        VALID_RESULTS_NUMBER = 4
-        result = list(findPoints(TEST_SERVICE, TEST_CHANNELS, TEST_NUMBER))
-        self.assertEquals(VALID_RESULTS_NUMBER, len(result))
-
-    def testDateSearch(self):
-        TEST_DATE_FROM = dateutil.parser.parse("900")
-        TEST_DATE_FROM = dateutil.parser.parse("0")
-        TEST_DATE_TO = dateutil.parser.parse("2000")
-        TEST_DATE_TO_1 = dateutil.parser.parse("2000")
-        VALID_RESULTS_NUMBER_1 = 4
-        VALID_RESULTS_NUMBER_2 = 3
-        VALID_RESULTS_NUMBER_3 = 1
+    def testApplydateCriterion2(self):
+        TEST_DATE_FROM = dateutil.parser.parse("1000")
+        TEST_DATE_TO = dateutil.parser.parse("1200")
         result_1 = list(
             findPoints(
                 TEST_SERVICE,
@@ -48,8 +39,8 @@ class TestFindpoints(unittest.TestCase):
                 None,
                 None,
                 None,
-                None,
-                TEST_DATE_TO_1))
+                TEST_DATE_FROM,
+                None))
         result_3 = list(
             findPoints(
                 TEST_SERVICE,
@@ -59,77 +50,8 @@ class TestFindpoints(unittest.TestCase):
                 None,
                 None,
                 None,
-                TEST_DATE_FROM_1))
-        self.assertEquals(VALID_RESULTS_NUMBER_1, len(result_1))
-        self.assertEquals(VALID_RESULTS_NUMBER_2, len(result_2))
-        self.assertEquals(VALID_RESULTS_NUMBER_3, len(result_3))
-
-    def testAltitudeSearch(self):
-        TEST_ALT_FROM = 1
-        TEST_ALT_FROM1 = 2
-
-        TEST_ALT_TO = 2
-        TEST_ALT_TO1 = 1
-
-        VALID_RESULTS_NUMBER_1 = 4
-        VALID_RESULTS_NUMBER_2 = 2
-        VALID_RESULTS_NUMBER_3 = 2
-
-        result_1 = list(
-            findPoints(
-                TEST_SERVICE,
-                TEST_CHANNELS,
-                TEST_NUMBER,
-                None,
-                TEST_ALT_FROM,
-                TEST_ALT_TO))
-        result_2 = list(
-            findPoints(
-                TEST_SERVICE,
-                TEST_CHANNELS,
-                TEST_NUMBER,
-                None,
-                TEST_ALT_FROM1))
-        result_3 = list(
-            findPoints(
-                TEST_SERVICE,
-                TEST_CHANNELS,
-                TEST_NUMBER,
-                None,
-                None,
-                TEST_ALT_TO1))
-        self.assertEquals(VALID_RESULTS_NUMBER_1, len(result_1))
-        self.assertEquals(VALID_RESULTS_NUMBER_2, len(result_2))
-        self.assertEquals(VALID_RESULTS_NUMBER_3, len(result_3))
-
-    def testGeometrySearch(self):
-        TEST_GEOMETRY_P = {'type': 'Polygon', 'coordinates': [
-            [[0.9, 0.9], [0.9, 1.1], [1.4, 1.1], [1.4, 0.9], [0.9, 0.9]]]}
-        TEST_GEOMETRY_C = {'type': 'Point', 'coordinates': [1, 1]}
-        TEST_RADIUS = 0.003
-
-        VALID_RESULTS_NUMBER_1 = 3
-        VALID_RESULTS_NUMBER_2 = 2
-
-        result_1 = list(
-            findPoints(
-                TEST_SERVICE,
-                TEST_CHANNELS,
-                TEST_NUMBER,
-                TEST_GEOMETRY_P))
-        result_2 = list(
-            findPoints(
-                TEST_SERVICE,
-                TEST_CHANNELS,
-                TEST_NUMBER,
-                TEST_GEOMETRY_C,
-                None,
-                None,
-                None,
-                None,
-                None,
-                None,
-                TEST_RADIUS))
-
-        self.assertEquals(VALID_RESULTS_NUMBER_1, len(result_1))
-        self.assertEquals(VALID_RESULTS_NUMBER_2, len(result_2))
+                TEST_DATE_TO,
+                None))
+        print result_1
+        print result_2
+        print result_3
