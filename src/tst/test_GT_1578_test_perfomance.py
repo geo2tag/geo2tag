30b55f755461d4306eafbdd669e2cb114459b13e
ilyaolchikov <ilyaolchikov@mail.ru>
2015-09-06 17:18:02 +0300

    GT-1623

--- src/tst/test_GT_1578_test_perfomance.py
+++ src/tst/test_GT_1578_test_perfomance.py
@@ -6,8 +6,8 @@ from test_performance import main
 CREATE_JOB_LINK = 'http://geomongo/instance/plugin/ok_import/service/testservice/job'
 JOB_DATA = '{"channelName":"testchannel","openDataUrl":"http://mobile.openkarelia.org//get_nearest_objects?latitude=61.787458487564&longitude=34.362810647964", "showObjectUrl":"", "showImageUrl":""} '
 VIEW_JOB_LINK = 'http://geomongo/instance/plugin/ok_import/service/testservice/job'
-JOB_COUNT = 2
-TIMEOUT = 2
+JOB_COUNT = 1
+TIMEOUT = 5
 
 class test_GT_1578testPerfomance(TestCase):
 
