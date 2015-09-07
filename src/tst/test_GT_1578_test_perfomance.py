9bc0f9cd47a5fde3ffca0caae42cf76e56f5646a
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-07 14:16:20 +0300

    GT-1485

--- src/tst/test_GT_1578_test_perfomance.py
+++ src/tst/test_GT_1578_test_perfomance.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../../scripts/performance/od_performance')
 from test_performance import main
 
-CREATE_JOB_LINK = 'http://geomongo/instance/plugin/ok_import/service/testservice/job'
+CREATE_JOB_LINK = 'http://not_valid_link'
 JOB_DATA = '{"channelName":"testchannel","openDataUrl":"http://mobile.openkarelia.org//get_nearest_objects?latitude=61.787458487564&longitude=34.362810647964", "showObjectUrl":"", "showImageUrl":""} '
 VIEW_JOB_LINK = 'http://geomongo/instance/plugin/ok_import/service/testservice/job'
 JOB_COUNT = 2
@@ -13,5 +13,5 @@ class test_GT_1578testPerfomance(TestCase):
 
     def test_GT_1578testPerfomance(self):
         ans = main(CREATE_JOB_LINK, JOB_DATA, VIEW_JOB_LINK, JOB_COUNT, TIMEOUT)
-        self.assertEquals(0, ans)
+        self.assertEquals(1, ans)
 
