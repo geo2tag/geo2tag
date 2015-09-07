c09ac65e0036f6466ad0a6d14dcb8c016a6c29ac
galiaf95 <galiaf1995@gmail.com>
2015-09-08 02:24:16 +0300

    GT-1620: Done

--- src/plugins/ok_import/open_karelia_object_to_point_translator.py
+++ src/plugins/ok_import/open_karelia_object_to_point_translator.py
@@ -38,19 +38,68 @@ class OpenKareliaObjectToPointTranslator:
                 float(self.objectRepresentation['latitude']),
                 float(self.objectRepresentation['longitude'])]}
         point['alt'] = 0
-        point['date'] = self.translateDate()
+        # Date trnslation
+        trans_date = self.translateDate()
+        if isinstance(trans_date, datetime):
+            point['date'] = trans_date
+            point['bc'] = False
+        elif len(trans_date) == 2:
+            point['date'] = trans_date[0]
+            point['bc'] = trans_date[1]
+        else:
+            point['date'] = trans_date[0]
+            point['json']['date_'] = trans_date[1]
+            point['bc'] = trans_date[2]
+            point['json']['bc_'] = trans_date[3]
+
         return point
 
     def translateDate(self):
-        try:
-            if self.objectRepresentation.get('year') != None:
-                return datetime(
-                    int(self.objectRepresentation['year']), 1, 1, 0, 0)
-            elif self.objectRepresentation.get('century') != None:
-                return datetime(int(self.objectRepresentation[
-                                'century']) * 100, 1, 1, 0, 0)
-            elif self.objectRepresentation.get('millenium') != None:
-                return datetime(int(self.objectRepresentation[
-                                'millenium']) * 1000, 1, 1, 0, 0)
-        finally:
-            return datetime.now()
+        # Checking for intervals
+        intervals_names = [['year_start', 'year_end'], ['century_start', 'century_end'],
+                           ['millenium_start', 'millenium_end']]
+        for i in range(3):
+            if key_in_dict_and_defined(intervals_names[i][0], self.objectRepresentation) and \
+                    key_in_dict_and_defined(intervals_names[i][1], self.objectRepresentation):
+                bc = {'bc_start': None, 'bc_end': None}
+                for i_bc in bc.keys():
+                    if key_in_dict_and_defined(i_bc, self.objectRepresentation):
+                        bc[i_bc] = self.objectRepresentation[i_bc]
+                    else:
+                        bc[i_bc] = False
+                return date_name_to_datetime(self.objectRepresentation, intervals_names[i][0]), \
+                    date_name_to_datetime(self.objectRepresentation, intervals_names[i][1]), \
+                    bc['bc_start'], bc['bc_end']
+        # Checking for presice dates
+        date_names = ['year', 'century', 'millenium']
+        for i in date_names:
+            datetime_from_date_name = date_name_to_datetime(self.objectRepresentation, i)
+            if datetime_from_date_name is not None:
+                bc = None
+                if key_in_dict_and_defined('bc', self.objectRepresentation):
+                    bc = self.objectRepresentation['bc']
+                else:
+                    bc = False
+                return datetime_from_date_name, bc
+        return datetime.now()
+
+
+def key_in_dict_and_defined(key, dictionary):
+    return key in dictionary and dictionary[key] is not None
+
+
+def date_name_to_datetime(names_dict, names_key):
+    if key_in_dict_and_defined(names_key,names_dict):
+        if names_key == 'year' or names_key == 'year_start' or names_key == 'year_end':
+            return datetime(int(names_dict[names_key]), 1, 1, 0, 0)
+        elif names_key == 'century' or names_key == 'century_start' or names_key == 'century_end':
+            return datetime(int(names_dict[names_key]) * 100, 1, 1, 0, 0)
+        elif names_key == 'millenium' or names_key == 'millenium_start' or names_key == 'millenium_end':
+            return datetime(int(names_dict[names_key]) * 1000, 1, 1, 0, 0)
+    return None
+
+import requests
+import json
+r = requests.get("http://mobile.openkarelia.org/get_nearest_objects?latitude=61.787458487564&longitude=34.362810647964")
+js_r = json.loads(r.text)[0]
+print OpenKareliaObjectToPointTranslator(None, None, js_r, None, None, None).translateDate()
---
+++ src/tst/test_GT_1620_ModifyingDateTranslationFromOK.py
@@ -0,0 +1,84 @@
+from unittest import TestCase
+from datetime import datetime
+import sys
+sys.path.append('../plugins/ok_import/')
+from open_karelia_object_to_point_translator import OpenKareliaObjectToPointTranslator
+
+TEST_DATA = [
+    'image_url',
+    'object_url',
+    {
+        'name': ['test_GT_1499'],
+        '_id': '111',
+        'latitude': 1,
+        'longitude': 2},
+    'test_version',
+    'test_import',
+    'channelId'
+]
+
+class TestModifyigDateTrnslationFromOK(TestCase):
+    def testModifyigDateTrnslationFromOK_JustDate(self):
+        td = OpenKareliaObjectToPointTranslator(None, None, {}, None, None, None).translateDate()
+        self.assertEqual(td.replace(microsecond=0), datetime.now().replace(microsecond=0))
+    def testModifyigDateTrnslationFromOK_PresiseDate(self):
+        td = OpenKareliaObjectToPointTranslator(None, None, {'year': 2000}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'century': 20}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'millenium': 2}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], False)
+    def testModifyigDateTrnslationFromOK_IntervalDates(self):
+        td = OpenKareliaObjectToPointTranslator(None, None, {'year_start': 2000, 'year_end': 3000}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], datetime(3000, 1, 1, 0, 0))
+        self.assertEqual(td[2], False)
+        self.assertEqual(td[3], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'century_start': 20, 'century_end': 30}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], datetime(3000, 1, 1, 0, 0))
+        self.assertEqual(td[2], False)
+        self.assertEqual(td[3], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'millenium_start': 2, 'millenium_end': 3}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], datetime(3000, 1, 1, 0, 0))
+        self.assertEqual(td[2], False)
+        self.assertEqual(td[3], False)
+    def testModifyigDateTrnslationFromOK_MixedDateTypes(self):
+        td = OpenKareliaObjectToPointTranslator(None, None, {'year': 1000, 'year_start': 2000, 'year_end': 3000}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], datetime(3000, 1, 1, 0, 0))
+        self.assertEqual(td[2], False)
+        self.assertEqual(td[3], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'century': 20, 'year_end': 3000}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(2000, 1, 1, 0, 0))
+        self.assertEqual(td[1], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'millenium': 4, 'century': 20, 'year': 3000}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(3000, 1, 1, 0, 0))
+        self.assertEqual(td[1], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'millenium_start': 2, 'millenium_end': 3, 'year_start': 4000, 'year_end': 5000, 'century_start': 60, 'century_end': 70}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(4000, 1, 1, 0, 0))
+        self.assertEqual(td[1], datetime(5000, 1, 1, 0, 0))
+        self.assertEqual(td[2], False)
+        self.assertEqual(td[3], False)
+        td = OpenKareliaObjectToPointTranslator(None, None, {'millenium_start': 2, 'millenium_end': 3, 'century_start': 40, 'century_end': 50}, None, None, None).translateDate()
+        self.assertEqual(td[0], datetime(4000, 1, 1, 0, 0))
+        self.assertEqual(td[1], datetime(5000, 1, 1, 0, 0))
+        self.assertEqual(td[2], False)
+        self.assertEqual(td[3], False)
+    def testModifyigDateTrnslationFromOK_RealRequestData(self):
+        import requests
+        import json
+        r = requests.get("http://mobile.openkarelia.org/get_nearest_objects?latitude=61.787458487564&longitude=34.362810647964")
+        js_r = json.loads(r.text)[0]
+        self.assertEqual(
+            OpenKareliaObjectToPointTranslator(None, None, js_r, None, None, None).translateDate(),
+            (datetime(1900, 1, 1, 0, 0), datetime(1900, 1, 1, 0, 0), False, False)
+        )
+    def testModifyigDateTrnslationFromOK_GetPointFunc(self):
+        td = OpenKareliaObjectToPointTranslator(TEST_DATA[0], TEST_DATA[1], TEST_DATA[2], TEST_DATA[3], TEST_DATA[4], TEST_DATA[5]).getPoint()
+        self.assertEqual(td['date'].replace(microsecond=0), datetime.now().replace(microsecond=0))
+        self.assertEqual(td['bc'], False)
