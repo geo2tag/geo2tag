0fc578511721b6026de956aa772a319a66e8891f
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-09 18:50:51 +0300

    GT-1624

--- scripts/performance/od_performance/test_performance.py
+++ scripts/performance/od_performance/test_performance.py
@@ -10,7 +10,7 @@ def main(createJobLink, jobData, viewJobsLink, jobsCount, timeout):
         for i in range(0, jobsCount):
             createImportJob(createJobLink, jobData)
     except ConnectionError:
-       #print "Connection to " +createJobLink+ " failed"
+        print "Error connection to " +createJobLink
         return 1
     time.sleep(timeout)
     jobsText = getImportJobsText(viewJobsLink)
