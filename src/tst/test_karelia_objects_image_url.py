347404da818188aea9c3f5c8ff40f91e76652444
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-15 10:56:37 +0300

    GT-1588

--- src/tst/test_karelia_objects_image_url.py
+++ src/tst/test_karelia_objects_image_url.py
@@ -7,6 +7,9 @@ from bson.objectid import ObjectId
 TEST_SERVICE = 'testservice'
 TEST_CHANNEL = [ObjectId("55dc620fbe9b3bf61be83f93")]
 TEST_NUMBER = 1000
+JSON = 'json'
+IMAGE_URL = 'image_url'
+HTTP_KEYWORD_TO_TEST = 'http'
 
 
 class TestKareliaObjectsImageUrl(unittest.TestCase):
@@ -14,4 +17,4 @@ class TestKareliaObjectsImageUrl(unittest.TestCase):
     def testKareliaObjectsImageUrl(self):
         result = list(findPoints(TEST_SERVICE, TEST_CHANNEL, TEST_NUMBER))
         for i in result:    
-            self.assertEquals( 'http' in i['json']['image_url'], True)
+            self.assertEquals( HTTP_KEYWORD_TO_TEST in i[JSON][IMAGE_URL], True)
