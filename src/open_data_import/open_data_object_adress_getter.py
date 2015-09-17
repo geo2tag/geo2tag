145ea27e0a539af0a77fab838d9632b7f049a5ab
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-17 18:37:20 +0300

    GT-1651

--- src/open_data_import/open_data_object_adress_getter.py
+++ src/open_data_import/open_data_object_adress_getter.py
@@ -1,9 +1,11 @@
 from abc import ABCMeta, abstractmethod
+
 class OpenDataObjectAddressGetter():
     __metaclass__=ABCMeta
 
     @abstractmethod
     def getAddress(obj):
+        pass
 
     def getAddressList(objList) :
-    return getAddress(x) for x in objList
+        return [getAddress(x) for x in objList]
---
+++ src/tst/test_open_data_object_address_getter.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('../open_data_import')
+from open_data_object_adress_getter import OpenDataObjectAddressGetter
+
+test = OpenDataObjectAddressGetter()
