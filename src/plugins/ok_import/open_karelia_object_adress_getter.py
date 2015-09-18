d981338562e39833b82572faf01761c70de6936b
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-18 18:38:29 +0300

    GT-1651

--- src/plugins/ok_import/open_karelia_object_adress_getter.py
+++ src/plugins/ok_import/open_karelia_object_adress_getter.py
@@ -3,5 +3,6 @@ sys.path.append('../../open_data_import')
 from open_data_object_address_getter import OpenDataObjectAddressGetter
 
 class OpenKareliaObjectAddressGetter(OpenDataObjectAddressGetter):
-	def getAdress(self, obj):
+	
+	def getAddress(self, obj):
 		return obj.site
--- src/tst/test_open_karelia_object_address_getter.py
+++ src/tst/test_open_karelia_object_address_getter.py
@@ -1,13 +1,16 @@
 import sys
 import unittest
 sys.path.append('../plugins/ok_import')
+sys.path.append('../open_data_import')
 from open_karelia_object_adress_getter import OpenKareliaObjectAddressGetter
 
-TEST_DATA = [1,2,3,4]
+TEST_SITE = 'SITE'
 
+class SiteClass:
+    site = 'SITE'
 
 class TestOpenKareliaObjectAddressGetter(unittest.TestCase):
 
     def testOpenKareliaObjectAddressGetter(self):
         test = OpenKareliaObjectAddressGetter()
-        test.getAdress(10)
+        self.assertEquals(test.getAddress(SiteClass()), TEST_SITE)
