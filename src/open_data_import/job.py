ecaadae0a43e7a37176932d7258485cde17f5999
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-09-20 21:06:37 +0300

    GT-1656

---
+++ src/open_data_import/job.py
@@ -0,0 +1,64 @@
+import threading
+import random
+import string
+from datetime import datetime
+from abc import ABCMeta, abstractmethod
+
+class Job():
+ 
+    def __init__(
+            self,
+            backgroundFunction,
+            channelName,
+            openDataUrl,
+            importDataDict,
+            serviceName):
+        self.thread = None
+        self._id = self.generateId()
+        self.startTime = datetime.now()
+        self.done = False
+        self.timeElapsed = None
+        self.backgroundFunction = backgroundFunction
+        self.channelName = channelName
+        self.openDataUrl = openDataUrl
+        self.importDataDict = importDataDict
+        self.serviceName = serviceName
+ 
+    @abstractmethod
+    def internalStart(self):
+        pass
+    @abstractmethod
+    def internalStop(self):
+        pass
+ 
+    def start(self):
+        self.startTime = datetime.now()
+        self.internalStart()
+ 
+    def stop(self):
+        self.internalStop()
+        self.done = True
+        self.timeElapsed = datetime.now() - self.startTime
+ 
+    def getTimeStatistics(self):
+        if self.timeElapsed is None:
+            return datetime.now() - self.startTime
+        return self.timeElapsed
+ 
+    def describe(self):
+        return {
+            '_id': self._id,
+            'time': str(
+                self.getTimeStatistics()),
+            'done': self.done,
+            'channelName': self.channelName,
+            'openDataUrl': self.openDataUrl,
+            'serviceName': self.serviceName}
+ 
+    @classmethod
+    def generateId(cls):
+        return ''.join(
+            random.choice(
+                string.ascii_uppercase +
+                string.ascii_lowercase +
+                string.digits) for x in range(12))
