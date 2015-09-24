7d545416d4476ccb46f8c091dc7d71cfff90e367
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-24 18:22:01 +0300

    GT-1660

--- src/plugins/ok_import/open_karelia_objects_loader.py
+++ src/plugins/ok_import/open_karelia_objects_loader.py
@@ -1,7 +1,14 @@
+import sys
+import os
+sys.path.append('../../open_data_import')
+sys.path.append('../..')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'open_data_import/')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from open_data_to_points_loader import OpenDataToPointsLoader
 import requests
 
 
-class OpenKareliaObjectsLoader:
+class OpenKareliaObjectsLoader(OpenDataToPointsLoader):
 
     def __init__(self, loadUrl):
         self.loadUrl = loadUrl
