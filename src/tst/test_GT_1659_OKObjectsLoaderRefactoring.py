824592a4f2d716205bc186b397672952ffb615d1
galiaf95 <galiaf1995@gmail.com>
2015-09-24 20:55:44 +0300

    Fixes due to comment in pull request

--- src/tst/test_GT_1659_OKObjectsLoaderRefactoring.py
+++ src/tst/test_GT_1659_OKObjectsLoaderRefactoring.py
@@ -8,17 +8,26 @@ from open_karelia_objects_loader import OpenKareliaObjectsLoader
 sys.path.append('../open_data_import/')
 from open_data_objects_loader import OpenDataObjectsLoader
 
-INHERITANCE_OK_PATT = re.compile('<class open_karelia_objects_loader.OpenKareliaObjectsLoader.*'
-                                 'class open_data_objects_loader.OpenDataObjectsLoader.*>')
+INHERITANCE_OK_PATT = re.compile("<class 'open_karelia_objects_loader.OpenKareliaObjectsLoader'.*"
+                                 "class 'open_data_objects_loader.OpenDataObjectsLoader'.*>")
+
+
 class testODOL(OpenDataObjectsLoader):
     pass
-test_class = testODOL('test')
+
+
+class testODOLimpl(OpenDataObjectsLoader):
+    def load(self):
+        return self.loadUrl
 
 
 class TestOKObjetcsLoaderRefactoring(TestCase):
     def testOKObjetcsLoaderRefactoring_inheritance(self):
-        self.assertNotEqual(re.search(INHERITANCE_OK_PATT, str(inspect.getmro(OpenKareliaObjectsLoader))), None)
+        self.assertIsNotNone(re.search(INHERITANCE_OK_PATT, str(inspect.getmro(OpenKareliaObjectsLoader))))
 
     def testOKObjetcsLoaderRefactoring_notInmlementedException(self):
-        with self.assertRaises(NotImplementedError):
-            test_class.load()
+        with self.assertRaises(TypeError):
+            testODOL('test')
+
+    def testOKObjetcsLoaderRefactoring_inmlementedException(self):
+        self.assertEqual(testODOLimpl('test').load(), 'test')
