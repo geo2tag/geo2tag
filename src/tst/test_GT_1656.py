a0a59af7fd82f12dd8b0a39381afb069dc642098
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-09-24 18:47:29 +0300

    GT-1656

--- src/tst/test_GT_1656.py
+++ src/tst/test_GT_1656.py
@@ -7,11 +7,11 @@ from thread_job import ThreadJob
 sys.path.append('../open_data_import/')
 from job import Job
 
-INHERITANCE_OK_PATT = re.compile('<class thread_job.ThreadJob.*'
+INHERITANCE_JOB_PATT = re.compile('<class thread_job.ThreadJob.*'
                                  'class job.Job.*>')
 
 class TestJobRefactoring(TestCase):
 
     def testJobRefactoring_inheritance(self):
-        self.assertNotEqual(re.search(INHERITANCE_OK_PATT, str(inspect.getmro(ThreadJob))), None)
+        self.assertNotEqual(re.search(INHERITANCE_JOB_PATT, str(inspect.getmro(ThreadJob))), None)
 
