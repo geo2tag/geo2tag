dc4c8954f4cd42f0e1d7fd3864739fef943877f6
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-25 20:37:01 +0300

    GT-1660

--- src/plugins/ok_import/open_karelia_import.py
+++ src/plugins/ok_import/open_karelia_import.py
@@ -2,7 +2,6 @@ from perfom_import_actions import performImportActions
 from open_karelia_objects_loader import OpenKareliaObjectsLoader
 from open_karelia_objects_parser import OpenKareliaObjectsParser
 from open_karelia_object_to_point_translator import OpenKareliaObjectToPointTranslator
-from open_karelia_data_to_points_loader import OpenKareliaDataToPointsLoader
 
 
 def openKareliaImport(
@@ -16,7 +15,6 @@ def openKareliaImport(
         OpenKareliaObjectsLoader,
         OpenKareliaObjectsParser,
         OpenKareliaObjectToPointTranslator,
-        OpenKareliaDataToPointsLoader,
         channelName,
         openDataUrl,
         showObjectUrl,
