b7b34174f816561a7dd5c97ea916a48a30388471
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-09-29 12:12:10 +0300

    GT-1656: removed sys.path.append

--- src/tst/test_GT_1658_OKObjectsParserRefactoring.py
+++ src/tst/test_GT_1658_OKObjectsParserRefactoring.py
@@ -2,10 +2,7 @@ import inspect
 import json
 import re
 from unittest import TestCase
-import sys
-sys.path.append('../plugins/ok_import/')
 from open_karelia_objects_parser import OpenKareliaObjectsParser
-sys.path.append('../open_data_import/')
 from open_data_objects_parser import OpenDataObjectsParser
 
 INHERITANCE_OK_PATT = re.compile("<class 'open_karelia_objects_parser.OpenKareliaObjectsParser'.*"
