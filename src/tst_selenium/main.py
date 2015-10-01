cd0966fff948b1fd8f4ab590be9ef12cde0325b0
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-10-02 00:12:15 +0300

    GT-1639

--- src/tst_selenium/main.py
+++ src/tst_selenium/main.py
@@ -3,11 +3,15 @@ import sys
 from basic_selenium_test import BasicSeleniumTest
 from test_GT_1635_status_OK import TestStatusOK
 from test_GT_1340_login_resurce import TestLoginResource
+from test_instance_tests import TestInstaceTest
+from test_instance_internal_tests import TestInstaceInternalTest
 
 def main(host):
     suite = unittest.TestSuite()
     suite.addTest(BasicSeleniumTest.parametrize(TestStatusOK, param=host))
     suite.addTest(BasicSeleniumTest.parametrize(TestLoginResource, param=host))
+    suite.addTest(BasicSeleniumTest.parametrize(TestInstaceTest, param=host))
+    suite.addTest(BasicSeleniumTest.parametrize(TestInstaceInternalTest, param=host))
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 
