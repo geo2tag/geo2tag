dd3081427c250e8ce8e36096d1c9514f8377661d
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-10-02 00:16:40 +0300

    GT-1639

--- src/selenium_tests/test_instance_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import sys
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from js_qunit_tests_wrapper import JsQUnitTestsWrapper
-
-class TestInstaceTest(JsQUnitTestsWrapper):
-    def testInstace(self):
-        self.checkTestPage(self.getUrl('/instance/tests'))
