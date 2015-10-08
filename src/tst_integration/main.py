aad5f3a53bbcec9f4c6e0f6dbb5f9253e36035e1
galiaf95 <galiaf1995@gmail.com>
2015-10-09 00:16:48 +0300

    GT-1732: Fixing errors

--- src/tst_integration/main.py
+++ src/tst_integration/main.py
@@ -39,10 +39,16 @@ from test_okimport_service_channel_not_exist import Test_OKImportJob_not_exist
 from test_GT_1511 import Test_GT_1511
 from test_bc_parametr_point_list_post import TestBcParametrPointListPost
 from test_GT_1590_extend_parse_parameters_for_point_list_resource import TestExtendPointListParserWithFlagsBC
+from test_GT_1732_GeocodingJob import TestGeocodingJob
+
 
 def main(host):
     suite = unittest.TestSuite()
     suite.addTest(
+        BasicIntegrationTest.parametrize(
+            TestGeocodingJob,
+            param=host))
+    """suite.addTest(
         BasicIntegrationTest.parametrize(
             TestPluginListResource,
             param=host))
@@ -180,7 +186,7 @@ def main(host):
     suite.addTest(
         BasicIntegrationTest.parametrize(
             TestExtendPointListParserWithFlagsBC,
-            param=host))
+            param=host))"""
 #    suite.addTest(BasicIntegrationTest.parametrize(
 #        Test_GT_1511, param=host))
 
