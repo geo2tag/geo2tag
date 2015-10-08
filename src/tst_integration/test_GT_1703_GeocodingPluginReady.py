9a7e5a89e3b104d6b8f7a6e7edc510a64fea93a8
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-10-08 21:45:28 +0300

    GT-1703

--- src/tst_integration/test_GT_1703_GeocodingPluginReady.py
+++ src/tst_integration/test_GT_1703_GeocodingPluginReady.py
@@ -2,17 +2,18 @@ import unittest
 import requests
 from basic_integration_test import BasicIntegrationTest
 import sys
+import json
 sys.path.append('../')
 from config_reader import getInstancePrefix
 
-TEST_URL = '/' + getInstancePrefix() + '/plugin/geocoder/service/testservice/job/TEST'
-VALID_RESPONSE_CODE = 404
-VALID_RESPONSE_TEXT = 'Plugin is not enabled'
+TEST_URL = '/' + getInstancePrefix() + '/plugin'
+PLUGIN_NAME = 'geocoder'
+PLUGIN_ENABLED = True
 
 class TestGeocodingPluginReady(BasicIntegrationTest):
     def testGeocodingPluginReady(self):
         response = requests.get(self.getUrl(TEST_URL))
         responseText = response.text
         responseCode = response.status_code
-        self.assertEquals(responseText, VALID_RESPONSE_TEXT)
-        self.assertEquals(responseCode, VALID_RESPONSE_CODE)
+        responseText = json.loads(responseText)
+        self.assertEqual(responseText[PLUGIN_NAME],PLUGIN_ENABLED)
