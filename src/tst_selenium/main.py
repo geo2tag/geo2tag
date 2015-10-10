5544196b64cef37b4eb61bae4e5f3b5215d62688
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-10-10 22:30:34 +0300

    GT-1737

--- src/tst_selenium/main.py
+++ src/tst_selenium/main.py
@@ -6,6 +6,8 @@ from test_GT_1340_login_resurce import TestLoginResource
 from test_instance_tests import TestInstaceTest
 from test_instance_internal_tests import TestInstaceInternalTest
 
+
+
 def main(host):
     suite = unittest.TestSuite()
     suite.addTest(BasicSeleniumTest.parametrize(TestStatusOK, param=host))
@@ -22,5 +24,5 @@ def main(host):
     sys.exit(returnCode)
 
 if __name__ == '__main__':
-    host = sys.argv[1]
-    main(host)
+    host_arg = sys.argv[1]
+    main(host_arg)
