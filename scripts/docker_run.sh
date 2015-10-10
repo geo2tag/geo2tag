f9c4f68279c10aad40d6bfbe5b8174c4b823b609
Vladislav <farmatholin@gmail.com>
2015-10-10 14:45:15 +0300

    GT-1720

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,10 +2,11 @@
 
 #start mongo
 exec mongod --smallfiles --noprealloc &
-#create link
-cp config/geomongo.conf config/000-default.conf
 #deploy app
-./scripts/local_deploy.sh 000-default.conf
+./scripts/local_deploy.sh
+#create link
+rm -f /etc/apache2/sites-enabled/000-default.conf
+ln -s config/geomongo.conf /etc/apache2/sites-enabled/000-default.conf
 #start service
 cp config/supervisord.conf /etc/supervisord.conf
 supervisord -n
