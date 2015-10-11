c4f66b5d07ba4e1beb63f4f2ed6fb3ae8b344f11
Stanislav Kruglov <kruglov.stas@gmail.com>
2015-10-11 11:17:01 +0300

    GT-1697: pep8's errors  fixes

--- src/tst_integration/test_GT_1697.py
+++ src/tst_integration/test_GT_1697.py
@@ -6,6 +6,7 @@ from basic_integration_test import BasicIntegrationTest
 TEST_URL = 'instance/login'
 VALID_RESPONSE_CODE = 200
 
+
 class TestGT1697(BasicIntegrationTest):
     def testGT1697(self):
         response = requests.get(self.getUrl(TEST_URL))
