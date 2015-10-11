71de18f846b151d7b72420bb9b9d7d7d5682eeb8
galiaf95 <galiaf1995@gmail.com>
2015-10-11 23:34:05 +0300

    GT-1742

--- src/plugins/geocoder/geocoder_import.py
+++ src/plugins/geocoder/geocoder_import.py
@@ -2,41 +2,57 @@ from json import dumps
 import sys
 sys.path.append('../../')
 from db_model import getServiceIdByName, getChannelByName, getDbObject
+from config_reader import getGeonamesLogin
 sys.path.append('../../geocoders/')
 from geocoder_request import GeonamesRequestSender
 from geocoder_response_parser import GeocoderResponseParser
 
 POINTS_COLL = 'points'
+CHANNEL_ID = 'channel_id'
 JSON = 'json'
 ADDRESS = 'address'
 ID = '_id'
 PROC_BLOCK = 5000
 
+
 def GeocoderImport(self,  channelName, serviceName):
+    # Checking for service existence
     getServiceIdByName(serviceName)
-    getChannelByName(serviceNamem, channelName)
-    points_coll = getDbObject(serviceName)[POINTS_COLL]
-    all_points = list(points_coll.find())
+    # Getting channel id
+    channel = getChannelByName(serviceName, channelName)
+    channel_id = channel[ID]
+    # Initialising requester and parser
     greqv = GeonamesRequestSender()
     gpars = GeocoderResponseParser()
-    point_blocks = slice_generator(all_points, PROC_BLOCK)
-    for point_block in point_blocks:
-        current_size = len(point_blocks)
+    # Getting points collection
+    points_coll = getDbObject(serviceName)[POINTS_COLL]
+    # Getting first slice of <= 5000 points
+    proc_step = 0
+    point_block = list(points_coll.find({
+        CHANNEL_ID: channel_id,
+        JSON+'.'+ADDRESS: {"$exists": True}
+    }).skip(proc_step).limit(PROC_BLOCK))
+    while point_block:
+        current_size = len(point_block)
         addresses = [point[JSON][ADDRESS] for point in point_block]
         ids = [point[ID] for point in point_block]
-        string_addresses = greqv.requestCoordinates(addresses, 'geo2tag', json_list_to_list_of_strings)
+        string_addresses = greqv.requestCoordinates(
+            addresses,
+            getGeonamesLogin(),
+            json_list_to_list_of_strings)
         json_coords = gpars.parseList(string_addresses)
         for i in range(0, current_size):
+            # If no coords was found - we change nothing
+            if json_coords[i] is None:
+                continue
             points_coll.update(
-                {
-                    ID: ids[i]
-                },
-                {
-                    '$set': {
-                        'location.coordinates': json_coords[i]
-                    }
-                }
+                {ID: ids[i]},
+                {'$set': {'location.coordinates': json_coords[i]}}
             )
+        # Getting next slice of <= 5000 points
+        proc_step += PROC_BLOCK
+        point_block = list(points_coll.find().skip(proc_step).limit(PROC_BLOCK))
+
 
 def json_list_to_list_of_strings(json_list):
     list_of_strings = []
@@ -44,12 +60,8 @@ def json_list_to_list_of_strings(json_list):
         list_of_strings.append(dumps(json))
     return list_of_strings
 
-def slice_generator(list_for_slices, slice_size):
-    slices = []
-    for i in range(0, len(list_for_slices), slice_size):
-        slices.append(list_for_slices[i:i+slice_size])
-    return slices
 
 def field_in_dict_and_defined(field, dictionary):
     return field in dictionary and dictionary[field] is not None
 
+#GeocoderImport(None, 'geocoder_plugin_test_channel', 'testservice')
