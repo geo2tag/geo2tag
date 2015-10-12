e4bc0f72ec6ae0d7abd6a0118c96891d1fa7861f
ilyaolchikov <ilyaolchikov@mail.ru>
2015-10-12 21:22:08 +0300

    GT-1731

--- src/open_data_import/job_list_resource_factory.py
+++ src/open_data_import/job_list_resource_factory.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('/var/www/geomongo/plugins/ok_import')
 from possible_exception import possibleException
 from flask_restful import reqparse
 from flask.ext.restful import Resource
