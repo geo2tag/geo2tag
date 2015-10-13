663e34851b6aba5f7e3835722bf1b0bc87bf899a
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-10-13 11:46:33 +0300

    GT-1731: resource derived

--- src/plugins/geocoder/main.py
+++ src/plugins/geocoder/main.py
@@ -1,10 +1,14 @@
 from job_resource import JobResource
 from geocoding_job_list_resource import GeocodingJobListResource
 
+class GeocodingJobResource(JobResource):
+    def derivedMethodStub(self):
+        pass
+
 
 def getPluginResources():
     return {'service/<string:serviceName>/job': GeocodingJobListResource,
-            'service/<string:serviceName>/job/<string:jobId>': JobResource}
+            'service/<string:serviceName>/job/<string:jobId>': GeocodingJobResource}
 
 
 def getPluginInfo():
