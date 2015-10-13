abae1f900d85352d04bfe3564c760e5380937163
Tatyana Berlenko <tank.berlin@gmail.com>
2015-10-13 15:40:30 +0200

    GT-1771

--- src/plugins/ok_import/ok_import_resource_parser.py
+++ src/plugins/ok_import/ok_import_resource_parser.py
@@ -1,12 +1,12 @@
 import sys
 import os
-sys.path.append(
+'''sys.path.append(
     os.path.abspath(
         os.path.join(
             os.path.dirname(__file__),
             '..',
             '..',
-            'open_data_import/')))
+            'open_data_import/')))'''
 from od_import_parser import OdImportParser, CHANNEL_NAME, OPEN_DATA_URL
 
 SHOW_OBJECT_URL = 'showObjectUrl'
