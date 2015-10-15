f53da1ac113401527d701463bbc88485878683de
Stanislav Kruglov <kruglov.stas@gmail.com>
2015-10-15 23:45:45 +0300

    GT-1764: parser was added; unittest was added;

--- src/UserListResourceParser.py
+++ src/UserListResourceParser.py
@@ -1,11 +1,19 @@
 from flask.ext.restful import Resource
-from possible_exception import possibleException
-from flask import request
+from flask_restful import reqparse
+
+
+GET_ARGS_NUMBER = 'number'
+GET_ARGS_OFFSET = 'offset'
+GET_ARGS_LOGIN = 'login'
 
 
 class Userlistresourceparser(Resource):
 
-    @possibleException
-    def post(self):
-        number = self.request.get("name")
-        
+    @staticmethod
+    def parseGetParameters():
+        parser = reqparse.RequestParser()
+        parser.add_argument(GET_ARGS_NUMBER, type=int, default=None)
+        parser.add_argument(GET_ARGS_OFFSET, type=int, default=None)
+        parser.add_argument(GET_ARGS_LOGIN, type=str, default=None)
+        args = parser.parse_args()
+        return args
