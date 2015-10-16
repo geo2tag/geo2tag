9f128d0e568bf464b5514644da6afce9cd97d813
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-10-16 20:28:14 +0300

    GT-1780

--- scripts/validate_plugin.py
+++ scripts/validate_plugin.py
@@ -8,27 +8,30 @@ TAIL_PEP8 = '/*.py'
 FORMAT = ' --format='
 DEFAULT = 'default'
 
-def make_reqpep8(name_plugin,type_format):
+
+def make_reqpep8(name_plugin, type_format):
     num_error = 0
-    str_pep8 = PEP8 + PATH_PLUGIN_DIR + str(name_plugin) + TAIL_PEP8 + FORMAT + type_format
+    str_pep8 = PEP8 + PATH_PLUGIN_DIR + \
+        str(name_plugin) + TAIL_PEP8 + FORMAT + type_format
     data = os.popen(str_pep8).read()
     if len(data) > 0:
-    	print data
-    	num_error = 1
+        print data
+        num_error = 1
     print 'Error code: ' + str(num_error)
     return num_error
 
+
 def run():
-    
+
     if len(sys.argv) == 1:
         print 'No argument: plugin name.'
 
     if len(sys.argv) == 2:
-    	name_plugin = sys.argv[1]
+        name_plugin = sys.argv[1]
         make_reqpep8(name_plugin, DEFAULT)
     if len(sys.argv) == 3:
-    	name_plugin = sys.argv[1]
-    	type_format = sys.argv[2]
-    	make_reqpep8(name_plugin, type_format)
+        name_plugin = sys.argv[1]
+        type_format = sys.argv[2]
+        make_reqpep8(name_plugin, type_format)
 if __name__ == '__main__':
     run()
--- src/tst/test_GT_1780_validate_plugin.py
+++ src/tst/test_GT_1780_validate_plugin.py
@@ -5,11 +5,9 @@ from unittest import TestCase
 import os
 import sys
 
-NAME_PLUGIN = 'test_for_validate_plugin'
+NAME_PLUGIN = 'geocoder'
 PY_SCRIPT = 'python ./scripts/validate_plugin.py ' + NAME_PLUGIN
-RESULT_PEP8 = 'src/plugins/test_for_validate_plugin/main.py:11:80: E501 line too long (145 > 79 characters)\n\nError code: 1\n'
-FORMAT = '"%\(code\)s"'
-RESULT_PEP8_FORMAT = 'E501\n\nError code: 1\n'
+RESULT_PEP8 = 'Error code: 0\n'
 
 
 class TestValidatePlugin(TestCase):
@@ -18,11 +16,4 @@ class TestValidatePlugin(TestCase):
         os.chdir('../..')
         data = os.popen(PY_SCRIPT).read()
         self.assertEqual(RESULT_PEP8, data)
-        os.chdir('src/tst')
-
-    def testValidatePlugin_Myformat(self):
-
-        os.chdir('../..')
-        data = os.popen(PY_SCRIPT + ' ' + FORMAT).read()
-        self.assertEqual(RESULT_PEP8_FORMAT, data)
         os.chdir('src/tst')
