bd3354f400fcc20e336e2dcede6ef98feaa949e2
Tatyana Berlenko <tank.berlin@gmail.com>
2015-10-19 17:33:06 +0200

    GT-1669

--- src/plugins/test_plugin_for_fail/main.py
+++ src/plugins/test_plugin_for_fail/main.py
@@ -1,5 +1,5 @@
-#from test_resource_2 import TestResource2
-#from test_resource_1 import TestResource1
+# from test_resource_2 import TestResource2
+# from test_resource_1 import TestResource1
 # def getPluginResources():
 #    result = {'res2':TestResource2, 'res1': TestResource1}
 #    return result
