426cc4bcca17406a056b1cba97a805d8c5134119
Vladislav <farmatholin@gmail.com>
2015-10-24 16:02:10 +0300

    GT-1720

--- scripts/papache_conf_generator.py
+++ scripts/papache_conf_generator.py
@@ -6,29 +6,24 @@ TEMPLATE_CONF = 'config/template.conf'
 SAVE_FOLDER = 'config/'
 
 
-def usage():
-    print "-n [server name] -f [folder] -o [output_name]"
-    sys.exit(1)
-
-
-def generate(site_name, site_folder, file_name):
+def generate(site_name, site_folder, file_name, error_log):
     with open(TEMPLATE_CONF, 'r') as conf_file:
         template = conf_file.read()
 
-    template = template.replace('%server_name%', site_name)\
-        .replace('%geomongo_path%', site_folder)
+    template = template.replace('%server_name%', site_name) \
+        .replace('%geomongo_path%', site_folder) \
+        .replace('%error_log%', error_log)
+
     with open(SAVE_FOLDER + file_name + '.conf', 'w') as res_file:
         res_file.write(template)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--name')
+    parser.add_argument('-n', '--name', required=True)
     parser.add_argument('-o', '--output', default="output")
     parser.add_argument('-f', '--folder', default='/var/www/geomongo')
+    parser.add_argument('-e', '--error', default='error')
     args = parser.parse_args()
 
-    if (args.name or args.folder) is None:
-        usage()
-    else:
-        generate(args.name, args.folder, args.output)
+    generate(args.name, args.folder, args.output, args.error)
