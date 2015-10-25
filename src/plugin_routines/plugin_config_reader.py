1ec0438b0a2585494480ebbaf500471149d71d03
Tatyana Berlenko <tank.berlin@gmail.com>
2015-10-25 22:49:01 +0100

    GT-1789

--- src/plugin_routines/plugin_config_reader.py
+++ src/plugin_routines/plugin_config_reader.py
@@ -1,21 +1,36 @@
 from configparser import RawConfigParser
-
+import os
 
 class PluginConfigReader:
 
     def __init__(self, pluginName):
         self.file_path = os.path.dirname(os.path.realpath(__file__)) + \
-            'config.ini'
+            '/config.ini'
 
     # Return content of the config.ini in a form of a dictionary
     def getConfigContent(self):
-        print '---------------------------'
-        conf = ConfigParser.RawConfigParser()
+        content = {}
+        conf = RawConfigParser()
         conf.read(self.file_path)
-        print conf.sections()
-        print '---------------------------'
-        return conf
+        sections = conf.sections()
+        for section in sections:
+            options = conf.options(section)
+            parametres = {}
+            for option in options:
+                value = conf.get(section, option)
+                parametres.update({option: value})
+            content.update({section: parametres})            
+        return content
 
     # Sets plugin config.ini content to content dictionary
     def setConfigContent(self, content):
-        pass
+        conf = RawConfigParser()
+        conf.read(self.file_path)
+        for section in content:
+            section_items = content[section].items()
+            for option, value in section_items:
+                if conf.has_section(section) == False:
+                    conf.add_section(section)
+                conf.set(section, option, value)
+        conf.write(open(self.file_path, "w"))
+
--- src/tst/test_GT_1789_plugin_config_reader.py
+++ src/tst/test_GT_1789_plugin_config_reader.py
@@ -1,9 +1,12 @@
-from unittest import TestCase
-import plugin_config_reader
+import unittest
+from plugin_config_reader import PluginConfigReader
 
 
-class TestPluginConfigReader(TestCase):
+TEST_CONTENT = {u'main': {u'db_name': u'test', u'port': u'111'}, u'geo': {u'geoname': u'g'}}
 
-    def getConfigContent(self):
-        print "getConfigContent"
-        conf = PluginConfigReader.getConfigContent()
+class TestPluginConfigReader(unittest.TestCase):
+    def testPluginConfigReader(self):
+        print "getConfigContent-------"
+        pluginConfigReader = PluginConfigReader('test')
+        conf = pluginConfigReader.getConfigContent()
+        pluginConfigReader.setConfigContent(TEST_CONTENT)
