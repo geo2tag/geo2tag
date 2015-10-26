c7af8017acc70cb7e7a0c71f0dd1e6ae030ac53d
Tatyana Berlenko <tank.berlin@gmail.com>
2015-10-26 19:08:49 +0100

    GT-1837

--- src/login_resource.py
+++ src/login_resource.py
@@ -7,15 +7,12 @@ from flask import render_template, make_response, session
 class LoginResource(Resource):
 
     def get(self):
-        print "____________________User"
-        
         if 'user_login' in session:
             user = session['user_login']
         elif 'user_id' in session:
             user = session['user_id']
         else:
             user = 'Anonym'
-        print user
         return make_response(
             render_template(
                 'login.html',
