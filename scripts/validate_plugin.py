647972f48aa2ac2233c423f56d999c2544acbb52
ilyaolchikov <ilyaolchikov@mail.ru>
2015-10-26 21:18:11 +0300

    GT-1824

--- scripts/validate_plugin.py
+++ scripts/validate_plugin.py
@@ -21,7 +21,6 @@ def make_reqpep8(name_plugin, type_format):
     print 'Error code: ' + str(num_error)
     sys.exit(str(num_error))
 
-
 def run():
     parser = argparse.ArgumentParser(description='Validate plugins')
     parser.add_argument('-name', help='Name plugin')
