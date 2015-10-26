f1f31726297926d6bed017d97e2b810ebe92033e
Tatyana Berlenko <tank.berlin@gmail.com>
2015-10-26 12:05:37 +0100

    GT-1793

--- src/tst/test_GT_1793_get_plugin_info.py
+++ src/tst/test_GT_1793_get_plugin_info.py
@@ -20,7 +20,8 @@ class TestGetPluginInfoFromDBModel(unittest.TestCase):
         result = getPluginInfo(TEST_PLUGIN_NAME)
         self.assertEquals(result, TEST_VALID_RESULT_FALSE)
         db[TEST_PLUGINS_COLLECTION].insert(
-            {NAME: TEST_PLUGIN_NAME, ENABLED: TEST_VALUE, CONFIGURABLE: TEST_VALUE})
+            {NAME: TEST_PLUGIN_NAME, ENABLED: TEST_VALUE,
+                CONFIGURABLE: TEST_VALUE})
         db[TEST_PLUGINS_COLLECTION].find({NAME: TEST_PLUGIN_NAME})
         result = getPluginInfo(TEST_PLUGIN_NAME)
         self.assertEquals(result, TEST_VALID_RESULT)
