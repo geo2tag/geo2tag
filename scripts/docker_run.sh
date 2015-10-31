e9e17da8ce9bff0b843353abda46b13e37a0fd46
Vladislav <farmatholin@gmail.com>
2015-10-31 16:35:16 +0300

    GT-1849

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,9 +2,9 @@
 
 #start mongo
 exec mongod --smallfiles --noprealloc &
-./scripts/papache_conf_generator.py -n localhost -o 000-default
+#./scripts/papache_conf_generator.py -n localhost -o 000-default.conf
 #deploy app
-./scripts/local_deploy.sh -e 000-default.conf
+./scripts/local_deploy.sh -e 000-default.conf #-s localhost
 #start service
 cp config/supervisord.conf /etc/supervisord.conf
 supervisord -n
