d5d970601e7432bfa9f4141b80f5ba7c9cfd7388
Vladislav <farmatholin@gmail.com>
2015-10-31 12:54:59 +0300

    GT-1845

--- scripts/run_unittests.sh
+++ scripts/run_unittests.sh
@@ -13,7 +13,7 @@ PATH5="$PWD/src/geocoders"
 PATH6="$PWD/src/plugins/geocoder"
 PATH7="$PWD/src/plugin_routines"
 PATH8="$PWD/scripts"
-eval 'export PYTHONPATH="$PYTHONPATH$PATH1:$PATH2:$PATH3:$PATH4:$PATH5:$PATH6:$PATH7$PATH8"'
+eval 'export PYTHONPATH="$PYTHONPATH$PATH1:$PATH2:$PATH3:$PATH4:$PATH5:$PATH6:$PATH7:$PATH8"'
 echo "$PYTHONPATH"
 DB_SCRIPTS_PATH='scripts/db/';
 
