009284a3648be7e68132dd7718eb008992231776
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-11-06 11:34:09 +0300

    GT-1876: added additional output

--- scripts/clear_tmp.sh
+++ scripts/clear_tmp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Before cleaning /tmp/"
 df -h
 LIST=`find /tmp -mtime +7 -print0 -name 'tmp*' -exec rm -fr "{}" \;`
-#echo $LIST | sed 's/\r/ /g' | wc -c
+echo "`echo $LIST | grep -o '/tmp/' | wc -w` files where deleted"
+echo "Disk space statistics after old files removing"
 df -h
