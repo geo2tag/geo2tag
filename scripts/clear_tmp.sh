1063467b00e8b989f7d309b707959374f97dc62b
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-11-10 17:36:35 +0300

    GT-1784: removed useless ouput

--- scripts/clear_tmp.sh
+++ scripts/clear_tmp.sh
@@ -5,7 +5,6 @@ AGE=${1:-7}
 echo "Before cleaning /tmp/"
 df -h
 echo "Removed files"
-find /tmp -type f  -name 'tmp*' -mtime +$AGE -print | sed -e 's|/tmp/|\n/tmp/|' 
 echo "`find /tmp -type f  -name 'tmp*' -mtime +$AGE -print | wc -l` files where deleted"
 LIST=`find /tmp -mtime +$AGE -print0 -name 'tmp*' -exec rm -fr "{}" \;`
 echo "Disk space statistics after old files removing"
