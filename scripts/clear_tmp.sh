ccd46c78938a503ac4977f88a0a618378d424ea7
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-11-10 17:31:56 +0300

    GT-1784: added param

--- scripts/clear_tmp.sh
+++ scripts/clear_tmp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+AGE=${1:-7}
+
 echo "Before cleaning /tmp/"
 df -h
 echo "Removed files"
-find /tmp -type f  -name 'tmp*' -mtime +7 -print | sed -e 's|/tmp/|\n/tmp/|' 
-echo "`find /tmp -type f  -name 'tmp*' -mtime +7 -print | wc -l` files where deleted"
-LIST=`find /tmp -mtime +7 -print0 -name 'tmp*' -exec rm -fr "{}" \;`
+find /tmp -type f  -name 'tmp*' -mtime +$AGE -print | sed -e 's|/tmp/|\n/tmp/|' 
+echo "`find /tmp -type f  -name 'tmp*' -mtime +$AGE -print | wc -l` files where deleted"
+LIST=`find /tmp -mtime +$AGE -print0 -name 'tmp*' -exec rm -fr "{}" \;`
 echo "Disk space statistics after old files removing"
 df -h
