c1d32cb50e00948be599d54d332fa9c52666ce22
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-11-10 17:20:42 +0300

    GT-1784: changed output2

--- scripts/clear_tmp.sh
+++ scripts/clear_tmp.sh
@@ -2,7 +2,7 @@
 echo "Before cleaning /tmp/"
 df -h
 LIST=`find /tmp -mtime +7 -print0 -name 'tmp*' -exec rm -fr "{}" \;`
-echo ${LIST} | sed -e 's/\/tmp\/tmp/\n/g'
+echo ${LIST} | sed -e 's/\/tmp\/tmp/\n\/tmp\/tmp/g'
 echo "`echo $LIST | grep -o '/tmp/tmp/' | wc -w` files where deleted"
 echo "Disk space statistics after old files removing"
 df -h
