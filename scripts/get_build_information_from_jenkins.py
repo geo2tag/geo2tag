cf6c51458b79092aae31f4e2a4e0e509e98c31e1
Tatyana Berlenko <tank.berlin@gmail.com>
2015-11-11 10:23:00 +0100

    GT-1693

--- scripts/get_build_information_from_jenkins.py
+++ scripts/get_build_information_from_jenkins.py
@@ -17,10 +17,12 @@ def main():
         password='qwerty')
     inf_1 = server.get_build_info('geo2tag-test', 2281)
     inf_2 = server.get_build_info('geo2tag-test', 2280)
-    
+       
     print inf_1[u'actions']
+    print len(inf_1[u'actions'])
     print '--------'
     print inf_2[u'actions']
+    print len(inf_2[u'actions'])
 
 if __name__ == '__main__':
     main()
