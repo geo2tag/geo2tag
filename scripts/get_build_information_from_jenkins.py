0969b412df2c1e133a0014bf71e1a8de824767d4
Tatyana Berlenko <tank.berlin@gmail.com>
2015-11-11 11:10:09 +0100

    GT-1693

--- scripts/get_build_information_from_jenkins.py
+++ scripts/get_build_information_from_jenkins.py
@@ -30,7 +30,9 @@ def main():
     print last_build_number
     for i in range(last_build_number, 0, -1):
         inf = server.get_build_info(JOB, i)
+        print inf[ACTIONS]
         print inf[ACTIONS][NUMBER]
+        print inf[ACTIONS][NUMBER][LAST_BUILD_REVISION]
         print inf[ACTIONS][NUMBER][LAST_BUILD_REVISION][BRANCH]
         if args.branch == inf[ACTIONS][NUMBER][LAST_BUILD_REVISION][BRANCH][NAME]:
             if inf[RESULT] == SUCCESS or inf[RESULT] == FIXED:
