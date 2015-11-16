e72efb2cf2ce0db28a92cb4224d1d95c6697192e
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-11-16 20:59:12 +0300

    GT-1884

--- src/static/js/tests/test_macros_pagination.js
+++ src/static/js/tests/test_macros_pagination.js
@@ -1,5 +1,4 @@
 QUnit.test('pagination macros test', function( assert ) {
-    var done = assert.async();
     var active_ul = 'p2';
     var set_active = 'p5';
     var unf = undefined;
@@ -9,5 +8,4 @@ QUnit.test('pagination macros test', function( assert ) {
     assert.equal(unf,pagination.getActiveUlId());
     pagination.setActiveUl(set_active);
     assert.equal(set_active,pagination.getActiveUlId());
-    done();
 });
