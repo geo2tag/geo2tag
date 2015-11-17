e106095b7a44e37a62f48b2f282c1dee1e06a91c
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-11-17 21:01:35 +0300

    GT-1910

--- src/tst/test_GT_1335_importdb.py
+++ src/tst/test_GT_1335_importdb.py
@@ -7,8 +7,9 @@ TEST_DB = 'master_db_template'
 DB_TAG = '--dbName '
 CONF_TAG = '--config '
 CONFIG_NAME = 'config'
-PATH_DB = 'python scripts/db/setupMasterDbTemplate.py ' +  DB_TAG +  TEST_DB
-PATH_CONF = 'python scripts/db/setupMasterDbTemplate.py ' +  CONF_TAG +  CONFIG_NAME
+PATH_DB = 'python scripts/db/setupMasterDbTemplate.py ' + DB_TAG + TEST_DB
+PATH_CONF = \
+    'python scripts/db/setupMasterDbTemplate.py ' + CONF_TAG + CONFIG_NAME
 TEST_PATH = os.getcwd()
 
 os.chdir('../../')
@@ -23,15 +24,14 @@ COUNT = 1
 class TestImportDb(TestCase):
 
     def testMyImport_Db_Tag(self):
-    	os.chdir('../../')
-    	os.system('./scripts/db/drop_test_db.sh')
+        os.chdir('../../')
+        os.system('./scripts/db/drop_test_db.sh')
         os.system(PATH_DB)
         count_mycoll = db[MYCOLLECTION].count()
         self.assertEqual(count_mycoll, COUNT)
-        os.system('./scripts/db/import_test_db.sh');
+        os.system('./scripts/db/import_test_db.sh')
         os.chdir(TEST_PATH)
 
     def testMyImport_Conf_Tag(self):
         count_mycoll = db[MYCOLLECTION].count()
         self.assertEqual(count_mycoll, COUNT)
-
