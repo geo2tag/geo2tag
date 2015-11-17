7fdd98b570843c246e75da44a7267d121bd9b0e6
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-11-17 20:55:14 +0300

    GT-1910

--- scripts/db/setupMasterDbTemplate.py
+++ scripts/db/setupMasterDbTemplate.py
@@ -1,6 +1,11 @@
 import os
 import sys
-import config_reader
+import argparse
+from configparser import SafeConfigParser
+
+SECTION = 'main'
+OPTION_DBNAME = 'db_name'
+DBNAME = 'geomongo'
 
 
 def import_db(db_name):
@@ -8,14 +13,29 @@ def import_db(db_name):
     os.system(str_forsh)
 
 
+def getConfigParser(config_name):
+    config = SafeConfigParser({OPTION_DBNAME: DBNAME})
+    config.read(config_name)
+    return config
+
+
+def getDbName(config_name):
+    return getConfigParser(config_name).get(SECTION, OPTION_DBNAME)
+
+
 def run():
-    if len(sys.argv) == 1:
-        db_name = config_reader.getDbName()
-    elif len(sys.argv) == 2:
-        db_name = sys.argv[1]
+
+    parser = argparse.ArgumentParser(description='Setup master db')
+    parser.add_argument('--dbName', help='Name db')
+    parser.add_argument('--config', help='Config name')
+    args = parser.parse_args()
+    if unicode(args.config) != 'None':
+        CONFIG_PATH = os.path.dirname(os.path.realpath(
+            __file__)) + '/' + unicode(args.config + '.ini')
+        db_name = getDbName(CONFIG_PATH)
+        import_db(unicode(db_name))
     else:
-        print "Error agrs!!!"
-    import_db(db_name)
+        import_db(unicode(args.dbName))
 
 if __name__ == '__main__':
     run()
