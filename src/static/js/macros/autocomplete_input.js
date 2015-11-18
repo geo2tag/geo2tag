cf1dbbf8df36a70742382d63ff52ab2237d023ee
Nikita Melnikov <nikita.melnikov95@gmail.com>
2015-11-18 18:37:51 +0300

    GT-1907

--- src/static/js/macros/autocomplete_input.js
+++ src/static/js/macros/autocomplete_input.js
@@ -1,3 +1,4 @@
+
 function AutocompliteInput(macroId,externalValue,internalValue) {
     this.macroId = macroId;
     this.inputobject = $('#autocomplite_'+macroId);
@@ -20,27 +21,28 @@ AutocompliteInput.prototype.getInternalValue = function(){
 	return this.internalValue;
 }
 AutocompliteInput.prototype.makeAutocompliteToRequest = function(){
-    var fn = this.makeAutocompliteToRequest;
+    var fn = this;
     var name = this.getExternalValue(); 
     var req_url = this.url_request + '?login=' + name + '&number=' + this.countView + '&offset=0'
-    $( this.inputobject).autocomplete({source: function(request, response) {
-        $.get( req_url, function( data ) {
-            var list = [];
-            for(var i = 0;i<data.length;i++){
-                list.push({
-                    label:data[i]['login'],
-                    value:data[i]['_id']
-                });
-            }
-            response(list);  
-            
-        });
-    },
-    select:function(e, ui) {
-        //this.externalValue = ui.item.label;
-        fn.internalValue = 1;
-        e.preventDefault();
-        $(this).val(ui.item.label);
-    }}); 
-
+    $( this.inputobject).autocomplete({
+        source: function(request, response) {
+            $.get( req_url, function( data ) {
+                var list = [];
+                for(var i = 0;i<data.length;i++){
+                    list.push({
+                        label:data[i]['login'],
+                        value:data[i]['_id']
+                    });
+                }
+                response(list);  
+                
+            });
+        },
+        select:function(e, ui) {
+            //this.externalValue = ui.item.label;
+            fn.internalValue = 1;
+            e.preventDefault();
+            $(this).val(ui.item.label);
+        }
+    }); 
 }
