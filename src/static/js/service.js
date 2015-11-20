322769120a4c618b57a164b71935de752cdd64d3
Tatyana Berlenko <tank.berlin@gmail.com>
2015-11-20 11:41:55 +0100

    GT-1913

--- src/static/js/service.js
+++ src/static/js/service.js
@@ -1,33 +1,41 @@
+$(document).ready(function (){
+    addSaveServiceHandler();
+});
+
 function saveServiceChange(service_id) {
      $.ajax({
         type: "GET",
-        url: "/instance/admin/service" + "service_id",
+        url: "/instance/admin/service" + service_id,
         timeout: 150000,
         crossDomain: true,
         success: function(json, status) {
             console.log("save service success" );
+            alert('this alert must be deleted')
             spinner.stop(target);
-            printBootstrapAlert();
+            console.log(json);
+            printBootstrapAlert(status, status, 'json');
         },
         error: function (jqXHR, textStatus, errorThrown){
             console.log("save service result: " + textStatus);
             console.log(errorThrown);
-            spinner.stop(target);
-            printBootstrapAlert();
+            printBootstrapAlert('danger', textStatus, '');
         }
     }); 
 }
+
 function addSaveServiceHandler(){
     $('#save_service_btn').click(function(){
         target = document.getElementById('spin')
         spinner = new Spinner().spin(target);
     });
-
 }
-function printBootstrapAlert(status){
-    result = '<div class="alert alert-"' + status + '>'
-    result += '<strong>' + status +'</strong>'
-    result += '</div>'
+
+function printBootstrapAlert(class_alert, status, msg){
+    result = '<div class="alert alert-' + class_alert + '">';
+    result += '<strong>' + status + ' ' + msg + '</strong>';
+    result += '</div>';
+    console.log(result);
+    $('#main_div_service_page_id').prepend(result);
 }
 
 
