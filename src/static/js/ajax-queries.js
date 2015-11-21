e266cf3b06941ddfb1af27b43e27af122eaf0c0e
ilyaolchikov <ilyaolchikov@mail.ru>
2015-11-21 22:40:43 +0300

    GT-1914

---
+++ src/static/js/ajax-queries.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	var countChannels = 0, countPoints = 0;
+	$.ajax({
+	   type: "GET",
+	   url: "/instance/service/testservice/channel?number=2000",
+	   success: function(channels){
+	     console.log(channels);
+	     countChannels = channels.length;
+		 $.each(channels, function(index){
+	        var channelId = this._id.$oid;
+	        $.ajax({
+			   type: "GET",
+			   url: "/instance/service/testservice/point?number=2000&channel_ids=" + channelId,
+			   success: function(points){
+			     console.log(points.length, index, channels.length);
+			     countPoints += points.length;
+			     if (index == channels.length - 1) {
+					return updateCounts();
+			     }
+			    }
+			});
+	     });
+	   }
+	 });
+	function updateCounts(){
+		$('.channels-count').text(countChannels);
+		$('.points-count').text(countPoints);
+	}
+});
