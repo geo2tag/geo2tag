0f14bdc85911a93da9b6b45da4383df38fd6f311
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-11-26 12:33:03 +0300

    GT-1918: added single script for running source checks

--- scripts/pep8_check.sh
+++ scripts/pep8_check.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo
+echo "===================== PEP8 CHECK ======================"
+echo
+
 pep8 `find ./ | grep .py$`
--- scripts/source_check.sh
+++ scripts/source_check.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-./scripts/pep8_check.sh
+
+./scripts/pep8_check.sh 
+codePep8=$?;
 ./scripts/pylint_check.sh 
+codePylint=$?;
+
+if [ $codePep8 -ne "0" ] || [ $codePylint -ne "0" ]
+then
+	exit 1
+fi
