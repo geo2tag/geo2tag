e60adb9706dd13f249c4a988cc4d36ffc9b8d14a
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-01 11:36:01 +0100

    GT-1985

--- src/static/js/service.js
+++ src/static/js/service.js
@@ -60,6 +60,7 @@ function getValuesForServicePage(service_name){
 }
 
 function initUi(){
+    initValuesForServicePage();
     ownerInput = new AutocompliteInput('owner_id', '/instance/user?login=' , 'login', '_id');
     logSizeInput = new IntegerInput('log_size');
 }
