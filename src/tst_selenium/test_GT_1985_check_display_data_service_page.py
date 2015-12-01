5a64470d16d03edbe21e234a03d101dcae72287f
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-01 08:56:26 +0100

    GT-1915

--- src/tst_selenium/test_GT_1985_check_display_data_service_page.py
+++ src/tst_selenium/test_GT_1985_check_display_data_service_page.py
@@ -1,18 +1,20 @@
 from basic_selenium_test import BasicSeleniumTest
 
 TEST_URL = '/instance/admin/service/testservice'
+TEST_URL_NEW = '/instance/admin/service/notvalidservicename'
 TEST_SCRIPT_RETURN_H2_VALUE = 'return $("#service_h2_id").text()'
-TEST_SCRIPT_RETURN_LOG_SIZE_VALUE = 'return $("#integer_input_log_size").text()'
-TEST_LOG_SIZE_VALUE = 10
-TEST_H2 = 'Service testservice'
-TEST_LOG_SIZE_VALUE_NEW = ''
-TEST_H2_NEW = 'New service testservice'
+TEST_SCRIPT_RETURN_LOG_SIZE_VALUE = 'return $("#integer_input_log_size").val()'
+TEST_LOG_SIZE_VALUE = u'10'
+TEST_H2 = u'Service testservice'
+TEST_LOG_SIZE_VALUE_NEW = u''
+TEST_H2_NEW = u'New service notvalidservicename'
 
 class TestCheckDisplayDataServicePage(BasicSeleniumTest):
 
     def testCheckDisplayDataServicePage(self):
         URL = self.getUrl(TEST_URL)
         self.getDriver().get(URL)
+        self.driver.implicitly_wait(30) 
         result = self.driver.execute_script(TEST_SCRIPT_RETURN_LOG_SIZE_VALUE)
         self.assertEqual(result, TEST_LOG_SIZE_VALUE)
         result = self.driver.execute_script(TEST_SCRIPT_RETURN_H2_VALUE)
