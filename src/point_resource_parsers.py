9e6bd5d882916ee153b3b4c2594ed9eb1e81d480
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-03 09:41:32 +0100

    GT-1954

--- src/point_resource_parsers.py
+++ src/point_resource_parsers.py
@@ -5,6 +5,7 @@ LON = 'lon'
 ALT = 'alt'
 JSON = 'json'
 CHANNEL_ID = 'channel_id'
+BC = 'bc'
 
 
 class PointResourceParsers():
@@ -17,5 +18,6 @@ class PointResourceParsers():
         parser.add_argument(ALT, type=float)
         parser.add_argument(JSON, type=unicode)
         parser.add_argument(CHANNEL_ID, type=unicode)
+        parser.add_argument(BC, type=boolean)
         args = parser.parse_args()
         return args
