8d1d358cfd92d71711d738401d4e0759c075c4ed
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-07 08:19:42 +0100

    GT-1997

--- src/tst_integration/test_metadata_resource.py
+++ src/tst_integration/test_metadata_resource.py
@@ -1,12 +1,16 @@
 import requests
 from basic_integration_test import BasicIntegrationTest
 
-TEST_URL = '/instance/service/testservice/metadata/552833515404411781370711'
+TEST_URL = '/instance/service/' + \
+    'testservice/metadata/552833515404411781370711'
+TEST_URL_DELETE = '/instance/service/' + \
+    'testservice/metadata/552833515404411781370722'
 VALID_RESPONSE_CODE = 200
-NOT_VALID_URL = '/instance/service/testservice/metadata/552833515404411781370710'
+NOT_VALID_URL = '/instance/service' + \
+    '/testservice/metadata/552833515404411781370710'
 NOT_VALID_RESPONSE_CODE = 400
-NOT_VALID_RESPONSE_CODE_GET = 404
-LIST_ARGS = {"json": {"name":"test_metadata_put"}}
+NOT_VALID_RESPONSE_CODE_NOT_FOUND = 404
+LIST_ARGS = {"json": {"name": "test_metadata_put"}}
 NOT_VALID_LIST_ARGS = "not_valid_data"
 
 
@@ -18,12 +22,22 @@ class TestMetadataResource(BasicIntegrationTest):
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
         response = requests.get(self.getUrl(NOT_VALID_URL))
         responseCode = response.status_code
-        self.assertEquals(responseCode, NOT_VALID_RESPONSE_CODE_GET)
+        self.assertEquals(responseCode, NOT_VALID_RESPONSE_CODE_NOT_FOUND)
 
     def testMetadataResourcePut(self):
         response = requests.put(self.getUrl(TEST_URL), data=LIST_ARGS)
         responseCode = response.status_code
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
-        response = requests.put(self.getUrl(NOT_VALID_URL), data=NOT_VALID_LIST_ARGS)
+        response = requests.put(
+            self.getUrl(NOT_VALID_URL),
+            data=NOT_VALID_LIST_ARGS)
         responseCode = response.status_code
         self.assertEquals(responseCode, NOT_VALID_RESPONSE_CODE)
+
+    def testMetadataResourceDelete(self):
+        response = requests.delete(self.getUrl(TEST_URL_DELETE))
+        responseCode = response.status_code
+        self.assertEquals(responseCode, VALID_RESPONSE_CODE)
+        response = requests.delete(self.getUrl(NOT_VALID_URL))
+        responseCode = response.status_code
+        self.assertEquals(responseCode, NOT_VALID_RESPONSE_CODE_NOT_FOUND)
