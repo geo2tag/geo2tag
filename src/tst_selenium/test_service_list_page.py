b77a4358d57971df52f04d8d7cdaab582f388743
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-12-08 12:35:55 +0300

    GT-2012: fix for selenium

--- src/tst_selenium/test_service_list_page.py
+++ src/tst_selenium/test_service_list_page.py
@@ -6,7 +6,7 @@ class TestServiceListResource(BasicSeleniumTest):
     def testServiceListResource(self):
         ELEMENT_IDS = ['create_new_service', 'service_name',
                        'service_list', 'autocomplite_owner_id']
-        URL = self.getUrl('/instance/service')
+        URL = self.getUrl('/instance/admin/service')
         self.getDriver().get(URL)
         self.getDriver().implicitly_wait(30)
         for elementId in ELEMENT_IDS:
