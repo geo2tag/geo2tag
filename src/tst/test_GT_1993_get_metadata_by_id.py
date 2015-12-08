06cb8f1c19a5a5971e244af3abb7f3e639261a61
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-08 12:38:13 +0100

    GT-2009

--- src/tst/test_GT_1993_get_metadata_by_id.py
+++ src/tst/test_GT_1993_get_metadata_by_id.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 from metadata_does_not_exist_exception import MetadataDoesNotExistException
 from db_model import getMetadataById, getDbObject
 
-TEST_ID = ObjectId("55671ae113293c504d515a33")
+TEST_ID = ObjectId("55671ae113293c504d515a31")
 TEST_DB = 'testservice'
 BAD_TEST_ID = ObjectId("aa671ae113293c504d515abb")
 TEST_COLLECTION = 'metadata'
