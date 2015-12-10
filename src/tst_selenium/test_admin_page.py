776ff256b71eb52609e35ecbe0c0f0c70928ad71
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-12-10 15:32:09 +0300

    GT-2009: removed fails from tests

--- src/tst_selenium/test_admin_page.py
+++ src/tst_selenium/test_admin_page.py
@@ -4,7 +4,7 @@ from basic_selenium_test import BasicSeleniumTest
 class TestAdminResource(BasicSeleniumTest):
 
     def testAdminResource(self):
-        URL = self.getUrl('/instance/admin1')
+        URL = self.getUrl('/instance/admin')
         self.getDriver().get(URL)
         self.getDriver().implicitly_wait(30)
         obj = self.getDriver().find_element_by_css_selector(".admin-header")
