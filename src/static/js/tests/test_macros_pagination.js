2338f62f9761eb36a942f3dc6e1dd17ec619217e
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-11 10:38:07 +0100

    GT-2042

--- src/static/js/tests/test_macros_pagination.js
+++ src/static/js/tests/test_macros_pagination.js
@@ -1,13 +1,6 @@
 QUnit.test('pagination macros test', function( assert ) {
-    var active_ul = 'p2';
-    var set_active = 'p5';
-    var unf = undefined;
     var pagination = new Pagination('pagintaion_block');
-    assert.equal(active_ul,pagination.getActiveUlId());
-    pagination.removeActivePage();
-    assert.equal(unf,pagination.getActiveUlId());
-    pagination.setActiveUl(set_active);
-    assert.equal(set_active,pagination.getActiveUlId());
+    assert.ok($('#pagintaion_block').children().children().length > 0);
 
     function testFunction(json){
         return "<div>" + json.name + "</div>"
