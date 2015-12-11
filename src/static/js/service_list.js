9b33c86a56bed4cb02283c1b9ec34db455015b3a
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-11 11:26:45 +0100

    GT-2013

--- src/static/js/service_list.js
+++ src/static/js/service_list.js
@@ -1,8 +1,8 @@
 function get_service_display(json){
     var service_name = json.name;
     var service_id = json._id;
-    var service_link = '/instance/admin/service/';
-    var result = '<div class="col-xs-8"><h3><a href = ' + service_link + service_id + '>' + service_name + '</a></h3></div>';
-    result += '<div class="col-xs-4"><button type="button" class="btn btn-primary btn-lg" id="delete_service_' + service_id + '">DELETE</button></div>';
+    var service_link = getUrlWithPrefix('/admin/service/');
+    var result = '<div class="row" id="' + service_id + '"><div class="col-xs-8"><h3><a href = ' + service_link + service_id + '>' + service_name + '</a></h3></div>';
+    result += '<div class="col-xs-4"><button type="button" class="btn btn-primary btn-lg" service_id="' + service_id + '">DELETE</button></div></div>';
     return result;
 }
