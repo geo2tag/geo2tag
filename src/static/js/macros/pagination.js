3a7b5701e095ec6c67e15a21f3b203f3995e9287
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-11 10:19:56 +0100

    GT-2042

--- src/static/js/macros/pagination.js
+++ src/static/js/macros/pagination.js
@@ -1,41 +1,9 @@
 function Pagination(macroId){
     this.macroId = macroId;
     this.jqueryObject = $('#'+macroId.toString());
-}
-
-$(function('#'+macroId) {
-    $('#'+macroId).pagination({
+    this.jqueryObject.pagination({
             items: 100,
             itemsOnPage: 10,
             cssStyle: 'light-theme'
         });
-}
-
-Pagination.prototype.setActiveUl = function(ulId){
-    this.removeActivePage();
-    this.jqueryObject.children('#' + ulId).addClass('active');
-}
-
-Pagination.prototype.getActiveUlId = function(){
-    var list_ul = this.jqueryObject.children();
-    for(var  i = 0;i<list_ul.length;i++){
-	if($(list_ul[i]).attr('class') == 'active'){
-            return list_ul[i].id;
-	    continue;
-	}
-    }
-}
-
-Pagination.prototype.getActiveUlNumber = function(){
-    var list_ul = this.jqueryObject.children();
-    for(var  i = 0;i<list_ul.length;i++){
-	if($(list_ul[i]).attr('class') == 'active'){
-	    return i;
-	    continue;
-	}
-    }
-}
-
-Pagination.prototype.removeActivePage = function(){
-    this.jqueryObject.children().removeClass();
 }
