ada950e1dc9d143d312a0cde82bebab8692b19e9
Tatyana Berlenko <tank.berlin@gmail.com>
2015-12-14 19:22:32 +0100

    GT-2063

--- src/static/js/service_list.js
+++ src/static/js/service_list.js
@@ -21,11 +21,9 @@ service_list.add({
 var ServiceView = Backbone.View.extend({
     tagName: "div",
     className: "container",
-    render: function() { // отрисовка вида (сервиса)
-        $(this.el).html(get_service_display(json)) // контроллер получит json
+    render: function() { // отрисовка вида (одного сервиса)
+        $(this.el).html(get_service_display(json))
     }
-    initialize: function(){
-
-
-    }
+//    initialize: function(){    }
 });
+
