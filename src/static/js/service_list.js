089065dbf272c8a2909a876a6de17446ccbd5ab3
mark_zaslavskiy <voodoomark@rambler.ru>
2015-12-22 11:53:33 +0000

    service_list.js edited online with Bitbucket

--- src/static/js/service_list.js
+++ src/static/js/service_list.js
@@ -61,7 +61,7 @@ function get_service_display(json){
     var service_id = json._id.$oid;
     var service_link = getUrlWithPrefix('/admin/service/');
     var result = '<div class="row" id="' + service_id + '"><div class="col-xs-8"><h3><a href = ' + service_link + service_id + '>' + service_name + '</a></h3></div>';
-    result += '<div class="col-xs-4"><button type="button" class="btn btn-primary btn-lg" service_id="' + service_id + '" onclick=btn_delete_listener(' + service_name + ')>DELETE</button></div></div>';
+    result += '<div class="col-xs-4"><button type="button" class="btn btn-primary btn-lg" service_id="' + service_id + '" onclick=btn_delete_listener("' + service_name + '")>DELETE</button></div></div>';
     return result;
 }
 
