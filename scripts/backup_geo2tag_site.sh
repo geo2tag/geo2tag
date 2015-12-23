35f8db3e7a425c4c72cc353e21ce1cb089591171
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-12-23 20:30:44 +0300

    GT-2036: added naive implementation

--- scripts/backup_geo2tag_site.sh
+++ scripts/backup_geo2tag_site.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-BACKUP_STORAGE=$1
 export SITE=geo2tag.org
 export SSH='ssh -p 20522 wpbackup@188.40.64.219'
-export BACKUP_DIRS='/var/www/geo2tag.org'
-PWD=`pwd`
-cd ${BACKUP_STORAGE}
-exec {$PWD}/scripts/copy-any-rootfs
+export BACKUP_DIRS='/var/www/geo2tag.org/'
+export PRE_COMMAND='/var/www/geo2tag.org/dump_db.sh'
+exec ./scripts/copy-any-rootfs
