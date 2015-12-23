6cea86522e0b31a5d4216a17eec7ada2e55ee045
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-12-23 13:20:52 +0300

    GT-2036: ./scripts/backup_geo2tag_site.sh fixed

--- scripts/backup_geo2tag_site.sh
+++ scripts/backup_geo2tag_site.sh
@@ -4,5 +4,6 @@ BACKUP_STORAGE=$1
 export SITE=geo2tag.org
 export SSH='ssh -p 20522 wpbackup@188.40.64.219'
 export BACKUP_DIRS='/var/www/geo2tag.org'
+PWD=`pwd`
 cd ${BACKUP_STORAGE}
-exec `dirname "$0"`/copy-any-rootfs
+exec {$PWD}/scripts/copy-any-rootfs
