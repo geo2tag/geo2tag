4f14090b5302170ebdb10e6e2e1ecc64c6157940
TankBerlin <tank.berlin@gmail.com>
2015-12-28 13:28:16 +0100

    GT-1799

--- scripts/check_task/check_test_scenario_field.py
+++ scripts/check_task/check_test_scenario_field.py
@@ -1,5 +1,23 @@
+from bitbucket.bitbucket import Bitbucket
+import webbrowser
+
+USERNAME = 'tankBerlin'
+PASSWORD = '13_HbReIf29'
+
+URL = 'https://bitbucket.org/osll/geomongo'
+CONSUMER_KEY = '7QzrFCqGNcag4sfe9S'
+CONSUMER_SECRET	= 'xKXuDB7Yz5nfNZ8ZPEe9gASGkLvacBaK'
+
+
+
 def check_test_scenario_field(issue):
+    bb = Bitbucket(USERNAME, PASSWORD)
+    success, repositories = bb.repository.all()
+    print success, repositories
+    print bb.get('geomongo')
+
     test_scenario_field = issue.fields.customfield_10800
+    print issue.fields().__dict__
     if test_scenario_field is None:
         print 'Test scenario is not found'
         return False
