d3ad6b430b9fd51616d521d5eecc782dcaeecfe6
TankBerlin <tank.berlin@gmail.com>
2015-12-29 12:21:57 +0100

    GT-2105

--- src/tst_selenium/test_GT_1913_check_spinjs_and_alert_for_service_page.py
+++ src/tst_selenium/test_GT_1913_check_spinjs_and_alert_for_service_page.py
@@ -6,10 +6,10 @@ TEST_LOG_SIZE_ID = 'integer_input_log_size'
 TEST_DATA = 0
 TEST_NOT_VALID_DATA = 'test_string'
 TEST_ALERT_CLASS = 'alert-success'
-TEST_MSG = u' Saving is finished successfully'
-TEST_MSG_ERROR = u' Saving is finished unsuccessfully error: 400'
-TEST_SCRIPT_SUCCESS = "return $('.alert-success').text()"
-TEST_SCRIPT_ERROR = "return $('.alert-danger').text()"
+TEST_MSG = u'Saving is finished successfully'
+TEST_MSG_ERROR = u'Saving is finished unsuccessfully error: 400'
+TEST_CLASS_ALERT_SUCCESS = "alert-success"
+TEST_CLASS_ALERT_ERROR = "alert-danger"
 TEST_SCRIPT_SPIN = "return $('.spinner').length"
 
 
@@ -22,10 +22,8 @@ class TestCheckSpinjsAndAlertForServicePage(BasicSeleniumTest):
         self.assertEqual(result, 0)
         self.driver.find_element_by_id(TEST_LOG_SIZE_ID).send_keys(TEST_DATA)
         self.driver.find_element_by_id(TEST_BTN_SAVE_ID).click()
-        alert = self.driver.switch_to_alert()
-        alert.accept()
-        result = self.driver.execute_script(TEST_SCRIPT_SUCCESS)
-        self.assertEqual(result, TEST_MSG)
+        result = self.driver.find_element_by_class_name(TEST_CLASS_ALERT_SUCCESS)
+        self.assertEqual(result.text, TEST_MSG)
 
     def testBadScenarioForServicePage(self):
         URL = self.getUrl(TEST_URL)
@@ -33,5 +31,5 @@ class TestCheckSpinjsAndAlertForServicePage(BasicSeleniumTest):
         logsize = self.driver.find_element_by_id(TEST_LOG_SIZE_ID)
         logsize.send_keys(TEST_NOT_VALID_DATA)
         self.driver.find_element_by_id(TEST_BTN_SAVE_ID).click()
-        result = self.driver.execute_script(TEST_SCRIPT_ERROR)
-        self.assertEqual(result, TEST_MSG_ERROR)
+        result = self.driver.find_element_by_class_name(TEST_CLASS_ALERT_ERROR)
+        self.assertEqual(result.text, TEST_MSG_ERROR)
