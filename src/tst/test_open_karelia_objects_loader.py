f104b9d6e3d7e8cf631f4112c484b3e7232c064b
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2015-12-29 11:13:44 +0300

    GT-2089: fixed pep8 issues

--- src/tst/test_open_karelia_objects_loader.py
+++ src/tst/test_open_karelia_objects_loader.py
@@ -10,10 +10,10 @@ class Test_GT_1509_open_karelia_objects_loader(TestCase):
 
     def test_GT_1509_open_karelia_objects_loader(self):
         obj = OpenKareliaObjectsLoader(
-           'http://geomongo/instance/service?number=1&offset=0')
+            'http://geomongo/instance/service?number=1&offset=0')
         try:
             data = obj.load()
             self.assertNotEquals(data, None)
             self.assertNotEquals(data, '')
         except ConnectionError:
-            print "Problem during data loading"   
+            print "Problem during data loading"
