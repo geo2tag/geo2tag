8bd5de0b5cbce249b9479d06683e5352dc344abb
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-01-11 17:51:50 +0300

    GT-2064: fixed src/db_model.py, changed tests src/tst/test_GT_1203_add_service.py and src/tst/test_GT_1329_addServiceDb.py

--- src/tst/test_GT_1203_add_service.py
+++ src/tst/test_GT_1203_add_service.py
@@ -4,10 +4,13 @@
 import unittest
 from db_model import addService, getDbObject
 from service_already_exists_exception import ServiceAlreadyExistsException
+from test_GT_1329_addServiceDb import checkGeneratedDb
 
 DB = "geomongo"
 COLLECTION = "services"
 ID = "_id"
+INVALID_SERVICE_NAME = "testservice"
+VALID_SERVICE_NAME = "test_GT_1203"
 
 
 class TestAddService(unittest.TestCase):
@@ -15,7 +18,9 @@ class TestAddService(unittest.TestCase):
     def testAddService(self):
         collection = getDbObject(DB)[COLLECTION]
         with self.assertRaises(ServiceAlreadyExistsException):
-            obj = addService("testservice", 1, ' ')
-        obj_id = addService("test_GT_1203", 1, ' ')
+            obj = addService(INVALID_SERVICE_NAME, 1, ' ')
+        obj_id = addService(VALID_SERVICE_NAME, 1, ' ')
         obj = collection.find_one({ID: obj_id})
         self.assertNotEqual(obj, None)
+
+        checkGeneratedDb(self, VALID_SERVICE_NAME)
