fc9050a048bdf10bfd383daf9fa8bd7e4338137f
TankBerlin <tank.berlin@gmail.com>
2016-01-19 11:21:25 +0100

    GT-2004

--- src/static/js/plugin_list.js
+++ src/static/js/plugin_list.js
@@ -2,7 +2,7 @@ function get_plugin_display(json){
     var plugin_name = json.name;
     var plugin_id = json.id;
     var result = '<div class="row" id="' + plugin_id + '"><div class="col-xs-8"><h3>' + plugin_name + '</h3></div>';
-    result += '<div class="col-xs-4"><button type="button" class="btn btn-primary btn-lg" plugin_id="' + plugin_id + '">X</button>';
+    result += '<div class="col-xs-4"><button type="button" class="btn btn-primary btn-lg btn-delete-plugin" plugin_id="' + plugin_id + '">X</button>';
     result += '<button type="button" class="btn btn-primary btn-lg btn-config-plugin" plugin_id="' + plugin_id + '">C</button>' + '</div></div>';
     return result;
 }
