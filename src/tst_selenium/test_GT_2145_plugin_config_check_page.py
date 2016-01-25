8eb29b33edf7b201d24087a3b7d1c4a1b3c70d4f
TankBerlin <tank.berlin@gmail.com>
2016-01-25 10:35:43 +0100

    GT-2145

--- src/tst_selenium/test_GT_2145_plugin_config_check_page.py
+++ src/tst_selenium/test_GT_2145_plugin_config_check_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from basic_selenium_test import BasicSeleniumTest
 
-TEST_URL = '/instance/admin/plugin'
+TEST_URL = '/instance/admin/plugin/config/test_plugin'
 H2 = u'Конфиг плагина'
 
 
