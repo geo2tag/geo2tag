042c58144a8f31b09c3b2bcc8be6cd088aafb6f8
TankBerlin <tank.berlin@gmail.com>
2016-01-25 16:20:25 +0100

    GT-2149

--- src/tst_selenium/test_GT_2149_unable_plugin.py
+++ src/tst_selenium/test_GT_2149_unable_plugin.py
@@ -11,14 +11,14 @@ TEST_ENABLED = u'enabled'
 
 
 class TestCheckUnablePluginBtn(BasicSeleniumTest):
-    
+
     def testCheckUnablePluginBtn(self):
         TEST_FLAG = False
         URL = self.getUrl(TEST_URL)
         self.getDriver().get(URL)
         self.getDriver().implicitly_wait(30)
         NAME = self.getDriver().find_elements_by_css_selector(
-           TEST_CLASS_NAME_SELECTOR)[0].text
+            TEST_CLASS_NAME_SELECTOR)[0].text
         response = requests.get(self.getUrl(TEST_URL_LIST_PLUGIN))
         response = json.loads(response._content)
         for i in response:
@@ -27,12 +27,12 @@ class TestCheckUnablePluginBtn(BasicSeleniumTest):
                     requests.get(self.getUrl(TEST_URL_MANAGE + NAME + '=True'))
                     TEST_FLAG = True
         BTN = self.getDriver().find_elements_by_css_selector(
-           TEST_CLASS_DELETE_BTN)[0]
+            TEST_CLASS_DELETE_BTN)[0]
         BTN.click()
         response = requests.get(self.getUrl(TEST_URL_LIST_PLUGIN))
         response = json.loads(response._content)
         for i in response:
             if i == NAME:
-                self.assertEqual(response[i][TEST_ENABLED], False)                 
-        if TEST_FLAG == False:
+                self.assertEqual(response[i][TEST_ENABLED], False)
+        if not TEST_FLAG:
             requests.get(self.getUrl(TEST_URL_MANAGE + NAME + '=True'))
