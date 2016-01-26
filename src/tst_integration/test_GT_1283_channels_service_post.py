37774618a661128e1437c2a33c2924a8809c8f82
TankBerlin <tank.berlin@gmail.com>
2016-01-26 14:24:11 +0100

    GT-2141

--- src/tst_integration/test_GT_1283_channels_service_post.py
+++ src/tst_integration/test_GT_1283_channels_service_post.py
@@ -2,7 +2,6 @@ import requests
 from basic_integration_test import BasicIntegrationTest
 import json
 from bson.objectid import ObjectId
-from db_model import getDbObject
 TEST_URL = '/instance/service/testservice/channel'
 
 TEST_SERVICE = 'testservice'
@@ -16,12 +15,13 @@ DATA2 = {'name': 123}
 class TestChannelServicePostRequest(BasicIntegrationTest):
 
     def testChannelServicePostRequest(self):
-        db = getDbObject(TEST_SERVICE)
         response = requests.post(self.getUrl(TEST_URL), data=DATA)
         responseText = response.text
         responseCode = response.status_code
         ID = json.loads(responseText)
+        
         db['channels'].remove({'_id': ObjectId(ID['$oid'])})
+
         self.assertNotEquals(responseText, [])
         self.assertNotEquals(responseText, {})
         self.assertEquals(responseCode, 200)
