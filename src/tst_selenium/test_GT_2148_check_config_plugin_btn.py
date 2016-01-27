58ce27894b62150aafb0630f05c89a43f11dcd70
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-01-27 13:04:33 +0300

    GT-2165: src/tst_selenium/test_GT_2148_check_config_plugin_btn.py fixed

--- src/tst_selenium/test_GT_2148_check_config_plugin_btn.py
+++ src/tst_selenium/test_GT_2148_check_config_plugin_btn.py
@@ -10,13 +10,17 @@ class TestCheckConfigPluginBtn(BasicSeleniumTest):
 
     def testCheckConfigPluginBtn(self):
         URL = self.getUrl(TEST_URL)
-        self.getDriver().get(URL)
-        self.getDriver().implicitly_wait(30)
-        BTN = self.getDriver().find_elements_by_css_selector(
+        driver = self.getDriver()
+        driver.get(URL)
+        driver.implicitly_wait(30)
+        BTN = driver.find_elements_by_css_selector(
             TEST_CLASS_BTN_CONFIG)[0]
-        NAME = self.getDriver().find_elements_by_css_selector(
+        NAME = driver.find_elements_by_css_selector(
             TEST_CLASS_NAME_SELECTOR)[0].text
         BTN.click()
+        main_window = driver.current_window_handle
+        driver.switch_to_window(driver.window_handles[1])
         self.assertEquals(
-            self.getDriver().current_url,
+            driver.current_url,
             str(URL) + TEST_CONFIG + NAME)
+        driver.switch_to_window(main_window)
