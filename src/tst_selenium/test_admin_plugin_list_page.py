5645eae6ef5e64c934992906e1bfdc8e6ea96d5b
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-01-27 11:24:59 +0300

    GT-2160: test commit

--- src/tst_selenium/test_admin_plugin_list_page.py
+++ src/tst_selenium/test_admin_plugin_list_page.py
@@ -27,19 +27,21 @@ class TestAdminPluginListPage(BasicSeleniumTest):
     def testCheckEnableButton(self):
         DISABLE = 'Disable'
         ENABLE = 'Enable'
+        BUTTON_SELECTOR = '[plugin_name="ok_import"]'
         URL = self.getUrl(TEST_URL)
         self.driver.get(URL)
 
         button = self.driver.find_element_by_css_selector(
-            '[plugin_name="ok_import"]')
+            BUTTON_SELECTOR)
         self.assertEquals(button.get_attribute('innerHTML'), DISABLE)
 
         button.click()
+
         WebDriverWait(self.driver, 10).until(
             expected_conditions.text_to_be_present_in_element(
-                (By.CSS_SELECTOR, '[plugin_name="ok_import"]'), ENABLE)
+                (By.CSS_SELECTOR, BUTTON_SELECTOR), ENABLE)
         )
 
         button1 = self.driver.find_element_by_css_selector(
-            '[plugin_name="ok_import"]')
+            BUTTON_SELECTOR)
         self.assertEquals(button1.get_attribute('innerHTML'), ENABLE)
