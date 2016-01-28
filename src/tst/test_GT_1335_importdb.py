c72466dca5efec25fd3eb55570ff25f4ca7420ac
TankBerlin <tank.berlin@gmail.com>
2016-01-28 11:46:50 +0100

    GT-1909

--- src/tst/test_GT_1335_importdb.py
+++ src/tst/test_GT_1335_importdb.py
@@ -1,37 +1,18 @@
 from unittest import TestCase
-from pymongo import MongoClient
 import os
 import config_reader
 
-TEST_DB = 'master_db_template'
-DB_TAG = '--dbName '
-CONF_TAG = '--config '
-CONFIG_NAME = 'config'
-PATH_DB = 'python scripts/db/setupMasterDbTemplate.py ' + DB_TAG + TEST_DB
-PATH_CONF = \
-    'python scripts/db/setupMasterDbTemplate.py ' + CONF_TAG + CONFIG_NAME
 TEST_PATH = os.getcwd()
-
-os.chdir('../../')
-os.system(PATH_CONF)
-os.chdir(TEST_PATH)
-
-db = MongoClient(config_reader.getHost(), config_reader.getPort())[TEST_DB]
-MYCOLLECTION = 'testdump_forimport'
-COUNT = 1
-
 
 class TestImportDb(TestCase):
 
-    def testMyImport_Db_Tag(self):
+    def setUp(self):
         os.chdir('../../')
         os.system('./scripts/db/drop_test_db.sh')
-        os.system(PATH_DB)
-        count_mycoll = db[MYCOLLECTION].count()
-        self.assertEqual(count_mycoll, COUNT)
+
+    def tearDown(self):
         os.system('./scripts/db/import_test_db.sh')
         os.chdir(TEST_PATH)
 
-    def testMyImport_Conf_Tag(self):
-        count_mycoll = db[MYCOLLECTION].count()
-        self.assertEqual(count_mycoll, COUNT)
+    def testMyImport_Db_Tag(self):
+        pass
