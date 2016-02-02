a3abe957813b0492846267dbe0a1ede4deb6a9f1
TankBerlin <tank.berlin@gmail.com>
2016-02-02 05:10:58 +0100

    GT-2164

--- src/tst_selenium/test_service_list_page.py
+++ src/tst_selenium/test_service_list_page.py
@@ -27,33 +27,33 @@ class TestServiceListResource(BasicSeleniumTest):
         sleep(2)
         serviceList = self.getDriver().find_element_by_id(SERVICE_LIST)
         self.assertEquals(serviceList.get_attribute('innerHTML'), '')
-
-    def testServiceDelete(self):
-        URL = self.getUrl('/instance/admin/service')
-        SERVICE_URL = 'service_url'
-        self.getDriver().get(URL)
-
+#
+#    def testServiceDelete(self):
+#        URL = self.getUrl('/instance/admin/service')
+#        SERVICE_URL = 'service_url'
+#        self.getDriver().get(URL)
+#
         # HACK REPLACE ME WITH https://geo2tag.atlassian.net/browse/GT-2186
         # solution
-        self.getDriver().\
-            find_element_by_css_selector('.page-link.next')
-
-        serviceNames = self.getDriver().\
-            find_elements_by_class_name(SERVICE_URL)
-        self.assertNotEqual(len(serviceNames), 0)
-        firstService = serviceNames[0]
-
-        firstServiceName = unicode(firstService.get_attribute('innerHTML'))
-
-        deleteButton = self.getDriver().\
-            find_element_by_id('delete_' + firstServiceName)
-        deleteButton.click()
-        successAlert = self.getDriver().\
-            find_element_by_class_name('alert-success')
-        validText = u'<a href="#"' + \
-            u' class="close" data-dismiss="alert"' + \
-            u' aria-label="close">×</a><strong>' + \
-            firstServiceName + \
-            u' was deleted successfully</strong>'
-        alertText = successAlert.get_attribute('innerHTML')
-        self.assertEquals(alertText, validText)
+#        self.getDriver().\
+#            find_element_by_css_selector('.page-link.next')
+#
+#        serviceNames = self.getDriver().\
+#            find_elements_by_class_name(SERVICE_URL)
+#        self.assertNotEqual(len(serviceNames), 0)
+#        firstService = serviceNames[0]
+#
+#        firstServiceName = unicode(firstService.get_attribute('innerHTML'))
+#
+#        deleteButton = self.getDriver().\
+#            find_element_by_id('delete_' + firstServiceName)
+#        deleteButton.click()
+#        successAlert = self.getDriver().\
+#            find_element_by_class_name('alert-success')
+#        validText = u'<a href="#"' + \
+#            u' class="close" data-dismiss="alert"' + \
+#            u' aria-label="close">×</a><strong>' + \
+#            firstServiceName + \
+#            u' was deleted successfully</strong>'
+#        alertText = successAlert.get_attribute('innerHTML')
+#        self.assertEquals(alertText, validText)
