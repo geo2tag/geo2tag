5b2a9a041d738e66843ac364ed1f7fc96cb4e3ce
TankBerlin <tank.berlin@gmail.com>
2016-02-16 22:20:51 +0100

    GT-2201

--- src/static/js/plugin_config.js
+++ src/static/js/plugin_config.js
@@ -1,1 +1,28 @@
+function getConfigDataFromPage(){
+    data = $('#container_config_plugin').html();
+    data = data.replace('<br>', '\n');
+    data = data.replace(/<\/?[^>]+>/g,'');
+    console.log(data);
+    return data;
+}
 
+function saveConfigPluginChange(plugin_name) {
+     page_data = getConfigDataFromPage();
+     $.ajax({
+        type: "PUT",
+        url: "/instance/service/" + plugin_name,
+        timeout: 150000,
+        crossDomain: true,
+        data: page_data,
+        success: function(json, status) {
+            console.log("save plugin config success" );
+            spinner.stop(target);
+            printBootstrapAlert(status,'Saving is finished successfully');
+        },
+        error: function (request, textStatus, errorThrown){
+            console.log("save plugin config result: " + textStatus);
+            msg = 'Saving is finished unsuccessfully ' + textStatus + ': ' + request.status;
+            printBootstrapAlert('danger', msg);
+        }
+    }); 
+}
