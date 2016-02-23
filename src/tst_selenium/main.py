c77917b570dc930cafc35671168ecd3a9bd27d67
TankBerlin <tank.berlin@gmail.com>
2016-02-23 12:41:14 +0100

    GT-1912

--- src/tst_selenium/main.py
+++ src/tst_selenium/main.py
@@ -33,6 +33,7 @@ from test_GT_2148_check_config_plugin_btn import TestCheckConfigPluginBtn
 from test_GT_2188_check_map_center import TestCheckMapCenter
 from test_GT_1912_btn_cancel_check import TestBtnCancelCheck
 
+
 def main(host):
     suite = unittest.TestSuite()
     suite.addTest(BasicSeleniumTest.parametrize(
