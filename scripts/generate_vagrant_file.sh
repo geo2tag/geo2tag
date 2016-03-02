d7e0dd842437636cc6e4c228f38d646a788445b7
TankBerlin <tank.berlin@gmail.com>
2016-03-02 16:51:35 +0100

    GT-2220

--- scripts/generate_vagrant_file.sh
+++ scripts/generate_vagrant_file.sh
@@ -10,4 +10,3 @@ SOURCE=$6
 cd vagrant
 cp Vagrantfile.template Vagrantfile
 sed -i -e 's/MEMORY/'"$MEMORY"'/g' -e 's/CPUS/'"$CPUS"'/g' -e 's/BOX/''"$BOX"''/g' -e 's|URL|''"$URL"''|g' -e 's/HD/''"$HD"''/g' -e 's|SOURCE|'"$SOURCE"'|g' Vagrantfile
-echo "$SOURCE"
