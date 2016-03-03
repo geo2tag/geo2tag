271146db9238cc3d235116def4ce2256d31efd2b
TankBerlin <tank.berlin@gmail.com>
2016-03-03 17:09:25 +0100

    GT-1959

--- scripts/check_task/check_git_conflict.py
+++ scripts/check_task/check_git_conflict.py
@@ -1,17 +1,17 @@
-from find_unsuccessfull_build import reopened_task, get_branch_number
 import subprocess
 
-SCRIPT = 'git fetch -a && git merge origin/master'
+CHECKOUT_SCRIPT = 'git checkout '
+MERGE_SCRIPT = '&& git fetch -a && git merge origin/master'
 PIPE = subprocess.PIPE
 ERROR_1 = 'Automatic merge failed'
 ERROR_2 = 'fatal: Exiting because of an unresolved conflict'
 
 
 def check_git_conflict(branch):
-    result = subprocess.Popen(SCRIPT, shell=True, stdin=PIPE, stdout=PIPE)
+    result = subprocess.Popen(CHECKOUT_SCRIPT + branch + MERGE_SCRIPT, shell=True, stdin=PIPE, stdout=PIPE)
     str_result = result.stdout.read()
-    if str_result.find(ERROR_1) == -1 or str_result.find(ERROR_2) == -1:
-        reopened_task(branch)
-
-if __name__ == '__main__':
-    check_git_conflict(get_branch_number())
+    print str_result
+    if str_result.find(ERROR_1) != -1 or str_result.find(ERROR_2) != -1:
+        return True
+    else:
+        return False
