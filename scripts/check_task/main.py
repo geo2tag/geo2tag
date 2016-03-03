43aa755179eced04a21d2adc026a66e40ab4e887
TankBerlin <tank.berlin@gmail.com>
2016-03-03 17:08:11 +0100

    GT-1959

---
+++ scripts/check_task/main.py
@@ -0,0 +1,43 @@
+from find_unsuccessfull_build import find_unsuccessfull_build_for_branch
+from check_test_scenario_field import check_test_scenario_field
+from check_pullrequest import check_pullrequest
+from check_git_conflict import check_git_conflict
+from check_git_branch import check_git_branch
+from jira_api import get_jira_server, get_jira_issue, reopen_issue
+import argparse
+
+ARG_BRANCH = '--branch'
+
+
+def check_issue(branch):
+    jira = get_jira_server()
+    issue = get_jira_issue(jira, branch)
+    test_scenario_field = check_test_scenario_field(issue)
+    if check_git_branch(branch) == True: 
+        conflict = check_git_conflict(branch)
+        pullrequest = check_pullrequest(branch)
+        success_build = find_unsuccessfull_build_for_branch(branch)
+        if not conflict and pullrequest and success_build and test_scenario_field:
+            print 'This issue', branch, 'is successfully completed'
+        else:
+            print 'This issue', branch, 'is unsuccessfully completed'
+            reopen_issue(jira, issue, branch)
+    else:
+        if test_scenario_field:
+            print 'This issue', branch, 'is successfully completed'
+        else:
+            print 'This issue', branch, 'is unsuccessfully completed'
+            reopen_issue(jira, issue, branch)
+
+
+def get_branch_number():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        ARG_BRANCH,
+        required=True)
+    args = parser.parse_args()
+    return args.branch
+
+
+if __name__ == '__main__':
+    check_issue(get_branch_number())
