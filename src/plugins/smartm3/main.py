b7f7e8ef6e76e701cf2696c174efc28a886f1d5a
TankBerlin <tank.berlin@gmail.com>
2016-03-11 11:51:17 +0100

    GT-2247

--- src/plugins/smartm3/main.py
+++ src/plugins/smartm3/main.py
@@ -2,7 +2,7 @@ from smartm3_resource import SmartM3Resource
 
 
 def getPluginResources():
-    result = {'service/<string:serviceName>/point': SmartM3Resource}
+    result = {'smartm3/service/<string:serviceName>/point': SmartM3Resource}
     return result
 
 
