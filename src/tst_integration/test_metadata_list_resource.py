1f56219f02bbe91be46f40261db16e0b0a8cc392
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-03-17 18:54:21 +0300

    fixed metadata list bug

--- src/tst_integration/test_metadata_list_resource.py
+++ src/tst_integration/test_metadata_list_resource.py
@@ -7,14 +7,17 @@ VALID_RESPONSE_CODE = 200
 NOT_VALID_RESPONSE_CODE = 400
 VALID_POST_ARGS = '{"json": {"name": "test_metadata_put"}}'
 NOT_VALID_POST_ARGS = "not_valid_data"
-VALID_GET_ARGS = "?number=1&offset=0&query={\"a\":1}"
-NOT_VALID_GET_ARGS = "?arg=1"
-
+VALID_GET_ARGS1 = '?number=1&offset=0&query={"a":1}'
+NOT_VALID_GET_ARGS = '?arg=1'
+VALID_GET_ARGS2 = '?number=1&offset=0'
 
 class TestMetadataListResource(BasicIntegrationTest):
 
     def testMetadataListResourceGet(self):
-        response = requests.get(self.getUrl(TEST_URL) + VALID_GET_ARGS)
+        response = requests.get(self.getUrl(TEST_URL) + VALID_GET_ARGS1)
+        responseCode = response.status_code
+        self.assertEquals(responseCode, VALID_RESPONSE_CODE)
+        response = requests.get(self.getUrl(TEST_URL) + VALID_GET_ARGS2)
         responseCode = response.status_code
         self.assertEquals(responseCode, VALID_RESPONSE_CODE)
         response = requests.get(self.getUrl(TEST_URL) + NOT_VALID_GET_ARGS)
