c5ae102eed3c12fd43e7d0d934092e106d2e8a24
TankBerlin <tank.berlin@gmail.com>
2016-03-22 12:56:10 +0100

    GT-2287

---
+++ src/plugins/plugin_for_test/main.py
@@ -0,0 +1,10 @@
+import os
+
+
+
+def getPluginInfo():
+    info = 'This plugin was creating for present plugins feature. ' \
+           'The function getPluginResources is for return TestResource1 ' \
+           'and TestResource2 '
+    print wertyu
+    return info
