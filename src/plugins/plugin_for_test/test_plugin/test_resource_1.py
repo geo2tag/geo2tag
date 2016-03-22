a79a9732c61d578c75871b7222fbd21f7d98f70a
TankBerlin <tank.berlin@gmail.com>
2016-03-22 12:57:23 +0100

    GT-2287

--- src/plugins/plugin_for_test/test_plugin/test_resource_1.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-
-class TestResource1(Resource):
-
-    def get(self):
-        return 'test_resource_1'
--- src/plugins/plugin_for_test/test_plugin/test_resource_2.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-
-class TestResource2(Resource):
-
-    def get(self):
-        return 'test_resource_2'
