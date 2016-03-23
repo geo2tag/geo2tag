d984dd9dbf3524a9d15b4c5dc4a639e37ad3cbc1
TankBerlin <tank.berlin@gmail.com>
2016-03-23 13:43:11 +0100

    GT-2259

--- scripts/check_task/main.py
+++ scripts/check_task/main.py
@@ -50,6 +50,7 @@ def check_issue(branch):
             comment = 'Test success'
             add_comment(jira, branch, comment)
             write_env_var(FAIL_REASON, SUCCESS_MSG)
+            print '----'
         else:
             print 'This issue', branch, 'is unsuccessfully completed'
             reopen_issue(jira, issue, branch, get_comment(False))
--- scripts/decide_build_success.sh
+++ scripts/decide_build_success.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "$FAIL_REASON"
 if [ "$FAIL_REASON" != "SUCCESS" ] 
 then
 	exit 1
