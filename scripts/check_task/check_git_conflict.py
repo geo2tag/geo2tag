d26054e43cfa44d72a10137a03cae66bb2a3d67e
TankBerlin <tank.berlin@gmail.com>
2016-03-23 13:49:28 +0100

    GT-2259

--- scripts/check_task/check_git_conflict.py
+++ scripts/check_task/check_git_conflict.py
@@ -1,6 +1,7 @@
 import subprocess
 
 CHECKOUT_SCRIPT = 'git checkout '
+PULL_SCRIPT = '&& git pull origin '
 MERGE_SCRIPT = '&& git fetch -a && git merge origin/master'
 PIPE = subprocess.PIPE
 ERROR_1 = 'Automatic merge failed'
@@ -10,6 +11,8 @@ ERROR_2 = 'fatal: Exiting because of an unresolved conflict'
 def check_git_conflict(branch):
     result = subprocess.Popen(
         CHECKOUT_SCRIPT +
+        branch +
+        PULL_SCRIPT + 
         branch +
         MERGE_SCRIPT,
         shell=True,
