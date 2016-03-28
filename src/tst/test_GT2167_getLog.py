a0199d3d5efd4b436ba2143824f32430d9afd8ff
ilyaolchikov <ilyaolchikov@mail.ru>
2016-03-28 21:24:06 +0300

    GT-2309

--- src/tst/test_GT2167_getLog.py
+++ src/tst/test_GT2167_getLog.py
@@ -5,13 +5,15 @@ from datetime import datetime
 from db_model import getLog, getDbObject
 
 DB = "geomongo"
-WRONG_DB = "wrong_db"
 COLLECTION = "log"
-
+DATE = "date"
+USER_ID = "user_id"
+STUB = "stub"
+MESS = "message"
+SERVICE = "service"
+TEST_SERVICE = "testservice"
 DATE_TO = datetime(2100, 1, 1)
 DATE_FROM = datetime(2000, 1, 1)
-WRONG_DATE_TO = datetime(3500, 1, 1)
-WRONG_DATE_FROM = datetime(3000, 1, 1)
 MESSAGE = 'testGT_2167'
 db = getDbObject()
 collection = db[COLLECTION]
@@ -20,10 +22,10 @@ collection = db[COLLECTION]
 def prepareDb():
     for i in range(10):
         # Inserting new post with date
-        post = {"user_id": "stub",
-                "date": datetime((2000 + i), 1, 1).utcnow().replace(2000 + i),
-                "message": "testGT_2167",
-                "service": "testservice"}
+        post = {USER_ID: STUB,
+                DATE: datetime((2000 + i), 1, 1).utcnow().replace(2000 + i),
+                MESS: MESSAGE,
+                SERVICE: TEST_SERVICE}
         collection.insert(post)
 
 
@@ -31,19 +33,19 @@ class TestGetLog(TestCase):
 
     def tearDown(self):
         collection.remove(
-            {"message": MESSAGE})
+            {MESS: MESSAGE})
 
     def testForDates(self):
         prepareDb()
         logList = list(getLog(DB, 10, None, None, None))
         for i in range(len(logList) - 1):
-            self.assertTrue(logList[i]['date'] > logList[i + 1]['date'])
+            self.assertTrue(logList[i][DATE] > logList[i + 1][DATE])
         logList = list(getLog(DB, 10, None, DATE_FROM, DATE_TO))
         for i in range(len(logList) - 1):
-            self.assertTrue(logList[i]['date'] > logList[i + 1]['date'])
+            self.assertTrue(logList[i][DATE] > logList[i + 1][DATE])
         logList = list(getLog(DB, 10, None, DATE_FROM, None))
         for i in range(len(logList) - 1):
-            self.assertTrue(logList[i]['date'] > logList[i + 1]['date'])
+            self.assertTrue(logList[i][DATE] > logList[i + 1][DATE])
         logList = list(getLog(DB, 10, None, None, DATE_TO))
         for i in range(len(logList) - 1):
-            self.assertTrue(logList[i]['date'] > logList[i + 1]['date'])
+            self.assertTrue(logList[i][DATE] > logList[i + 1][DATE])
