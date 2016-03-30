c8d06e39e581337c4bef763bc3949509f11f84bb
TankBerlin <tank.berlin@gmail.com>
2016-03-30 12:04:27 +0200

    GT-2214

--- src/tst_selenium/test_config_plugin.py
+++ src/tst_selenium/test_config_plugin.py
@@ -6,12 +6,14 @@ VALID_CONFIG = '[SECTION2]\nopt21=val21\nopt22=val22\nopt23=val23\n' + \
     '[SECTION1]\nopt11=val11\n'
 TEST_TEXTAREA_ID = 'container_config_plugin'
 TEST_SCRIPT_RETURN_VALUE = 'return $("#' + TEST_TEXTAREA_ID + '").val()'
-TEST_TEST_ADD = '[SECTION3]\nopt31=val31\n'
+TEST_TEST_ADD = 'opt31=val31\n'
 BTN_SAVE_CONFIG = 'save_plugin_btn'
-VALID_CONFIG_AFTER_SAVING = '[SECTION3]\nopt31=val31\n' + \
+VALID_CONFIG_AFTER_SAVING = \
     '[SECTION2]\nopt21=val21\nopt22=val22\nopt23=val23\n' + \
-    '[SECTION1]\nopt11=val11\n'
-TEST_SCRIPT_CHANGE_VAL = '$("#' + TEST_TEXTAREA_ID + '").val("")'
+    '[SECTION1]\nopt11=val11\nopt31=val31\n'
+TEST_SCRIPT_CLEAR_VAL = '$("#' + TEST_TEXTAREA_ID + '").val("")'
+TEST_SCRIPT_CHANGE_VAL = 'return $("#' + TEST_TEXTAREA_ID + '").val("' + \
+    VALID_CONFIG + '")'
 
 
 
@@ -29,8 +31,13 @@ class TestConfigPlugin(BasicSeleniumTest):
         TEST_TEXTAREA = self.getDriver().find_element_by_id(TEST_TEXTAREA_ID)
         TEST_TEXTAREA.send_keys(TEST_TEST_ADD)
         self.getDriver().find_element_by_id(BTN_SAVE_CONFIG).click()
-        URL = self.getUrl(TEST_URL)
         self.getDriver().get(URL)
         result = self.driver.execute_script(TEST_SCRIPT_RETURN_VALUE)
         self.assertEqual(result, VALID_CONFIG_AFTER_SAVING)
-        self.driver.execute_script(TEST_SCRIPT_CHANGE_VAL)
+        self.driver.execute_script(TEST_SCRIPT_CLEAR_VAL)
+        TEST_TEXTAREA = self.getDriver().find_element_by_id(TEST_TEXTAREA_ID)
+        TEST_TEXTAREA.send_keys(VALID_CONFIG)
+        self.getDriver().find_element_by_id(BTN_SAVE_CONFIG).click()
+        self.getDriver().get(URL)
+        result = self.driver.execute_script(TEST_SCRIPT_RETURN_VALUE)
+        self.assertEqual(result, VALID_CONFIG) 
