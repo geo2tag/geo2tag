997319427053dd6718c9e380aaa920e4da0a6273
TankBerlin <tank.berlin@gmail.com>
2016-04-05 16:55:17 +0200

    GT-2345

--- src/admin_log_parser.py
+++ src/admin_log_parser.py
@@ -7,6 +7,7 @@ OFFSET = 'offset'
 SERVICE = 'service_name'
 DATE_FROM = 'date_from'
 DATE_TO = 'date_to'
+TESTSERVICE = 'testservice'
 
 
 class AdminLogParser():
@@ -17,7 +18,7 @@ class AdminLogParser():
         parser.add_argument(SUBSTRING, type=unicode)
         parser.add_argument(NUMBER, type=int)
         parser.add_argument(OFFSET, type=int)
-        parser.add_argument(SERVICE, type=unicode)
+        parser.add_argument(SERVICE, type=unicode, default=TESTSERVICE)
         parser.add_argument(
             DATE_FROM, type=date_utils.datetime_from_iso8601, default=None)
         parser.add_argument(
