86f56347d8a5b767e0c6fb79e740ccbdb83f2459
TankBerlin <tank.berlin@gmail.com>
2016-04-05 13:00:32 +0200

    GT-2345

--- src/admin_log_parser.py
+++ src/admin_log_parser.py
@@ -3,6 +3,9 @@ from flask_restful import reqparse
 SUBSTRING = 'substring'
 NUMBER = 'number'
 OFFSET = 'offset'
+SERVICE = 'service_name'
+DATE_FROM = 'date_from'
+DATE_TO = 'date_to'
 
 
 class AdminLogParser():
@@ -13,5 +16,10 @@ class AdminLogParser():
         parser.add_argument(SUBSTRING, type=unicode)
         parser.add_argument(NUMBER, type=int)
         parser.add_argument(OFFSET, type=int)
+        parser.add_argument(SERVICE, type=unicode)
+        parser.add_argument(
+            DATE_FROM, type=date_utils.datetime_from_iso8601, default=None)
+        parser.add_argument(
+            DATE_TO, type=date_utils.datetime_from_iso8601, default=None)
         args = parser.parse_args()
         return args
--- src/admin_log_resource.py
+++ src/admin_log_resource.py
@@ -3,20 +3,29 @@ from flask import render_template
 from flask import make_response
 from admin_log_parser import LogAdminParser
 from db_model import getLog
+from date_utils import dateDeserialiser
 
 SUBSTRING = 'substring'
 NUMBER = 'number'
 OFFSET = 'offset'
+SERVICE = 'service_name'
+DATE_FROM = 'date_from'
+DATE_TO = 'date_to'
 
 
 class AdminLogResource(Resource):
 
     def get(self):
         parser_dict = AdminLogParser.parseGetParameters()
-        number = parser_dict[NUMBER]
-        offset = parser_dict[OFFSET]
-        substr = parser_dict[SUBSTRING]
-        getLog()
+        getLog(parser_dict[SERVICE],
+            parser_dict[NUMBER],
+            parser_dict[OFFSET],
+            dateDeserialiser(
+                parser_dict,
+                DATE_FROM),
+            dateDeserialiser(
+                parser_dict,
+                DATE_TO))
         return make_response(
             render_template(
                 'log.html'))
