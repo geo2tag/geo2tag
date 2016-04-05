b988b9ba95533572993cdf4e34208b709bb3332c
TankBerlin <tank.berlin@gmail.com>
2016-04-05 13:08:08 +0200

    GT-2345

---
+++ src/tst_integration/test_GT_2345_admin_log_resource.py
@@ -0,0 +1,14 @@
+import requests
+from basic_integration_test import BasicIntegrationTest
+
+TEST_URL = "/instance/admin/log?" \
+            "number=10&subsrting=plugin"
+VALID_RESPONSE_CODE = 200
+
+
+class TestAdminLogResource(BasicIntegrationTest):
+
+    def testAdminLogResource(self):
+        response = requests.get(self.getUrl(TEST_URL))
+        responseCode = response.status_code
+        self.assertEquals(responseCode, VALID_RESPONSE_CODE)
