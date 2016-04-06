36efe3e1f73f006b22b052de50857f5738531236
TankBerlin <tank.berlin@gmail.com>
2016-04-06 22:07:45 +0200

    GT-2377

--- src/static/js/parsing_url.js
+++ src/static/js/parsing_url.js
@@ -2,6 +2,12 @@ function getUrlPage(){
     return window.location.toString();
 }
 
-function getArgsQuery(url){ # return json
-    return URI.parseQuery(url)
+function getArgsQuery(url){ // return json
+    var i = url.indexOf('?');
+    if(i == -1)
+        return {};
+    else{
+        url = url.substring(i)
+        return URI.parseQuery(url)
+    }
 }
