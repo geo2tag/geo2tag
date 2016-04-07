1596b6b15713a7a10bfb49a162327c958c16449c
TankBerlin <tank.berlin@gmail.com>
2016-04-07 12:52:40 +0200

    GT-2377

--- src/static/js/parsing_url.js
+++ src/static/js/parsing_url.js
@@ -7,12 +7,15 @@ function getArgsQuery(url){ // return json
     var params = uri.search();
     var SERVICE = 'service/';
     var MAP = '/map';
+    var ZOOM = 'zoom';
     var SERVICE_NAME_FIELD = 'serviceName';
     var index_beg = url.indexOf(SERVICE) + SERVICE.length;
     var index_end = url.indexOf(MAP);
     var SERVICE_NAME = url.substring(index_beg, index_end)
     result = URI.parseQuery(params);
     result[SERVICE_NAME_FIELD] = SERVICE_NAME;
+    if(result[ZOOM] == undefined)
+        result[ZOOM] = 14;
     console.log(result)
     return result;
 }
