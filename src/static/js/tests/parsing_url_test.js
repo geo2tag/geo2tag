a83c58471f23c285a77342643b6842c3495f449c
TankBerlin <tank.berlin@gmail.com>
2016-04-11 14:37:02 +0200

    GT-2378

--- src/static/js/tests/parsing_url_test.js
+++ src/static/js/tests/parsing_url_test.js
@@ -1,6 +1,6 @@
 QUnit.test('Parsing url', function(assert) {
     url = 'http://geomongo/instance/service/testservice/map?longitude=10&lantitude=3'
-    par = getArgsQueryForMap(url)
+    par = getArgsQueryForMap(url, [])
     assert.equal(par['longitude'], 10);
     assert.equal(par['lantitude'], 3);
 });
