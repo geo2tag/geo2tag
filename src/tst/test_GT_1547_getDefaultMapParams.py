5145a8113e984d80da1dd41ad38d9e2de4ec859d
TankBerlin <tank.berlin@gmail.com>
2016-04-11 14:22:03 +0200

    GT-2378

--- src/tst/test_GT_1547_getDefaultMapParams.py
+++ src/tst/test_GT_1547_getDefaultMapParams.py
@@ -12,5 +12,5 @@ CHANNEL_IDS = 'channel_ids'
 class TestgetDefaultMapParams(TestCase):
 
     def testgetDefaultChannelIds(self):
-        result = getDefaultMapParams(SERVICE_NAME)
+        result = getDefaultChannelIds(SERVICE_NAME)
         self.assertEqual(result[CHANNEL_IDS][0], TEST_ID)
