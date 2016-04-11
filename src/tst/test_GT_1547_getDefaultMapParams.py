1cfabe8ca24bc24dc133b3b47efc0036403250f6
TankBerlin <tank.berlin@gmail.com>
2016-04-11 14:15:20 +0200

    GT-2378

--- src/tst/test_GT_1547_getDefaultMapParams.py
+++ src/tst/test_GT_1547_getDefaultMapParams.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
-from map_resource import getDefaultMapParams
+from map_resource import getDefaultChannelIds
 
 SERVICE_NAME = 'testservice'
 TEST_ID = '556721a52a2e7febd2744200'
@@ -11,6 +11,6 @@ CHANNEL_IDS = 'channel_ids'
 
 class TestgetDefaultMapParams(TestCase):
 
-    def testgetDefaultMapParams(self):
+    def testgetDefaultChannelIds(self):
         result = getDefaultMapParams(SERVICE_NAME)
         self.assertEqual(result[CHANNEL_IDS][0], TEST_ID)
