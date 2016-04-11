468b5f5b627916f1ef66023985bb4aab8dd2b255
TankBerlin <tank.berlin@gmail.com>
2016-04-11 13:58:59 +0200

    GT-2378

--- src/map_resource.py
+++ src/map_resource.py
@@ -4,35 +4,22 @@ from flask import make_response
 from db_model import getAllChannelIds
 from map_resource_parser import MapParser
 
-RADIUS_VALUE = 1000
-NUMBER_VALUE = 1000
-SERVICE_NAME = 'serviceName'
 CHANNEL_IDS = 'channel_ids'
-ZOOM_VALUE = 14
-NUMBER = 'number'
-LATITUDE = 'latitude'
-LONGITUDE = 'longitude'
-ZOOM = 'zoom'
-RADIUS = 'radius'
 
 
 class MapResource(Resource):
 
     def get(self, serviceName=None):
         args = MapParser.parseGetParameters()
-        for val in args.values():
-            if val is not None:
-                return make_response(render_template('map.html'))
-        get_param = getDefaultMapParams(serviceName)
-        return make_response(render_template('map.html', params=get_param))
+        result = {}
+        if args[CHANNEL_IDS] is None:
+            result.update(getDefaultChannelIds(serviceName))
+        
+        return make_response(render_template('map.html', params=result))
 
 
-def getDefaultMapParams(serviceName):
+def getDefaultChannelIds(serviceName):
     result = {}
     all_channel_ids = getAllChannelIds(serviceName)
     result[CHANNEL_IDS] = all_channel_ids
-    result[SERVICE_NAME] = serviceName
-    result[NUMBER] = NUMBER_VALUE
-    result[ZOOM] = ZOOM_VALUE
-    result[RADIUS] = RADIUS_VALUE
     return result
--- src/static/js/parsing_url.js
+++ src/static/js/parsing_url.js
@@ -2,20 +2,26 @@ function getUrlPage(){
     return window.location.toString();
 }
 
-function getArgsQuery(url){ // return json
+function getArgsQueryForMap(url, channel_ids){ // return json
     var uri = new URI(url);
     var params = uri.search();
     var SERVICE = 'service/';
     var MAP = '/map';
     var ZOOM = 'zoom';
     var SERVICE_NAME_FIELD = 'serviceName';
+    var CHANNEL_IDS = 'channel_ids'
     var index_beg = url.indexOf(SERVICE) + SERVICE.length;
     var index_end = url.indexOf(MAP);
     var SERVICE_NAME = url.substring(index_beg, index_end)
     result = URI.parseQuery(params);
+    console.log(result)
     result[SERVICE_NAME_FIELD] = SERVICE_NAME;
     if(result[ZOOM] == undefined)
         result[ZOOM] = 14;
+    if(channel_ids.length != 0)
+        result[CHANNEL_IDS] = channel_ids
+    else
+        result[CHANNEL_IDS] = JSON.parse(result[CHANNEL_IDS])
     console.log(result)
     return result;
 }
