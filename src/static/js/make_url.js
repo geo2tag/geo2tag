f07fc6f344ebf49d5a44094174fb61a2f43ebbc2
4ukuta <nikita.melnikov95@gmail.com>
2016-04-11 20:30:46 +0300

    GT-2264

--- src/static/js/make_url.js
+++ src/static/js/make_url.js
@@ -1,4 +1,4 @@
-function MakeUrlByChannelIds(service_name,channel_ids,number_point){
+function MakeUrlByChannelIds(service_name,channel_ids,number_point,zoom,latitude,longitude){
     var POINT = 'point';
     var NUMBER = 'number';
     var CHANNEL_IDS = 'channel_ids';
@@ -8,5 +8,5 @@ function MakeUrlByChannelIds(service_name,channel_ids,number_point){
         str_channel_ids += '&' + CHANNEL_IDS +'=' + channel_ids[i];
     }
     url = url + '/' + POINT + '?' + NUMBER + '=' + number_point + str_channel_ids;
-    return url;
+    return url  + "&lat1={lat1}&lat2={lat2}&lon1={lon1}&lon2={lon2}";
 }
--- src/static/js/tests/make_url_by_channel_ids.js
+++ src/static/js/tests/make_url_by_channel_ids.js
@@ -2,7 +2,7 @@ QUnit.test('Test make url by channel_ids', function(assert) {
 	var done = assert.async();
 	var TEST_CHANNEL = 'test123';
 	var TEST_CHANNEL_IDS = ["1","2","3"];
-	var test_result = '/instance/service/test123/point?number=1000&channel_ids=1&channel_ids=2&channel_ids=3';
+	var test_result = "/instance/service/test123/point?number=1000&channel_ids=1&channel_ids=2&channel_ids=3&lat1={lat1}&lat2={lat2}&lon1={lon1}&lon2={lon2}";
 	var url = MakeUrlByChannelIds(TEST_CHANNEL,TEST_CHANNEL_IDS,1000);
 	assert.equal(url,test_result);
 	done();
