3d7e949cc025b8409d9195bfb73ea9ad84c47a3e
TankBerlin <tank.berlin@gmail.com>
2016-04-14 17:07:34 +0200

    master test

--- scripts/db/drop_test_db.sh
+++ scripts/db/drop_test_db.sh
@@ -3,4 +3,3 @@
 mongo geomongo --eval "db.dropDatabase()"
 mongo testservice --eval "db.dropDatabase()"
 mongo master_db_template --eval "db.dropDatabase()"
-mongo geomongo --eval "db.dropDatabase()"
