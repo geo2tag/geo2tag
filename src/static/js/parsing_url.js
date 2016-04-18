6676e4c3e91dbcf6c35313c26ebe517e685918af
TankBerlin <tank.berlin@gmail.com>
2016-04-18 19:34:13 +0200

    GT-2389

--- src/static/js/parsing_url.js
+++ src/static/js/parsing_url.js
@@ -2,12 +2,16 @@ function getUrlPage(){
     return window.location.toString();
 }
 
+var NUMBER_VAL = 1000;
+var ZOOM_VAL = 14;
+
 function getArgsQueryForMap(url, channel_ids){ // return json
     var uri = new URI(url);
     var params = uri.search();
     var SERVICE = 'service/';
     var MAP = '/map';
     var ZOOM = 'zoom';
+    var NUMBER = 'number';
     var SERVICE_NAME_FIELD = 'serviceName';
     var CHANNEL_IDS = 'channel_ids'
     var index_beg = url.indexOf(SERVICE) + SERVICE.length;
@@ -15,8 +19,10 @@ function getArgsQueryForMap(url, channel_ids){ // return json
     var SERVICE_NAME = url.substring(index_beg, index_end)
     result = URI.parseQuery(params);
     result[SERVICE_NAME_FIELD] = SERVICE_NAME;
+    if(result[NUMBER] == undefined)
+        result[NUMBER] = NUMBER_VAL;
     if(result[ZOOM] == undefined)
-        result[ZOOM] = 14;
+        result[ZOOM] = ZOOM_VAL;
     if(channel_ids.length != 0)
         result[CHANNEL_IDS] = channel_ids
     else if(result[CHANNEL_IDS] != undefined)
