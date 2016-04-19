f028dabbc2402b427727af6e686e9b2a256882ed
TankBerlin <tank.berlin@gmail.com>
2016-04-19 14:50:24 +0200

    GT-2389

--- src/icon_resource.py
+++ src/icon_resource.py
@@ -1,8 +1,7 @@
 from flask_restful import Resource
 from possible_exception import possibleException
-from icon_resource_parser import IconResourceParser, MARKER, RADIUS, CHANNEL_ID
+from icon_resource_parser import IconResourceParser, CHANNEL_ID
 import hashlib
-import json
 
 
 class IconResource(Resource):
@@ -11,28 +10,21 @@ class IconResource(Resource):
     def get(self, serviceName):
         print serviceName
         args = IconResourceParser.parseGetParameters()
-        template = generateMarkerSvgCode(
-            json.loads(args[MARKER]), args[RADIUS], args[CHANNEL_ID])
+        template = generateMarkerSvgCode(args[CHANNEL_ID])
         return render_template("icon.svg", template = template)
 
 
-def generateMarkerSvgCode(marker, radius, channel_id):
-    template = u'<svg><circle cx="X_PH" cy="Y_PH" r="RADIUS_PH" ' + \
+def generateMarkerSvgCode(channel_id):
+    template = u'<svg><circle cx="5" cy="5" r="10" ' + \
         'transform="matrix(1 0 0 1 0 0)" ' + \
-        'style="fill: rgb("R_PH", "G_PH", "B_PH"); ' + \
+        'fill="#PH_COLOR"' + \
         'cursor: move;"></circle></svg>'
     colors = getColorsFromChannelId(channel_id)
-    placeHolders = {'X_PH': u'x', 'Y_PH': u'y', 'R_PH': 'R',
-                    'G_PH': 'G', 'B_PH': 'B'}
-    marker.update(colors)
-    for placeHolder in placeHolders:
-        field = placeHolders[placeHolder]
-        template = template.replace(placeHolder, marker[field])
-    template = template.replace('RADIUS_PH', str(radius))
+    template = template.replace('PH_COLOR', colors)
     return template
 
 
 def getColorsFromChannelId(channel_id):
     hdigest = hashlib.md5(channel_id).hexdigest()
-    colors = {"R": hdigest[:2], "G": hdigest[3:5], "B": hdigest[6:8]}
+    colors = hdigest[:6]
     return colors
--- src/static/js/map.js
+++ src/static/js/map.js
@@ -29,8 +29,6 @@ $(document).ready(function (){
     var LOCATION = 'location'
     var SERVICE_NAME ='serviceName'
     var CHANNEL_IDS = 'channel_ids'
-    var MARKER = '{"x" : "7", "y" : "7"}'
-    var RADIUS = 10
     
     var url = MakeUrlByChannelIds(par);
     var l = new L.LayerJSON({url: url,
@@ -40,8 +38,7 @@ $(document).ready(function (){
         }, 
         buildIcon: function(data, title) {
             return new L.Icon({
-                iconUrl : "get_icon?marker=" + MARKER + "&radius=" + RADIUS + "&channel_id=" + par[CHANNEL_IDS][0],
-                iconSize: new L.Point(30, 30)
+                iconUrl : "get_icon?channel_id=" + data.channel_id.$oid
             });
         }
     }); 
