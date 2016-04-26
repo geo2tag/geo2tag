5ee1e748423a276c9f45f3e52ea02105a9a5f520
TankBerlin <tank.berlin@gmail.com>
2016-04-26 13:18:31 +0200

    GT-2396

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -18,30 +18,39 @@ function fixMapSize(){
     map.invalidateSize();
 }
 
-$(document).ready(function (){
-    if(par.latitude != null && par.longitude != null)
-        map = createMap('map', false, par.zoom, par.latitude, par.longitude);
-    else
-        map = createMap('map', true, par.zoom)
-    $(window).on('resize', fixMapSize());
-    var path_marker = '../../../static/img';
-    var COORDINATES = 'coordinates'
-    var LOCATION = 'location'
-    var SERVICE_NAME ='serviceName'
-    var CHANNEL_IDS = 'channel_ids'
-    
-    var url = MakeUrlByChannelIds(par);
+function getLayer(url){
+    console.log('layer')
     var l = new L.LayerJSON({url: url,
         propertyLoc: ['location.coordinates.0','location.coordinates.1'],
         buildPopup: function(data) {
             return data.json.name || null;
-        }, 
+        },
         buildIcon: function(data, title) {
             var url_icon = "get_icon?channel_id=" + data.channel_id.$oid
             return new L.Icon({
                 iconUrl : url_icon
             });
         }
-    }); 
-    map.addLayer(l);
+    });
+    return l;
+}
+
+$(document).ready(function (){
+    if(par.latitude != null && par.longitude != null)
+        map = createMap('map', false, par.zoom, par.latitude, par.longitude);
+    else
+        map = createMap('map', true, par.zoom)
+    $(window).on('resize', fixMapSize());
+    var url = MakeUrlByChannelIds(par);
+    if(par.refresh != 0){
+        setInterval(function() {refreshMap(url)}, par.refresh * 1000);
+    }
+    else
+        refreshMap(url);
 });
+
+function refreshMap(url){
+    var l = getLayer(url);
+    map.addLayer(l);
+}
+
