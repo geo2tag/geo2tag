7ced8d43d994a2eb2cdfbacd772eee51e0859d3b
TankBerlin <tank.berlin@gmail.com>
2016-05-10 10:18:05 +0200

    GT-2271

--- scripts/pdocker.py
+++ scripts/pdocker.py
@@ -19,7 +19,7 @@ db = MongoClient(HOST, PORT)[DBNAME]
 # scripts
 CREATE_CONTAINER = "scripts/docker_create.sh"
 MANAGE_CONTAINER = "scripts/docker_manage.sh"
-CAT_LOCAL_DEPLOY_LOG = 'cat scripts/local_deploy.log'
+CAT_LOCAL_DEPLOY_LOG = 'cat local_deploy.log'
 
 # keys
 CONTAINER_NAME = "name"
