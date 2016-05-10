896228287c13aa2f6133d5b9c5e19d092771630d
TankBerlin <tank.berlin@gmail.com>
2016-05-10 12:22:39 +0200

    GT-2271

--- scripts/pdocker.py
+++ scripts/pdocker.py
@@ -230,7 +230,7 @@ def main(name, ports):
         write_log(container_start_name, NO_PORTS_MSG)
         write_env_var(FAIL_REASON, NO_PORTS_MSG)
         sys.exit(0)
-    manage_script(name, ['docker', 'exec', '/bin/bash', '-c',
+    manage_script(name, ['docker', 'exec', name, '/bin/bash', '-c',
                          CAT_LOCAL_DEPLOY_LOG])
     mongo_start_waiter(container_start_name)
     write_log(
@@ -271,7 +271,6 @@ if __name__ == "__main__":
     parser.add_argument('-k', '--kill', action='store_true')
     parser.add_argument('-t', '--time', default='1w')
     parsed_args = parser.parse_args()
-    print '===================='
     if parsed_args.kill is not False:
         timestamp = 0
         if parsed_args.time is not None:
