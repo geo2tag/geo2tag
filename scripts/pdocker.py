27cad57e1251fb1ed823c69d458a8e410ca38c56
TankBerlin <tank.berlin@gmail.com>
2016-05-10 12:31:01 +0200

    GT-2271

--- scripts/pdocker.py
+++ scripts/pdocker.py
@@ -230,8 +230,9 @@ def main(name, ports):
         write_log(container_start_name, NO_PORTS_MSG)
         write_env_var(FAIL_REASON, NO_PORTS_MSG)
         sys.exit(0)
-    manage_script(name, ['docker', 'exec', name, '/bin/bash', '-c',
-                         CAT_LOCAL_DEPLOY_LOG])
+    manage_script(container_start_name,
+                  ['docker', 'exec', container_start_name,
+                   '/bin/bash', '-c', CAT_LOCAL_DEPLOY_LOG])
     mongo_start_waiter(container_start_name)
     write_log(
         container_start_name,
