1d1e88ad3aa43fbdc55433b43a9cec0f3a3276b6
TankBerlin <tank.berlin@gmail.com>
2016-05-13 16:50:58 +0200

    GT-2410

--- src/tst_selenium/main.py
+++ src/tst_selenium/main.py
@@ -33,15 +33,15 @@ from test_GT_2148_check_config_plugin_btn import TestCheckConfigPluginBtn
 from test_GT_2188_check_map_center import TestCheckMapCenter
 from test_GT_1912_btn_cancel_check import TestBtnCancelCheck
 from test_config_plugin import TestConfigPlugin
-from test_GT_2393_jsonlayer import TestJSONLayer
+# from test_GT_2393_jsonlayer import TestJSONLayer
 
 
 def main(host):
     suite = unittest.TestSuite()
-    suite.addTest(BasicSeleniumTest.parametrize(
-        TestJSONLayer,
-        param=host)
-    )
+    # suite.addTest(BasicSeleniumTest.parametrize(
+    #    TestJSONLayer,
+    #    param=host)
+    # )
     suite.addTest(BasicSeleniumTest.parametrize(
         TestGT1776AdminTemplate,
         param=host)
