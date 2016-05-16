5b69be0a4e831be74016c7a1674d9d63271ebfe1
TankBerlin <tank.berlin@gmail.com>
2016-05-16 11:18:14 +0200

    GT-2350

--- src/decoding_of_the_json_data_failed_excertion.py
+++ src/decoding_of_the_json_data_failed_excertion.py
@@ -1,12 +1,12 @@
 from base_geo2tag_exception import BaseGeo2TagException
 from werkzeug.exceptions import BadRequest
 
-URL = 'https://tools.ietf.org/html/rfc7159'
+URL = "https://tools.ietf.org/html/rfc7159"
 
 
 class DecodingOfTheJsonDataFailedException(BaseGeo2TagException, BadRequest):
 
     def getReturnObject(self):
-        ERROR = 'Decoding of the JSON data failed: JSON data request' + \
-            'doesn/'t conform to the standard ' + URL
+        ERROR = "Decoding of the JSON data failed: JSON data request" + \
+            "doesn't conform to the standard " + URL
         return ERROR, 400
