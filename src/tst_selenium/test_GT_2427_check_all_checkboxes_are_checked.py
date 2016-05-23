99140ad0b8f1884d8dd3fab20eb5fd52ee29c9ec
TankBerlin <tank.berlin@gmail.com>
2016-05-23 15:05:03 +0200

    GT-2427

--- src/tst_selenium/test_GT_2427_check_all_checkboxes_are_checked.py
+++ src/tst_selenium/test_GT_2427_check_all_checkboxes_are_checked.py
@@ -1,9 +1,10 @@
 from basic_selenium_test import BasicSeleniumTest
 
-TEST_URL = '/instance/service/testservice/map'
-TEST_SELECTOR = 'leaflet-control-layers-toggle'
-TEST_CHECKBOX_SELECTOR = 'input.leaflet-control-layers-selector'
-
+TEST_URL = '/instance/service/testservice/map?refresh=0'
+TEST_CHECKBOX_SELECTOR = '.leaflet-control-layers-selector'
+TYPE = 'type'
+CHECKBOX = 'checkbox'
+CHECKED = 'checked'
 
 
 class TestCheckAllCheckboxesAreChecked(BasicSeleniumTest):
@@ -12,9 +13,8 @@ class TestCheckAllCheckboxesAreChecked(BasicSeleniumTest):
         URL = self.getUrl(TEST_URL)
         self.getDriver().get(URL)
         self.getDriver().implicitly_wait(30)
-        obj = self.getDriver().find_element_by_css_selector(TEST_SELECTOR)
-        obj.click()
         list_checkboxes = self.getDriver().find_elements_by_css_selector(
             TEST_CHECKBOX_SELECTOR)
-        
-        
+        for ch in list_checkboxes:
+            if ch.get_attribute(TYPE) == CHECKBOX:
+                self.assertTrue(ch.get_attribute(CHECKED))
